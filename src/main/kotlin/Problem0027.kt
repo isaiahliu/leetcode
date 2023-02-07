c4a9358cbe97c350ff134d73e8d5f8e147c4d5df
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-07 10:03:47 +0800

    problem 27

---
+++ src/main/kotlin/Problem0027.kt
@@ -0,0 +1,20 @@
+fun main() {
+    class Solution {
+        fun removeElement(nums: IntArray, `val`: Int): Int {
+            var processIndex = 0
+            var watchIndex = 0
+
+            while (watchIndex < nums.size) {
+                val num = nums[watchIndex++]
+
+                if (num != `val`) {
+                    nums[processIndex++] = num
+                }
+            }
+
+            return processIndex
+        }
+    }
+
+}
+

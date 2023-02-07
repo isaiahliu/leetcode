d0f4f223e72f4e9c2d2ebdaf4eb4ca03917f35f6
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-07 14:29:01 +0800

    problem 32

---
+++ src/main/kotlin/Problem0032.kt
@@ -0,0 +1,51 @@
+import java.util.*
+
+fun main() {
+    class Solution {
+        fun longestValidParentheses(s: String): Int {
+            var max = 0
+
+            val stack = LinkedList<Int>()
+            s.forEach {
+                when (it) {
+                    '(' -> {
+                        stack.push(-1)
+                    }
+
+                    ')' -> {
+                        var sum = 0
+                        var match = false
+                        while (!match && stack.isNotEmpty()) {
+                            val t = stack.pop()
+                            if (t == -1) {
+                                match = true
+                                sum += 2
+                            } else {
+                                sum += t
+                            }
+                        }
+
+                        if (match) {
+                            while (stack.isNotEmpty()) {
+                                if (stack.peek() == -1) {
+                                    break
+                                } else {
+                                    sum += stack.pop()
+                                }
+                            }
+
+                            max = max.coerceAtLeast(sum)
+
+                            stack.push(sum)
+                        }
+                    }
+                }
+            }
+
+            return max
+        }
+    }
+
+    println(Solution().longestValidParentheses("()(()"))
+}
+

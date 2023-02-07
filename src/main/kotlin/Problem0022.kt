ac4d67a2e3a7e107ecf43c700a8f3d80fe47cebd
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-07 09:32:38 +0800

    problem 22

---
+++ src/main/kotlin/Problem0022.kt
@@ -0,0 +1,28 @@
+fun main() {
+    class Solution {
+        fun generateParenthesis(n: Int): List<String> {
+            val result = arrayListOf<String>()
+
+            if (n == 0) {
+                return listOf("")
+            }
+
+            if (n == 1) {
+                return listOf("()")
+            }
+
+            repeat(n) {
+                result += generateParenthesis(it).map { left ->
+                    generateParenthesis(n - it - 1).map { right ->
+                        "(${left})${right}"
+                    }
+                }.flatten()
+            }
+
+            return result
+        }
+    }
+
+    println(Solution().generateParenthesis(3))
+}
+

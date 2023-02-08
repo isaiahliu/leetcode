17a0017b5da9074b18d16ab94615eeb34e578001
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-08 14:31:49 +0800

    problem 51

---
+++ src/main/kotlin/Problem0051.kt
@@ -0,0 +1,40 @@
+fun main() {
+    class Solution {
+        fun solveNQueens(n: Int): List<List<String>> {
+            val pos = arrayListOf(-1)
+
+            val result = arrayListOf<List<String>>()
+
+            while (pos.isNotEmpty()) {
+                if (pos.size == n + 1) {
+                    pos.removeAt(n)
+
+                    result.add(pos.map { p ->
+                        CharArray(n) {
+                            if (p == it) 'Q' else '.'
+                        }.joinToString("")
+                    })
+                }
+
+                val last = pos.last()
+                pos.remove(last)
+
+                (last + 1 until n).firstOrNull { new ->
+                    val index = pos.size
+
+                    pos.filterIndexed { i, n ->
+                        n == new || (index - i) == (n - new) || (index - i) == (new - n)
+                    }.isEmpty()
+                }?.let {
+                    pos += it
+                    pos += -1
+                }
+            }
+
+            return result
+        }
+    }
+
+    println(Solution().solveNQueens(1).size)
+}
+

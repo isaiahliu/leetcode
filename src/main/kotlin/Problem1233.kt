901f08b178642cf2260a7d81281af4212bcb222d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-08 09:58:00 +0800

    problem 1233

---
+++ src/main/kotlin/Problem1233.kt
@@ -0,0 +1,19 @@
+fun main() {
+    class Solution {
+        fun removeSubfolders(folder: Array<String>): List<String> {
+            folder.sort()
+
+            var top = folder[0] + "z"
+            return buildList {
+                folder.forEach {
+                    if (!it.startsWith(top)) {
+                        add(it)
+                        top = "$it/"
+                    }
+                }
+            }
+        }
+    }
+
+    Solution().removeSubfolders(arrayOf("/a", "/a/b", "/c/d", "/c/d/e", "/c/f"))
+}

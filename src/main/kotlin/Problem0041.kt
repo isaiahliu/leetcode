920fd0399af85a2cd76e0df35732bfa2391e56ae
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-08 09:15:30 +0800

    problem 41

--- src/main/kotlin/Problem0041.kt
+++ src/main/kotlin/Problem0041.kt
@@ -3,75 +3,30 @@ fun main() {
         fun firstMissingPositive(nums: IntArray): Int {
             val range = 1..nums.size
 
-            var hasOne = false
             for (i in nums.indices) {
                 if (nums[i] !in range) {
                     nums[i] = 0
                 }
-
-                if (nums[i] == 1) {
-                    hasOne = true
-                }
             }
 
-            if (!hasOne) {
-                return 1
-            }
-
-            val flag = nums.size + 1
+            val max = nums.size + 1
 
             for (i in nums.indices) {
-                val num = nums[i]
-                when {
-                    num == i + 1 -> {
-                        nums[i] = flag
-                    }
+                val num = nums[i] % max
+                if (num in range) {
+                    nums[num - 1] = nums[num - 1] % max + max
 
-                    num < 0 -> {
-                        when {
-                            nums[0 - num - 1] in range -> {
-                                nums[0 - num - 1] *= -1
-                            }
-
-                            0 - nums[0 - num - 1] in range -> {
-                            }
-
-                            else -> {
-                                nums[0 - num - 1] = flag
-                            }
-                        }
-
-                        nums[i] = flag
-                    }
-
-                    num == flag -> {}
-
-                    num > 0 -> {
-                        when {
-                            nums[num - 1] in range -> {
-                                nums[num - 1] *= -1
-                            }
-
-                            0 - nums[num - 1] in range -> {
-                            }
-
-                            else -> {
-                                nums[num - 1] = flag
-                            }
-                        }
-
-                        nums[i] = 0
-                    }
+                    nums[i] -= num
                 }
             }
 
             for (i in nums.indices) {
-                if (nums[i] != flag) {
+                if (nums[i] != max) {
                     return i + 1
                 }
             }
 
-            return nums.size + 1
+            return max
         }
     }
 

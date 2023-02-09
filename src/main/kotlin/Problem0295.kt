c03835f09d13fd188e409934f0bc9f48d5789f06
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-09 09:30:34 +0800

    problem 0295

---
+++ src/main/kotlin/Problem0295.kt
@@ -0,0 +1,32 @@
+fun main() {
+    class MedianFinder {
+        val nums = arrayListOf<Int>()
+
+        fun addNum(num: Int) {
+            var index = 0
+            while (index < nums.size && num < nums[index]) {
+                index++
+            }
+
+            nums.add(index, num)
+        }
+
+        fun findMedian(): Double {
+            val size = nums.size
+            return if (size % 2 == 1) {
+                nums[size / 2].toDouble()
+            } else {
+                (nums[size / 2 - 1] + nums[size / 2]) / 2.0
+            }
+        }
+    }
+
+    MedianFinder().also {
+        it.addNum(1)
+        it.addNum(2)
+        it.addNum(3)
+
+        println(it.findMedian())
+    }
+}
+

1c49cbffa08264f01fb783c604fa093d6ad35ab5
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-13 10:33:58 +0800

    problem 1234

---
+++ src/main/kotlin/Problem1234.kt
@@ -0,0 +1,96 @@
+fun main() {
+    class Solution {
+        fun balancedString(s: String): Int {
+            fun Char.index(): Int {
+                return when (this) {
+                    'Q' -> 0
+                    'W' -> 1
+                    'E' -> 2
+                    else -> 3
+                }
+            }
+
+            val counts = IntArray(4)
+
+            val countsPerChar = s.length / 4
+
+            s.forEach { c ->
+                counts[c.index()]++
+            }
+
+            repeat(4) {
+                if (counts[it] > countsPerChar) {
+                    counts[it] = counts[it] - countsPerChar
+                } else {
+                    counts[it] = Int.MIN_VALUE
+                }
+            }
+
+            if (counts.all { it == Int.MIN_VALUE }) {
+                return 0
+            }
+
+            if (counts.filter { it != Int.MIN_VALUE }.sum() == 1) {
+                return 1
+            }
+
+            var leftIndex = 0
+            while (counts[s[leftIndex].index()] < 0) {
+                leftIndex++
+            }
+
+            var rightIndex = leftIndex
+
+            counts[s[leftIndex].index()]--
+
+            var result = Int.MAX_VALUE
+            while (rightIndex < s.length) {
+                if (counts.all { it <= 0 }) {
+                    var found = false
+                    found@ while (true) {
+                        if (!found) {
+                            result = (rightIndex - leftIndex + 1).coerceAtMost(result)
+                        }
+
+                        if (counts[s[leftIndex].index()] >= 0) {
+                            if (found) {
+                                break@found
+                            } else {
+                                found = true
+                            }
+                        }
+
+                        counts[s[leftIndex].index()]++
+
+                        leftIndex++
+                    }
+
+                } else {
+                    rightIndex++
+                    found@ while (rightIndex < s.length) {
+                        when (counts[s[rightIndex].index()]) {
+                            Int.MIN_VALUE -> {
+                                rightIndex++
+                            }
+
+                            1 -> {
+                                counts[s[rightIndex].index()]--
+                                break@found
+                            }
+
+                            else -> {
+                                counts[s[rightIndex].index()]--
+                                rightIndex++
+                            }
+                        }
+
+                    }
+                }
+            }
+            return result
+        }
+    }
+
+    println(Solution().balancedString("QQWE"))
+}
+

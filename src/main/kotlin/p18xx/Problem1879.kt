c82bda2c1c8d12f1d4d9a70be9d71713c5c2d766
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-13 17:16:14 +0800

    1879

---
+++ src/main/kotlin/p18xx/Problem1879.kt
@@ -0,0 +1,29 @@
+package p18xx
+
+fun main() {
+    class Solution {
+        fun minimumXORSum(nums1: IntArray, nums2: IntArray): Int {
+            val dp = IntArray(1 shl nums1.size) { Int.MAX_VALUE }
+
+            dp[0] = 0
+
+            for (d in 1 until dp.size) {
+                nums2.forEachIndexed { num2Index, n ->
+                    (1 shl num2Index).takeIf { it and d > 0 }?.also { num2Pos ->
+                        dp[d] = dp[d].coerceAtMost(dp[d xor num2Pos] + (nums1[Integer.bitCount(d) - 1] xor n))
+                    }
+                }
+            }
+
+            return dp.last()
+        }
+    }
+
+    println(
+        Solution().minimumXORSum(
+            intArrayOf(72, 97, 8, 32, 15),
+            intArrayOf(63, 97, 57, 60, 83),
+        )
+    )
+}
+

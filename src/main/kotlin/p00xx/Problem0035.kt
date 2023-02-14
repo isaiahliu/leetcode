19fe55a874d81a804685b9ecd4ae03679415b541
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-14 16:50:43 +0800

    Problem 0035

---
+++ src/main/kotlin/p00xx/Problem0035.kt
@@ -0,0 +1,32 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun searchInsert(nums: IntArray, target: Int): Int {
+            val max = nums.size
+            fun binarySearch(leftIndex: Int, rightIndex: Int): Int {
+                if (leftIndex > rightIndex) {
+                    return max
+                }
+
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                return if (nums[midIndex] >= target) {
+                    midIndex.coerceAtMost(binarySearch(leftIndex, midIndex - 1))
+                } else {
+                    binarySearch(midIndex + 1, rightIndex)
+                }
+            }
+
+            return binarySearch(0, nums.size - 1)
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().searchInsert(intArrayOf(1, 3, 5, 6), 0))
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

729dcba4bfd584dc7261972cc3409eefb43442cb
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-14 16:44:23 +0800

    Problem 0034

---
+++ src/main/kotlin/p00xx/Problem0034.kt
@@ -0,0 +1,72 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun searchRange(nums: IntArray, target: Int): IntArray {
+            val result = intArrayOf(-1, -1)
+
+            fun findLeft(leftIndex: Int, rightIndex: Int) {
+                if (leftIndex > rightIndex) {
+                    return
+                }
+
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                if (nums[midIndex] == target) {
+                    result[0] = midIndex
+                    findLeft(leftIndex, midIndex - 1)
+                } else {
+                    findLeft(midIndex + 1, rightIndex)
+                }
+            }
+
+            fun findRight(leftIndex: Int, rightIndex: Int) {
+                if (leftIndex > rightIndex) {
+                    return
+                }
+
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                if (nums[midIndex] == target) {
+                    result[1] = midIndex
+                    findRight(midIndex + 1, rightIndex)
+                } else {
+                    findRight(leftIndex, midIndex - 1)
+                }
+            }
+
+            fun findBoth(leftIndex: Int, rightIndex: Int) {
+                if (leftIndex > rightIndex) {
+                    return
+                }
+
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                if (nums[midIndex] == target) {
+                    result[0] = midIndex
+                    result[1] = midIndex
+
+                    findLeft(leftIndex, midIndex - 1)
+                    findRight(midIndex + 1, rightIndex)
+                } else if (nums[midIndex] > target) {
+                    findBoth(leftIndex, midIndex - 1)
+                } else {
+                    findBoth(midIndex + 1, rightIndex)
+                }
+            }
+
+            if (nums.isNotEmpty() && target in nums[0]..nums[nums.size - 1]) {
+                findBoth(0, nums.size - 1)
+            }
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().searchRange(intArrayOf(5, 7, 7, 8, 8, 10), 8).joinToString())
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

b6820302c3cccdcac490fed871f96b669d2a01b1
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 14:34:26 +0800

    Problem 0063

---
+++ src/main/kotlin/p00xx/Problem0063.kt
@@ -0,0 +1,44 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun uniquePathsWithObstacles(obstacleGrid: Array<IntArray>): Int {
+            val m = obstacleGrid.size
+            val n = obstacleGrid[0].size
+            val dp = Array(m) { IntArray(n) }
+
+            dp[0][0] = 1 - obstacleGrid[0][0]
+
+            repeat(m) { row ->
+                repeat(n) { column ->
+                    if (obstacleGrid[row][column] == 0) {
+                        dp.getOrNull(row - 1)?.getOrNull(column)?.also {
+                            dp[row][column] += it
+                        }
+
+                        dp.getOrNull(row)?.getOrNull(column - 1)?.also {
+                            dp[row][column] += it
+                        }
+                    }
+                }
+            }
+
+            return dp[m - 1][n - 1]
+        }
+    }
+
+    measureTimeMillis {
+        println(
+            Solution().uniquePathsWithObstacles(
+                arrayOf(
+                    intArrayOf(1, 1, 1),
+                    intArrayOf(1, 1, 1),
+                    intArrayOf(1, 1, 1)
+                )
+            )
+        )
+    }.also { println("Time cost: ${it}ms") }
+}
+

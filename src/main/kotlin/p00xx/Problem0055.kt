1c0983a355f299c87079952839e228027977f3ef
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 11:31:01 +0800

    Problem 0055

---
+++ src/main/kotlin/p00xx/Problem0055.kt
@@ -0,0 +1,33 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canJump(nums: IntArray): Boolean {
+            var startIndex = 0
+            var endIndex = 0
+
+            while (endIndex < nums.size - 1) {
+                val nextStart = endIndex + 1
+                for (i in startIndex..endIndex) {
+                    endIndex = endIndex.coerceAtLeast(i + nums[i])
+                }
+
+                if (endIndex < nextStart) {
+                    return false
+                }
+
+                startIndex = nextStart
+            }
+
+            return true
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().canJump(intArrayOf(1, 2, 3, 4)))
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

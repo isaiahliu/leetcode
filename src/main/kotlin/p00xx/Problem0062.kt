20a6c47f4933997df298b9d131f8cc4b2d88f2d8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 14:15:26 +0800

    Problem 0062

---
+++ src/main/kotlin/p00xx/Problem0062.kt
@@ -0,0 +1,32 @@
+package p00xx
+
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun uniquePaths(m: Int, n: Int): Int {
+            val rightCount = (m - 1).toBigInteger()
+            val downCount = (n - 1).toBigInteger()
+
+            val min = rightCount.min(downCount).toInt()
+
+            var sum = BigInteger.ONE
+
+            repeat(min) {
+                sum *= rightCount + downCount - it.toBigInteger()
+            }
+
+            repeat(min) {
+                sum /= BigInteger.ONE + it.toBigInteger()
+            }
+
+            return sum.toInt()
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().uniquePaths(3, 1))
+    }.also { println("Time cost: ${it}ms") }
+}
+

30ce678d584edd5d7d03f52a33a2a612615293bb
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 10:49:58 +0800

    Problem 0050

---
+++ src/main/kotlin/p00xx/Problem0050.kt
@@ -0,0 +1,41 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun myPow(x: Double, n: Int): Double {
+            if (x == 1.0) {
+                return 1.0
+            }
+
+            if (x == -1.0) {
+                return if (n % 2 == 1) -1.0 else 1.0
+            }
+
+            var result = 1.0
+
+            if (n < 0) {
+                for (i in -1 downTo n) {
+                    result /= x
+
+                    if (result == 0.0) {
+                        break
+                    }
+                }
+            } else {
+                for (i in 0 until n) {
+                    result *= x
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().myPow(2.0, Int.MIN_VALUE))
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

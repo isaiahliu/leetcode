5497d9885506fa7f7f02514fc54bd568c3704475
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 13:48:24 +0800

    Problem 0061

---
+++ src/main/kotlin/p00xx/Problem0061.kt
@@ -0,0 +1,49 @@
+package p00xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    class Solution {
+        fun rotateRight(head: ListNode?, k: Int): ListNode? {
+            if (head == null || k == 0) {
+                return head
+            }
+
+            var t = head
+
+            val stack = LinkedList<ListNode>()
+
+            while (t != null) {
+                stack.push(t)
+                t = t.next
+            }
+
+            val popCount = k % stack.size
+
+            if (popCount == 0) {
+                return head
+            }
+
+            stack.peek().next = head
+
+            var top = head
+            repeat(popCount) {
+                top = stack.pop()
+            }
+
+            stack.peek().next = null
+
+            return top
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().rotateRight(null, 1))
+    }.also { println("Time cost: ${it}ms") }
+}
+

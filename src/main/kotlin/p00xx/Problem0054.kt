bd539547b26143b2c6b936bfcbd1b86663622200
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 11:24:01 +0800

    Problem 0054

---
+++ src/main/kotlin/p00xx/Problem0054.kt
@@ -0,0 +1,87 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun spiralOrder(matrix: Array<IntArray>): List<Int> {
+            if (matrix.isEmpty() || matrix[0].isEmpty()) {
+                return emptyList()
+            }
+
+            val up = 0
+            val right = 1
+            val down = 2
+            val left = 3
+
+            var rowIndex = 0
+            var columnIndex = 0
+
+            var minRowIndex = 0
+            var maxRowIndex = matrix.size - 1
+            var minColumnIndex = 0
+            var maxColumnIndex = matrix[0].size - 1
+
+            val result = arrayListOf(matrix[0][0])
+            var direction = right
+
+            while (minRowIndex <= maxRowIndex && minColumnIndex <= maxColumnIndex) {
+                when (direction % 4) {
+                    up -> {
+                        repeat(rowIndex - minRowIndex) {
+                            rowIndex--
+                            result.add(matrix[rowIndex][columnIndex])
+                        }
+
+                        minColumnIndex++
+                    }
+
+                    right -> {
+                        repeat(maxColumnIndex - columnIndex) {
+                            columnIndex++
+                            result.add(matrix[rowIndex][columnIndex])
+                        }
+
+                        minRowIndex++
+                    }
+
+                    down -> {
+                        repeat(maxRowIndex - rowIndex) {
+                            rowIndex++
+                            result.add(matrix[rowIndex][columnIndex])
+                        }
+
+                        maxColumnIndex--
+                    }
+
+                    left -> {
+                        repeat(columnIndex - minColumnIndex) {
+                            columnIndex--
+                            result.add(matrix[rowIndex][columnIndex])
+                        }
+
+                        maxRowIndex--
+                    }
+                }
+
+                direction++
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        println(
+            Solution().spiralOrder(
+                arrayOf(
+                    intArrayOf(1, 2, 3, 4),
+                    intArrayOf(5, 6, 7, 8),
+                    intArrayOf(9, 10, 11, 12)
+                )
+            )
+        )
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

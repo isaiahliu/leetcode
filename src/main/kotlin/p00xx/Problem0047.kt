4ff52a76a8cc152826366bf2fdd8295d477d0943
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 09:44:24 +0800

    Problem 0047

---
+++ src/main/kotlin/p00xx/Problem0047.kt
@@ -0,0 +1,37 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun permuteUnique(nums: IntArray): List<List<Int>> {
+            val all = (1 shl nums.size) - 1
+
+            val result = arrayListOf<List<Int>>()
+
+            fun walk(used: Int, route: List<Int>) {
+                if (used == all) {
+                    result.add(route)
+                    return
+                }
+
+                val visited = hashSetOf<Int>()
+                repeat(nums.size) {
+                    if (used and (1 shl it) == 0 && visited.add(nums[it])) {
+                        walk(used + (1 shl it), route + nums[it])
+                    }
+                }
+            }
+
+            walk(0, emptyList())
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().permuteUnique(intArrayOf(1, 2, 3)))
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

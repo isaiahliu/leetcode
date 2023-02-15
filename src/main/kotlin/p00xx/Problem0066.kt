49444328c10828e03cf52297b517fd9313ac1724
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-15 14:45:03 +0800

    Problem 0066

---
+++ src/main/kotlin/p00xx/Problem0066.kt
@@ -0,0 +1,31 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun plusOne(digits: IntArray): IntArray {
+            digits[digits.size - 1]++
+            for (i in digits.size - 1 downTo 1) {
+                if (digits[i] > 9) {
+                    digits[i] %= 10
+                    digits[i - 1]++
+                } else {
+                    break
+                }
+            }
+
+            return if (digits[0] > 9) {
+                digits[0] %= 10
+                intArrayOf(1) + digits
+            } else {
+                digits
+            }
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().plusOne(intArrayOf(1, 3, 1)))
+    }.also { println("Time cost: ${it}ms") }
+}
+

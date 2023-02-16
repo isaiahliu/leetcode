aa4a21a2314f3a370a5fa23570ddb5f8e2b7eecd
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 16:47:24 +0800

    Problem 0090

---
+++ src/main/kotlin/p00xx/Problem0090.kt
@@ -0,0 +1,35 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun subsetsWithDup(nums: IntArray): List<List<Int>> {
+            var result = listOf(emptyList<Int>())
+
+            nums.toList().groupingBy { it }.eachCount().forEach { (num, count) ->
+                result = (0..count).map {
+                    val appendList = arrayListOf<Int>()
+                    repeat(it) {
+                        appendList.add(num)
+                    }
+
+                    result.map {
+                        it + appendList
+                    }
+                }.flatten()
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().subsetsWithDup(
+            intArrayOf(4)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

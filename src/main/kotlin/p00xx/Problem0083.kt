3c6631d0697884473bc592156b5a6d8bb2b68eb8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 13:33:26 +0800

    Problem 0083

---
+++ src/main/kotlin/p00xx/Problem0083.kt
@@ -0,0 +1,30 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    class Solution {
+        fun deleteDuplicates(head: ListNode?): ListNode? {
+            var t = head
+
+            while (t != null) {
+                if (t.`val` == t.next?.`val`) {
+                    t.next = t.next?.next
+                } else {
+                    t = t.next
+                }
+            }
+
+            return head
+        }
+    }
+
+    measureTimeMillis {
+        Solution().deleteDuplicates(null)
+    }.also { println("Time cost: ${it}ms") }
+}
+

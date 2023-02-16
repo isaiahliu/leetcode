d7fcdb19770abf6d1c11295483b13b18cc269c10
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 14:49:40 +0800

    Problem 0085

---
+++ src/main/kotlin/p00xx/Problem0085.kt
@@ -0,0 +1,77 @@
+package p00xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maximalRectangle(matrix: Array<CharArray>): Int {
+            var max = 0
+
+            val heights = IntArray(matrix[0].size)
+
+            matrix.forEach {
+                it.forEachIndexed { index, c ->
+                    if (c == '0') {
+                        heights[index] = 0
+                    } else {
+                        heights[index]++
+                    }
+                }
+
+                max = max.coerceAtLeast(largestRectangleArea(heights))
+            }
+
+            return max
+        }
+
+        fun largestRectangleArea(heights: IntArray): Int {
+            var max = 0
+
+            val stack = LinkedList<IntArray>()
+
+            fun popStack(lowerLimit: Int): Int {
+                var count = 0
+                while (stack.isNotEmpty() && stack.peek()[0] > lowerLimit) {
+                    val top = stack.pop()
+                    count += top[1]
+
+                    max = max.coerceAtLeast(top[0] * count)
+                }
+
+                return count
+            }
+            heights.forEach {
+                if (stack.isEmpty()) {
+                    stack.push(intArrayOf(it, 1))
+                } else {
+                    val top = stack.peek()
+                    when {
+                        it < top[0] -> {
+                            stack.push(intArrayOf(it, popStack(it) + 1))
+                        }
+
+                        it > top[0] -> {
+                            stack.push(intArrayOf(it, 1))
+                        }
+
+                        else -> {
+                            top[1]++
+                        }
+                    }
+                }
+            }
+
+            popStack(-1)
+
+            return max
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maximalRectangle(emptyArray()).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

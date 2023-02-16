1ee032aa812dc9954716279738469f3c46da5c86
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 11:19:31 +0800

    Problem 0079

---
+++ src/main/kotlin/p00xx/Problem0079.kt
@@ -0,0 +1,44 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun exist(board: Array<CharArray>, word: String): Boolean {
+            fun walk(pos: Pair<Int, Int>, matchCount: Int, route: Set<Pair<Int, Int>>): Boolean {
+                if (matchCount != route.size) {
+                    return false
+                }
+
+                if (matchCount == word.length) {
+                    return true
+                }
+
+                return arrayOf(
+                    pos.first - 1 to pos.second,
+                    pos.first to pos.second - 1,
+                    pos.first + 1 to pos.second,
+                    pos.first to pos.second + 1
+                ).filter { (r, c) -> board.getOrNull(r)?.getOrNull(c) == word[matchCount] }.any {
+                    walk(it, matchCount + 1, route + it)
+                }
+            }
+            board.forEachIndexed { rowIndex, row ->
+                row.forEachIndexed { columnIndex, c ->
+                    if (c == word[0]) {
+                        if (walk(rowIndex to columnIndex, 1, setOf(rowIndex to columnIndex))) {
+                            return true
+                        }
+                    }
+                }
+            }
+
+            return false
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().exist(emptyArray(), ""))
+    }.also { println("Time cost: ${it}ms") }
+}
+

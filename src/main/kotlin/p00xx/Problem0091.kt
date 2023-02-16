b77946c6178431814a298d12e59b1d43483e1471
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 16:59:51 +0800

    Problem 0091

---
+++ src/main/kotlin/p00xx/Problem0091.kt
@@ -0,0 +1,39 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numDecodings(s: String): Int {
+            val dp = IntArray(s.length)
+
+            if (s[s.length - 1] > '0') {
+                dp[s.length - 1] = 1
+            }
+
+            for (i in dp.size - 2 downTo 0) {
+                val n1 = s[i] - '0'
+                val n2 = s[i + 1] - '0'
+
+                if (n1 > 0) {
+                    dp[i] += dp[i + 1]
+
+                    if (n1 * 10 + n2 <= 26) {
+                        dp[i] += dp.getOrNull(i + 2) ?: 1
+                    }
+                }
+            }
+
+            return dp[0]
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numDecodings(
+            "12"
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

19be77dfff41ee2188900afdea3feba2a6f28153
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 18:16:35 +0800

    Problem 0096

---
+++ src/main/kotlin/p00xx/Problem0096.kt
@@ -0,0 +1,29 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numTrees(n: Int): Int {
+            val dp = IntArray(n + 1)
+            dp[0] = 1
+            dp[1] = 1
+            for (i in 2 until n + 1) {
+                for (j in 0 until i) {
+                    dp[i] += dp[j] * dp[i - j - 1]
+                }
+            }
+
+            return dp[n]
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numTrees(
+            3
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

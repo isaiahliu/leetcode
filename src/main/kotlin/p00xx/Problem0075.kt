84f84419c7d0fa054f4f66794cb480e10ca736a9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 09:58:58 +0800

    Problem 0075

---
+++ src/main/kotlin/p00xx/Problem0075.kt
@@ -0,0 +1,45 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun sortColors(nums: IntArray): Unit {
+            var leftIndex = 0
+            var rightIndex = nums.size - 1
+
+            var cursor = 0
+            while (cursor <= rightIndex) {
+                when (nums[cursor]) {
+                    0 -> {
+                        val t = nums[leftIndex]
+                        nums[cursor] = t
+                        nums[leftIndex] = 0
+
+                        leftIndex++
+                        cursor++
+                    }
+
+                    2 -> {
+                        val t = nums[rightIndex]
+                        nums[cursor] = t
+                        nums[rightIndex] = 2
+
+                        rightIndex--
+                    }
+
+                    else -> {
+                        cursor++
+                    }
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        println(
+            Solution().sortColors(intArrayOf(0, 2, 1))
+        )
+    }.also { println("Time cost: ${it}ms") }
+}
+

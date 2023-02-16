9cee70f96790f7cd5577582b8119e45a03784715
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-16 11:57:11 +0800

    Problem 0081

---
+++ src/main/kotlin/p00xx/Problem0081.kt
@@ -0,0 +1,68 @@
+package p00xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun search(nums: IntArray, target: Int): Boolean {
+            val splitNum = nums[0]
+
+            if (splitNum == target) {
+                return true
+            }
+
+            fun findMaxNumIndex(leftIndex: Int, rightIndex: Int): Boolean {
+                if (leftIndex > rightIndex) {
+                    return false
+                }
+
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+                return when (val midNum = nums[midIndex]) {
+                    target -> return true
+                    else -> when {
+                        midNum > splitNum -> {
+                            when (target) {
+                                in (splitNum + 1) until midNum -> {
+                                    //find left
+                                    findMaxNumIndex(leftIndex, midIndex - 1)
+                                }
+
+                                else -> {
+                                    //find right
+                                    findMaxNumIndex(midIndex + 1, rightIndex)
+                                }
+                            }
+                        }
+
+                        midNum < splitNum -> {
+                            when (target) {
+                                in midNum + 1 until splitNum -> {
+                                    //find right
+                                    findMaxNumIndex(midIndex + 1, rightIndex)
+                                }
+
+                                else -> {
+                                    //find left
+                                    findMaxNumIndex(leftIndex, midIndex - 1)
+                                }
+                            }
+                        }
+
+                        else -> {
+                            findMaxNumIndex(leftIndex, midIndex - 1) || findMaxNumIndex(midIndex + 1, rightIndex)
+                        }
+                    }
+                }
+            }
+
+            return findMaxNumIndex(0, nums.size - 1)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().search(intArrayOf(1), 0).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

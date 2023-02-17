db7c3346c5ca26014e957a5b1cb9bc3071ef62e4
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-17 16:39:22 +0800

    Problem 0105

---
+++ src/main/kotlin/p01xx/Problem0105.kt
@@ -0,0 +1,46 @@
+package p01xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+class Solution {
+    fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
+        fun build(preRange: IntRange, inRange: IntRange): TreeNode? {
+            if (preRange.first > preRange.last) {
+                return null
+            }
+
+            val root = TreeNode(preorder[preRange.first])
+
+            var rootInOrderIndex = inRange.first
+            while (inorder[rootInOrderIndex] != root.`val`) {
+                rootInOrderIndex++
+            }
+
+            root.left = build(
+                preRange.first + 1..preRange.first + rootInOrderIndex - inRange.first,
+                inRange.first until rootInOrderIndex
+            )
+
+            root.right = build(
+                preRange.first + rootInOrderIndex - inRange.first + 1..preRange.last,
+                rootInOrderIndex + 1..inRange.last
+            )
+
+            return root
+        }
+
+        return build(preorder.indices, inorder.indices)
+    }
+}
+
+    measureTimeMillis {
+        Solution().buildTree(
+            intArrayOf(1), intArrayOf(1)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

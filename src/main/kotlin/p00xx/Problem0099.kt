befcfc251871f94acd6f8d0d9715a590a818df22
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-17 13:38:19 +0800

    Problem 0099

---
+++ src/main/kotlin/p00xx/Problem0099.kt
@@ -0,0 +1,59 @@
+package p00xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun recoverTree(root: TreeNode?): Unit {
+            var leftNode: TreeNode? = null
+            var rightNode: TreeNode? = null
+
+            var previous: TreeNode? = null
+
+            fun TreeNode.visit() {
+                previous?.also {
+                    when {
+                        leftNode == null && it.`val` > this.`val` -> {
+                            leftNode = previous
+                        }
+
+                        rightNode == null && leftNode?.takeIf { this.`val` > it.`val` } != null -> {
+                            rightNode = previous
+                        }
+                    }
+                }
+                previous = this
+            }
+
+            fun TreeNode.walk() {
+                if (rightNode != null) {
+                    return
+                }
+
+                left?.walk()
+                visit()
+                right?.walk()
+            }
+
+            root?.walk()
+
+            if (rightNode == null) {
+                rightNode = previous
+            }
+
+            val t = leftNode?.`val`
+            leftNode?.`val` = rightNode?.`val` ?: 0
+            rightNode?.`val` = t ?: 0
+        }
+    }
+
+    measureTimeMillis {
+        Solution().recoverTree(
+            TreeNode(68, TreeNode(41))
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

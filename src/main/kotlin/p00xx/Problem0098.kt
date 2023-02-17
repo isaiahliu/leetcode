393f8f74c7ab462eadde4527c76437055e31ed16
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-17 11:25:14 +0800

    Problem 0098

---
+++ src/main/kotlin/p00xx/Problem0098.kt
@@ -0,0 +1,37 @@
+package p00xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isValidBST(root: TreeNode?): Boolean {
+            fun TreeNode?.isValid(lowerLimit: Int? = null, upperLimit: Int? = null): Boolean {
+                if (this == null) {
+                    return true
+                }
+
+                if (lowerLimit != null && `val` <= lowerLimit) {
+                    return false
+                }
+
+                if (upperLimit != null && `val` >= upperLimit) {
+                    return false
+                }
+
+                return left.isValid(lowerLimit, `val`) && right.isValid(`val`, upperLimit)
+            }
+
+            return root.isValid()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isValidBST(
+            TreeNode(5, TreeNode(4, TreeNode(6)))
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

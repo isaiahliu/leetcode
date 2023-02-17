94b964d5bb94457a55751227fd5f6de7d1d523cb
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-17 17:03:28 +0800

    Problem 0108

---
+++ src/main/kotlin/p01xx/Problem0108.kt
@@ -0,0 +1,36 @@
+package p01xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun sortedArrayToBST(nums: IntArray): TreeNode? {
+            fun build(leftIndex: Int, rightIndex: Int): TreeNode? {
+                if (leftIndex > rightIndex) {
+                    return null
+                }
+
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                val root = TreeNode(nums[midIndex])
+
+                root.left = build(leftIndex, midIndex - 1)
+                root.right = build(midIndex + 1, rightIndex)
+
+                return root
+            }
+
+            return build(0, nums.size - 1)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().sortedArrayToBST(
+            intArrayOf(1)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

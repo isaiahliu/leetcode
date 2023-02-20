9ac3dd561e2026b3c25d136423e597edee2ec9d0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-20 16:05:29 +0800

    Problem 0129

---
+++ src/main/kotlin/p01xx/Problem0129.kt
@@ -0,0 +1,35 @@
+package p01xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun sumNumbers(root: TreeNode?): Int {
+            var result = 0
+            fun TreeNode.walk(base: Int = 0) {
+                val num = base + `val`
+
+                if (left == null && right == null) {
+                    result += num
+                } else {
+                    left?.walk(num * 10)
+                    right?.walk(num * 10)
+                }
+            }
+
+            root?.walk()
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().sumNumbers(
+            null
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

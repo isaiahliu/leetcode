c2d1aa7d6fab397156fb97f3117d1f2c2d5f4e65
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-20 18:26:04 +0800

    Problem 0134

---
+++ src/main/kotlin/p01xx/Problem0134.kt
@@ -0,0 +1,44 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+            var startIndex = 0
+
+            while (startIndex < gas.size && gas[startIndex] - cost[startIndex] < 0) {
+                startIndex++
+            }
+
+            var sum = 0
+            var endIndex = startIndex
+            while (startIndex < gas.size && endIndex - startIndex < gas.size) {
+                sum += gas[endIndex % gas.size] - cost[endIndex % gas.size]
+
+                if (sum < 0) {
+                    while (startIndex < gas.size && sum < 0) {
+                        sum -= gas[startIndex] - cost[startIndex]
+                        startIndex++
+                    }
+                }
+                endIndex++
+            }
+
+            return if (startIndex < gas.size) {
+                startIndex
+            } else {
+                -1
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().canCompleteCircuit(
+            intArrayOf(5, 1, 2, 3, 4), intArrayOf(4, 4, 1, 5, 1)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

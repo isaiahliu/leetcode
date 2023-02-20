93f62dcd5e4ae3fb22c2837efbff3210a83dc0ae
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-20 11:27:42 +0800

    Problem 0122

---
+++ src/main/kotlin/p01xx/Problem0122.kt
@@ -0,0 +1,29 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxProfit(prices: IntArray): Int {
+            var result = 0
+            var min = prices[0]
+            (prices + 0).forEachIndexed { index, num ->
+                if (index > 0) {
+                    if (num < prices[index - 1]) {
+                        result += prices[index - 1] - min
+                        min = num
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxProfit(intArrayOf()).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

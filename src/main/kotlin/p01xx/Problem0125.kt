958b13b1d15b0c907540c53ec33686802fc32995
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-20 14:13:12 +0800

    Problem 0125

---
+++ src/main/kotlin/p01xx/Problem0125.kt
@@ -0,0 +1,21 @@
+package p01xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isPalindrome(s: String): Boolean {
+            val newStr = s.lowercase(Locale.getDefault()).filter { it in 'a'..'z' || it in '0'..'9' }
+
+            return newStr == newStr.reversed()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isPalindrome("").also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

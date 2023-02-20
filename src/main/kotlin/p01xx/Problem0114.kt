c66e5b0a024aab5145c8a588bdef1acf662a125b
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-20 09:27:53 +0800

    Problem 0114

---
+++ src/main/kotlin/p01xx/Problem0114.kt
@@ -0,0 +1,40 @@
+package p01xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun flatten(root: TreeNode?): Unit {
+            fun TreeNode.walk(): TreeNode {
+                val leftNode = left
+                val rightNode = right
+
+                var leftTail = this
+                if (leftNode != null) {
+                    this.left = null
+                    this.right = leftNode
+                    leftTail = leftNode.walk()
+                }
+
+                var rightTail = leftTail
+                if (rightNode != null) {
+                    leftTail.right = rightNode
+                    rightTail = rightNode.walk()
+                }
+
+                return rightTail
+            }
+            root?.walk()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().flatten(
+            null
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

d314ed3f320c05741136a59159edfb8314d9a65d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-21 18:38:42 +0800

    Problem 0148

---
+++ src/main/kotlin/p01xx/Problem0148.kt
@@ -0,0 +1,48 @@
+package p01xx
+
+import util.ListNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun sortList(head: ListNode?): ListNode? {
+            head?.next ?: return head
+
+            val root = ListNode(Int.MIN_VALUE)
+            root.next = head
+
+            var t = head.next
+            head.next = null
+
+            var previousT = root
+            while (t != null) {
+                val nextT = t.next
+
+                var first = if (t.`val` >= previousT.`val`) previousT else root
+                var second = first.next
+
+                while (second != null && t.`val` >= second.`val`) {
+                    first = second
+                    second = second.next
+                }
+
+                first.next = t
+                t.next = second
+
+                previousT = t
+
+                t = nextT
+            }
+
+            return root.next
+        }
+    }
+    measureTimeMillis {
+        Solution().sortList(
+            null
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

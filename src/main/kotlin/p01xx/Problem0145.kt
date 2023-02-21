f28b5cedc1c40094c5c5eeaa9cf37b3476c23c6c
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-21 18:12:32 +0800

    Problem 0145

---
+++ src/main/kotlin/p01xx/Problem0145.kt
@@ -0,0 +1,31 @@
+package p01xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun postorderTraversal(root: TreeNode?): List<Int> {
+            val result = arrayListOf<Int>()
+
+            fun TreeNode.walk() {
+                left?.walk()
+                right?.walk()
+                result.add(`val`)
+            }
+
+            root?.walk()
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().postorderTraversal(
+            null
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

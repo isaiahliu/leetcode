202a2ea814c8e6cd9c4c53d1aea87476a67f954a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-21 10:49:57 +0800

    Problem 0135

---
+++ src/main/kotlin/p01xx/Problem0135.kt
@@ -0,0 +1,56 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun candy(ratings: IntArray): Int {
+            var current = Int.MAX_VALUE
+            var size = 1
+
+            val incArr = IntArray(ratings.size) {
+                val r = ratings[it]
+
+                if (r > current) {
+                    size++
+                } else {
+                    size = 1
+                }
+
+                current = r
+
+                size
+            }
+
+            current = Int.MAX_VALUE
+            size = 1
+
+            var sum = 0
+
+            for (i in ratings.size - 1 downTo 0) {
+                val r = ratings[i]
+
+                if (r > current) {
+                    size++
+                } else {
+                    size = 1
+                }
+
+                current = r
+
+                sum += size.coerceAtLeast(incArr[i])
+            }
+
+            return sum
+        }
+    }
+
+    measureTimeMillis {
+        Solution().candy(
+            intArrayOf(1, 2, 2)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

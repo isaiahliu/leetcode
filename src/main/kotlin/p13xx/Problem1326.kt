39dd5b4b57d537a53d5f9b74b79bd6a6aa339da3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-21 09:38:48 +0800

    Problem 1326

---
+++ src/main/kotlin/p13xx/Problem1326.kt
@@ -0,0 +1,40 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minTaps(n: Int, ranges: IntArray): Int {
+            val sortedRanges = ranges.mapIndexed { index, i ->
+                index - i..index + i
+            }.filter { it.first < it.last }.sortedBy { it.first }
+
+            var result = 0
+            var rangeIndex = 0
+            var endIndex = 0
+            while (endIndex < n) {
+                var newEndIndex = endIndex
+                while (rangeIndex < sortedRanges.size && sortedRanges[rangeIndex].first <= endIndex && newEndIndex < n) {
+                    newEndIndex = newEndIndex.coerceAtLeast(sortedRanges[rangeIndex++].last)
+                }
+
+                result++
+                if (newEndIndex <= endIndex) {
+                    return -1
+                }
+                endIndex = newEndIndex
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minTaps(
+            5, intArrayOf(3, 4, 1, 1, 0, 0)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

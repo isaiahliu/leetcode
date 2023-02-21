e5a03de31f2b5d72f092e7792f975d75d91aa8ac
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-21 18:20:22 +0800

    Problem 0146

---
+++ src/main/kotlin/p01xx/Problem0146.kt
@@ -0,0 +1,35 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class LRUCache(private val capacity: Int) {
+        val cache = LinkedHashMap<Int, Int>()
+
+        fun get(key: Int): Int {
+            return cache[key]?.also {
+                cache.remove(key)
+                cache[key] = it
+            } ?: -1
+        }
+
+        fun put(key: Int, value: Int) {
+            cache.remove(key)
+
+            cache[key] = value
+
+            if (cache.size > capacity) {
+                cache.remove(cache.keys.first())
+            }
+        }
+    }
+
+    measureTimeMillis {
+        LRUCache(1).get(
+            1
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

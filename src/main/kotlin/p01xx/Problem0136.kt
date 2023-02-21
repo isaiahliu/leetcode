7b46253bc503fbf0f07d47e54f2a8dc980d37fe1
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-21 10:51:28 +0800

    Problem 0136

---
+++ src/main/kotlin/p01xx/Problem0136.kt
@@ -0,0 +1,20 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun singleNumber(nums: IntArray): Int {
+            return nums.reduce { a, b -> a xor b }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().singleNumber(
+            intArrayOf(1, 2, 2)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

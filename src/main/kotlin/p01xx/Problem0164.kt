b7b453d70721bc5e091243826647043a77f28065
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 16:25:21 +0800

    Problem 0164

---
+++ src/main/kotlin/p01xx/Problem0164.kt
@@ -0,0 +1,55 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maximumGap(nums: IntArray): Int {
+            if (nums.size == 1) {
+                return 0
+            }
+
+            val max = nums.max()
+            val min = nums.min()
+            val bucketSize = ((max - min) / (nums.size - 1)).coerceAtLeast(1)
+
+            val bucket = Array((max - min) / bucketSize + 1) {
+                intArrayOf(-1, -1)
+            }
+
+            nums.forEach { num ->
+                bucket[(num - min) / bucketSize].also {
+                    if (it[0] == -1) {
+                        it[0] = num
+                        it[1] = num
+                    } else {
+                        it[0] = it[0].coerceAtMost(num)
+                        it[1] = it[1].coerceAtLeast(num)
+                    }
+                }
+            }
+
+            var result = bucket[0][1] - bucket[0][0]
+
+            var bucketMax = bucket[0][1]
+            for (i in 1 until bucket.size) {
+                if (bucketMax > -1) {
+                    result = result.coerceAtLeast(bucket[i][0] - bucketMax)
+                }
+
+                bucketMax = bucketMax.coerceAtLeast(bucket[i][1])
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maximumGap(
+            intArrayOf(1, 1, 1, 1)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

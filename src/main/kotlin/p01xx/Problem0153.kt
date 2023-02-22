f4f36459a18815376ef694aecd96323fe222a70f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 14:47:17 +0800

    Problem 0153

---
+++ src/main/kotlin/p01xx/Problem0153.kt
@@ -0,0 +1,47 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findMin(nums: IntArray): Int {
+            var min = nums[0]
+            if (min <= nums[nums.size - 1]) {
+                return min
+            } else {
+                fun findMinIndex(leftIndex: Int, rightIndex: Int) {
+                    if (leftIndex > rightIndex) {
+                        return
+                    }
+
+                    if (leftIndex == rightIndex) {
+                        min = min.coerceAtMost(nums[leftIndex])
+                        return
+                    }
+
+                    val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                    min = min.coerceAtMost(nums[midIndex])
+                    if (nums[midIndex] < nums[0]) {
+                        findMinIndex(leftIndex, midIndex - 1)
+                    } else {
+                        findMinIndex(midIndex + 1, rightIndex)
+                    }
+                }
+
+                findMinIndex(0, nums.size - 1)
+
+                return min
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findMin(
+            intArrayOf(3, 4, 5, 1, 2)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

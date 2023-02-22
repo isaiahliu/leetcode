a10d9601172bfe73fb00874af734c580c49cd709
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 16:29:49 +0800

    Problem 0165

---
+++ src/main/kotlin/p01xx/Problem0165.kt
@@ -0,0 +1,38 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun compareVersion(version1: String, version2: String): Int {
+            val v1 = version1.split(".").map { it.toInt() }
+            val v2 = version2.split(".").map { it.toInt() }
+
+            for (i in 0 until v1.size.coerceAtLeast(v2.size)) {
+                val t1 = v1.getOrElse(i) { 0 }
+                val t2 = v2.getOrElse(i) { 0 }
+
+                when {
+                    t1 < t2 -> {
+                        return -1
+                    }
+
+                    t1 > t2 -> {
+                        return 1
+                    }
+                }
+            }
+
+            return 0
+        }
+    }
+
+    measureTimeMillis {
+        Solution().compareVersion(
+            "", ""
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

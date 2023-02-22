1510a52f22f14e4a00c2c7a6cb0290da014d2efd
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 16:49:00 +0800

    Problem 0166

---
+++ src/main/kotlin/p01xx/Problem0166.kt
@@ -0,0 +1,58 @@
+package p01xx
+
+import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun fractionToDecimal(numerator: Int, denominator: Int): String {
+            val numLong = numerator.toLong().absoluteValue
+            val denLong = denominator.toLong().absoluteValue
+
+            val neg = numerator > 0 && denominator < 0 || numerator < 0 && denominator > 0
+
+            var t = numLong % denLong
+
+            var integerPart = (numLong / denLong).absoluteValue.toString()
+
+            if (t == 0L) {
+                return if (integerPart == "0") {
+                    integerPart
+                } else {
+                    (if (neg) "-" else "") + integerPart
+                }
+            }
+
+            integerPart = (if (neg) "-" else "") + integerPart
+
+            val decimalPart = StringBuilder()
+
+            val cache = hashMapOf<Long, Int>()
+            var pos = 0
+            while (t != 0L && t !in cache) {
+                cache[t] = pos++
+
+                t *= 10
+                decimalPart.append((t / denLong).absoluteValue)
+
+                t %= denLong
+            }
+
+            return if (t > 0) {
+                val p = cache[t] ?: 0
+                "${integerPart}.${decimalPart.substring(0, p)}(${decimalPart.substring(p)})"
+            } else {
+                "${integerPart}.$decimalPart"
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().fractionToDecimal(
+            Int.MIN_VALUE, -1
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

3276e4a61a17589ad0d8524fa64be36d688f889d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 11:47:19 +0800

    Problem 1140

---
+++ src/main/kotlin/p11xx/Problem1140.kt
@@ -0,0 +1,35 @@
+package p11xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun stoneGameII(piles: IntArray): Int {
+            if (piles.size == 1) {
+                return piles[0]
+            }
+
+            val dp = Array(piles.size + 1) { IntArray(piles.size) }
+
+            var sum = 0
+
+            for (index in dp.size - 2 downTo 0) {
+                sum += piles[index]
+
+                for (m in 1 until piles.size) {
+                    for (c in index + 1..(index + m * 2).coerceAtMost(dp.size - 1)) {
+                        dp[index][m] = dp[index][m].coerceAtLeast(
+                            sum - dp[c][m.coerceAtLeast(c - index).coerceAtMost(piles.size - 1)]
+                        )
+                    }
+                }
+            }
+
+            return dp[0][1]
+        }
+    }
+
+    measureTimeMillis {
+        println(Solution().stoneGameII(intArrayOf(1)))
+    }.also { println("Time cost: ${it}ms") }
+}

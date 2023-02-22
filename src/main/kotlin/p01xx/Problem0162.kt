628fb970c9802c0c5220ec59d21f3670cf040bb0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 15:52:26 +0800

    Problem 0162

---
+++ src/main/kotlin/p01xx/Problem0162.kt
@@ -0,0 +1,33 @@
+package p01xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findPeakElement(nums: IntArray): Int {
+            var leftIndex = 0
+            var rightIndex = nums.size - 1
+
+            while (leftIndex < rightIndex) {
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                if (nums[midIndex] < nums[midIndex + 1]) {
+                    leftIndex = midIndex + 1
+                } else {
+                    rightIndex = midIndex
+                }
+            }
+
+            return leftIndex
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findPeakElement(
+            intArrayOf(1)
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

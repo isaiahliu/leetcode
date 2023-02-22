a8671cdd151d2aef8d4cc56b978414da527dadbb
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 14:10:23 +0800

    Problem 0150

---
+++ src/main/kotlin/p01xx/Problem0150.kt
@@ -0,0 +1,52 @@
+package p01xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun evalRPN(tokens: Array<String>): Int {
+            val stack = LinkedList<Int>()
+            tokens.forEach {
+                val num = it.toIntOrNull()
+
+                val calculator: (Int, Int) -> Int = when (it) {
+                    "+" -> {
+                        { b, a -> a + b }
+                    }
+
+                    "-" -> {
+                        { b, a -> a - b }
+                    }
+
+                    "*" -> {
+                        { b, a -> a * b }
+                    }
+
+                    "/" -> {
+                        { b, a -> a / b }
+                    }
+
+                    else -> {
+                        stack.push(it.toInt())
+
+                        return@forEach
+                    }
+                }
+
+                stack.push(calculator(stack.pop(), stack.pop()))
+            }
+
+            return stack.pop()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().evalRPN(
+            emptyArray()
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

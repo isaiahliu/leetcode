2166d0593dad25493a5269a06781b9dd64aaf47f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-22 15:07:52 +0800

    Problem 0155

---
+++ src/main/kotlin/p01xx/Problem0155.kt
@@ -0,0 +1,65 @@
+package p01xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class MinStack {
+        val stack = LinkedList<Int>()
+
+        val numCounts = hashMapOf<Int, Int>()
+
+        var minNum = 0
+        var minCount = 0
+
+        fun push(`val`: Int) {
+            if (`val` == minNum) {
+                minCount++
+            } else if (`val` < minNum || minCount == 0) {
+                minNum = `val`
+                minCount++
+            }
+
+            numCounts[`val`] = (numCounts[`val`] ?: 0) + 1
+
+            stack.push(`val`)
+        }
+
+        fun pop() {
+            val top = stack.pop()
+
+            if (numCounts[top] == 1) {
+                numCounts.remove(top)
+
+                minNum = numCounts.keys.minOrNull() ?: 0
+                minCount = numCounts[minNum] ?: 0
+            } else {
+                numCounts[top] = (numCounts[top] ?: 0) - 1
+            }
+        }
+
+        fun top(): Int {
+            return stack.peek()
+        }
+
+        fun getMin(): Int {
+            return minNum
+        }
+    }
+
+    measureTimeMillis {
+        val stack = MinStack()
+
+        stack.push(Int.MAX_VALUE - 1)
+        stack.push(Int.MAX_VALUE - 1)
+        stack.push(Int.MAX_VALUE)
+        stack.top()
+        stack.pop()
+        stack.getMin()
+        stack.pop()
+        stack.getMin()
+        stack.pop()
+        stack.push(Int.MAX_VALUE)
+    }.also { println("Time cost: ${it}ms") }
+}
+

61afae19cd0a2f63d462ca003087a8c9912136c8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-23 14:21:33 +0800

    Problem 0199

---
+++ src/main/kotlin/p01xx/Problem0199.kt
@@ -0,0 +1,30 @@
+package p01xx
+
+import util.TreeNode
+
+fun main() {
+    class Solution {
+        fun rightSideView(root: TreeNode?): List<Int> {
+            val map = hashMapOf<Int, Int>()
+
+            fun TreeNode.walk(depth: Int = 0) {
+                if (depth >= 0) {
+                    map[depth] = `val`
+                }
+
+                left?.walk(depth + 1)
+                right?.walk(depth + 1)
+            }
+
+            root?.walk()
+
+            return map.entries.sortedBy { it.key }.map { it.value }
+        }
+    }
+    println(
+        Solution().rightSideView(
+            null
+        )
+    )
+}
+

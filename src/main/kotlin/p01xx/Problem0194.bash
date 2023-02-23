78bc0a8f4ca8b2e3d5556a862c92f1f7a851ee9f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-23 13:57:00 +0800

    Problem 0194

---
+++ src/main/kotlin/p01xx/Problem0194.bash
@@ -0,0 +1,14 @@
+awk '{
+  for (i=1; i<=NF; i++){
+    if(NR==1){
+      res[i]=$i;
+    } else {
+      res[i]=res[i] " " $i
+    }
+  }
+}
+END{
+  for (i=1; i<=NF; i++){
+		print res[i]
+	}
+}' file.txt

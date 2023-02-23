bc06d76a2bfa3ee3038f9b4dd02ebbe5aefc2af8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-23 14:41:19 +0800

    Problem 0201

---
+++ src/main/kotlin/p02xx/Problem0201.kt
@@ -0,0 +1,37 @@
+package p02xx
+
+fun main() {
+    class Solution {
+        fun rangeBitwiseAnd(left: Int, right: Int): Int {
+            if (left == right) {
+                return left
+            }
+
+            var l = left
+            var r = right
+
+            var result = 0
+            var h1 = Integer.highestOneBit(l)
+            var h2 = Integer.highestOneBit(r)
+
+            while (h1 == h2) {
+                result += h1
+
+                l -= h1
+                r -= h2
+
+                h1 = Integer.highestOneBit(l)
+                h2 = Integer.highestOneBit(r)
+            }
+
+            return result
+        }
+    }
+
+    println(
+        Solution().rangeBitwiseAnd(
+            1, 2
+        )
+    )
+}
+

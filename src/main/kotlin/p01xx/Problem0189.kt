d59f31d9e72855804ca19e1831ce46443b6090ff
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-23 11:57:50 +0800

    Problem 0189

---
+++ src/main/kotlin/p01xx/Problem0189.kt
@@ -0,0 +1,41 @@
+package p01xx
+
+fun main() {
+    class Solution {
+        fun rotate(nums: IntArray, k: Int): Unit {
+            val numSize = nums.size
+
+            val k2 = k % numSize
+            if (k2 == 0) {
+                return
+            }
+
+            if (numSize <= 1) {
+                return
+            }
+
+            val gcd = numSize.toBigInteger().gcd(k2.toBigInteger()).toInt()
+
+            repeat(gcd) { step ->
+                val tNum = nums[step]
+
+                var t = step
+                repeat(numSize / gcd - 1) {
+                    val nextT = (t - k2 + numSize) % numSize
+                    nums[t] = nums[nextT]
+
+                    t = nextT
+                }
+
+                nums[t] = tNum
+            }
+        }
+    }
+
+    println(
+        Solution().rotate(
+            intArrayOf(1, 2), 3
+        )
+    )
+}
+

aeb544118851325de68dfce6e728a75ddb1f4978
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-23 18:15:35 +0800

    Problem 0213

---
+++ src/main/kotlin/p02xx/Problem0213.kt
@@ -0,0 +1,36 @@
+package p02xx
+
+fun main() {
+    class Solution {
+        fun rob(nums: IntArray): Int {
+            if (nums.size <= 2) {
+                return nums.max()
+            }
+
+            //steal 0
+            val dp1 = IntArray(nums.size)
+            dp1[0] = nums[0]
+            dp1[1] = nums[0]
+            for (i in 2 until nums.size - 1) {
+                dp1[i] = dp1[i - 1].coerceAtLeast(dp1[i - 2] + nums[i])
+            }
+
+            //not steal 0
+            val dp2 = IntArray(nums.size)
+            dp2[1] = nums[1]
+
+            for (i in 2 until nums.size) {
+                dp2[i] = dp2[i - 1].coerceAtLeast(dp2[i - 2] + nums[i])
+            }
+
+            return dp1[nums.size - 2].coerceAtLeast(dp2[nums.size - 1])
+        }
+    }
+
+    println(
+        Solution().rob(
+            intArrayOf(4, 1, 2, 7, 5, 3, 1)
+        )
+    )
+}
+

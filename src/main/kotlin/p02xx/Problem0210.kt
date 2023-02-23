7990e96b348a563558283c14df38e0bd8af3209a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-23 16:37:22 +0800

    Problem 0210

---
+++ src/main/kotlin/p02xx/Problem0210.kt
@@ -0,0 +1,41 @@
+package p02xx
+
+fun main() {
+    class Solution {
+        fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+            val result = arrayListOf<Int>()
+
+            val dependencies = hashMapOf<Int, MutableSet<Int>>()
+            repeat(numCourses) {
+                dependencies[it] = hashSetOf()
+            }
+
+            prerequisites.forEach { (c1, c2) ->
+                dependencies[c1]?.add(c2)
+            }
+
+            while (true) {
+                val finishCourse = dependencies.entries.firstOrNull { it.value.isEmpty() } ?: break
+                dependencies.remove(finishCourse.key)
+                result.add(finishCourse.key)
+
+                dependencies.forEach {
+                    it.value.remove(finishCourse.key)
+                }
+            }
+
+            return if (dependencies.isEmpty()) {
+                result.toIntArray()
+            } else {
+                intArrayOf()
+            }
+        }
+    }
+
+    println(
+        Solution().findOrder(
+            7, emptyArray()
+        )
+    )
+}
+

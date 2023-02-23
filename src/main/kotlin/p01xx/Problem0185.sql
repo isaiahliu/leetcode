b21e7636ae93113455bcb995d2e8af17d165c689
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-23 09:47:33 +0800

    Problem 0184

---
+++ src/main/kotlin/p01xx/Problem0185.sql
@@ -0,0 +1,13 @@
+WITH t AS (
+    SELECT id AS `id`, dense_rank() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS `r`
+    FROM Employee
+)
+SELECT 
+d.name AS `Department`,
+e.name AS `Employee`,
+e.salary AS `Salary`
+
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+JOIN t ON e.id = t.id AND t.r <= 3

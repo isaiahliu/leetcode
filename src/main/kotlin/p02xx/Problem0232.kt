665fa56d681cde0621b825a67d6165d390a947f0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-24 15:04:51 +0800

    Problem 0232

---
+++ src/main/kotlin/p02xx/Problem0232.kt
@@ -0,0 +1,44 @@
+package p02xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class MyQueue {
+        val stack1 = LinkedList<Int>()
+        val stack2 = LinkedList<Int>()
+
+        fun push(x: Int) {
+            while (stack1.isNotEmpty()) {
+                stack2.push(stack1.pop())
+            }
+
+            stack2.push(x)
+        }
+
+        fun pop(): Int {
+            while (stack2.isNotEmpty()) {
+                stack1.push(stack2.pop())
+            }
+
+            return stack1.pop()
+        }
+
+        fun peek(): Int {
+            while (stack2.isNotEmpty()) {
+                stack1.push(stack2.pop())
+            }
+
+            return stack1.peek()
+        }
+
+        fun empty(): Boolean {
+            return stack1.isEmpty() && stack2.isEmpty()
+        }
+    }
+
+    measureTimeMillis {
+        MyQueue()
+    }
+}
+

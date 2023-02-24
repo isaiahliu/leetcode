4704d320e7ab2e587de81d30b7e6b2158a42896f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-24 14:13:02 +0800

    Problem 0225

---
+++ src/main/kotlin/p02xx/Problem0225.kt
@@ -0,0 +1,30 @@
+package p02xx
+
+import java.util.*
+
+fun main() {
+    class MyStack {
+        val stack = LinkedList<Int>()
+        fun push(x: Int) {
+            stack.add(x)
+        }
+
+        fun pop(): Int {
+            return stack.pollLast()
+        }
+
+        fun top(): Int {
+            return stack.peekLast()
+        }
+
+        fun empty(): Boolean {
+            return stack.isEmpty()
+        }
+    }
+
+
+    println(
+
+    )
+}
+

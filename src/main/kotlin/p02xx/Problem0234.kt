8f35d0ca542c3655a6723a89ec7eadc77f8860a9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-24 15:13:49 +0800

    Problem 0234

---
+++ src/main/kotlin/p02xx/Problem0234.kt
@@ -0,0 +1,52 @@
+package p02xx
+
+import util.ListNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun reverseList(head: ListNode?): ListNode? {
+            head?.next ?: return head
+
+            val next = head.next
+            head.next = null
+
+            val reversedList = reverseList(next)
+
+            next?.next = head
+
+            return reversedList
+        }
+
+        fun isPalindrome(head: ListNode?): Boolean {
+            head?.next ?: return true
+
+            var slow = head
+            var fast = head
+
+            while (fast != null) {
+                slow = slow?.next
+                fast = fast.next?.next
+            }
+
+            var t1 = head
+            var t2 = reverseList(slow)
+
+            while (t1 != null && t2 != null) {
+                if (t1.`val` != t2.`val`) {
+                    return false
+                }
+
+                t1 = t1.next
+                t2 = t2.next
+            }
+
+            return true
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isPalindrome(null).also { println(it) }
+    }
+}
+

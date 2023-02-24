ffc559cc461e568ecbc9e94b5b581c0acbbc3ad7
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-24 11:48:11 +0800

    Problem 0222

---
+++ src/main/kotlin/p02xx/Problem0222.kt
@@ -0,0 +1,67 @@
+package p02xx
+
+import util.TreeNode
+
+fun main() {
+    class Solution {
+        fun countNodes(root: TreeNode?): Int {
+            fun TreeNode?.countDepth(index: Int, max: Int): Int {
+                if (this == null) {
+                    return 0
+                }
+
+                return if (max == 0 || index < max / 2) {
+                    left.countDepth(index, max / 2)
+                } else {
+                    right.countDepth(index - max / 2, max / 2)
+                } + 1
+            }
+
+            val maxDepth = root.countDepth(0, 0)
+
+            if (maxDepth <= 1) {
+                return maxDepth
+            }
+
+            val maxLeafCount = 1 shl (maxDepth - 1)
+
+            var minIndex = maxLeafCount
+            fun binarySearch(startIndex: Int, endIndex: Int) {
+                if (startIndex > endIndex) {
+                    return
+                }
+
+                val midIndex = endIndex - startIndex
+                val midDepth = root.countDepth(midIndex, maxLeafCount)
+
+                if (midDepth < maxDepth) {
+                    minIndex = midIndex
+
+                    binarySearch(startIndex, midIndex - 1)
+                } else {
+                    binarySearch(midIndex + 1, endIndex)
+                }
+            }
+
+            binarySearch(0, maxLeafCount)
+
+            return minIndex + (1 shl (maxDepth - 1)) - 1
+        }
+    }
+
+    println(
+        Solution().countNodes(
+            TreeNode(
+                1, TreeNode(
+                    2,
+                    TreeNode(4),
+                    TreeNode(5),
+                ), TreeNode(
+                    3,
+                    TreeNode(6),
+                )
+            )
+        )
+    )
+}
+

a70299d9c663b5958e53c83e23eb42a75305170a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-24 10:53:51 +0800

    Problem 0219

---
+++ src/main/kotlin/p02xx/Problem0219.kt
@@ -0,0 +1,38 @@
+package p02xx
+
+fun main() {
+    class Solution {
+        fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+            if (k == 0) {
+                return false
+            }
+
+            val set = hashSetOf<Int>()
+            repeat((k + 1).coerceAtMost(nums.size)) {
+
+                if (!set.add(nums[it])) {
+                    return true
+                }
+            }
+
+            var leftIndex = 0
+            var rightIndex = k + 1
+
+            while (rightIndex < nums.size) {
+                set.remove(nums[leftIndex++])
+
+                if (!set.add(nums[rightIndex++])) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
+    println(
+        Solution().containsNearbyDuplicate(
+            intArrayOf(1, 0, 1, 1), 1
+        )
+    )
+}
+

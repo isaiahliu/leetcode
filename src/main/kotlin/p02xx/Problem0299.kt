14ae4743d71f93d1f5d931e1f5d17e3ed53e8fe7
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-27 17:13:49 +0800

    Problem 0299

---
+++ src/main/kotlin/p02xx/Problem0299.kt
@@ -0,0 +1,38 @@
+package p02xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun getHint(secret: String, guess: String): String {
+            val leftCounts = IntArray(10)
+            val rightCounts = IntArray(10)
+
+            var aCount = 0
+            secret.forEachIndexed { index, l ->
+                val r = guess[index]
+
+                if (l == r) {
+                    aCount++
+                } else {
+                    leftCounts[l - '0']++
+                    rightCounts[r - '0']++
+                }
+            }
+
+            var bCount = 0
+            leftCounts.forEachIndexed { index, l ->
+                val r = rightCounts[index]
+
+                bCount += l.coerceAtMost(r)
+            }
+
+            return "${aCount}A${bCount}B"
+        }
+    }
+
+    measureTimeMillis {
+        Solution().getHint("1807", "7810").also { println(it) }
+    }
+}
+

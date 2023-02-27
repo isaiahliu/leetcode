060e2477d24bdfc9a611b573e5a2b319a58809e3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-27 11:58:14 +0800

    Problem 0264

---
+++ src/main/kotlin/p02xx/Problem0264.kt
@@ -0,0 +1,31 @@
+package p02xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun nthUglyNumber(n: Int): Int {
+            val uglyNums = sortedSetOf(1L)
+            uglyNums.first()
+
+            var current = 1L
+            repeat(n) {
+                current = uglyNums.first()
+                uglyNums.remove(current)
+
+                uglyNums.add(current * 2)
+                uglyNums.add(current * 3)
+                uglyNums.add(current * 5)
+            }
+
+            return current.toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().nthUglyNumber(
+            1600
+        ).also { println(it) }
+    }
+}
+

276975e1b4675c908501572dd28acdff55799df8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-28 09:23:06 +0800

    Problem 0303

---
+++ src/main/kotlin/p03xx/Problem0303.kt
@@ -0,0 +1,22 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+class NumArray(private val nums: IntArray) {
+    init {
+        for (i in 1 until nums.size) {
+            nums[i] += nums[i - 1]
+        }
+    }
+
+    fun sumRange(left: Int, right: Int): Int {
+        return nums[right] - nums.getOrElse(left - 1) { 0 }
+    }
+}
+
+    measureTimeMillis {
+        NumArray(intArrayOf()).also { println(it) }
+    }
+}
+

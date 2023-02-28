8444c95a184f947cada621ae2a608e9a6ef9b36e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-02-28 16:54:22 +0800

    Problem 0313

---
+++ src/main/kotlin/p03xx/Problem0313.kt
@@ -0,0 +1,38 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun nthSuperUglyNumber(n: Int, primes: IntArray): Int {
+            val dp = LongArray(n)
+            dp[0] = 1L
+
+            val lastBased = IntArray(primes.size)
+            val nextNums = LongArray(primes.size) { primes[it].toLong() }
+
+            for (i in 1 until dp.size) {
+                val nextNum = nextNums.min()
+                dp[i] = nextNum
+
+                nextNums.withIndex().filter { (_, n) -> n == nextNum }.forEach { (primeIndex, _) ->
+                    nextNums[primeIndex] = dp[++lastBased[primeIndex]] * primes[primeIndex]
+                }
+            }
+
+            return dp[n - 1].toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().nthSuperUglyNumber(
+            5911, intArrayOf(
+                2,
+                3,
+                5,
+                7
+            )
+        ).also { println(it) }
+    }
+}
+

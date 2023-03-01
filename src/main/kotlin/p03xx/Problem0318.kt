574d0dab82497b0984a30cf79e94c0c4571f5967
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-01 11:46:47 +0800

    Problem 0318

---
+++ src/main/kotlin/p03xx/Problem0318.kt
@@ -0,0 +1,150 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxProduct(words: Array<String>): Int {
+            fun String.toCharMap(): Int {
+                var result = 0
+
+                this.forEach {
+                    result = result or (1 shl (it - 'a'))
+                }
+
+                return result
+            }
+
+            val sortedWords = words.map { it.toCharMap() to it.length }.groupingBy { it.first }.fold(0) { a, b ->
+                a.coerceAtLeast(b.second)
+            }.entries.toList().sortedByDescending { it.value }
+
+            var result = 0
+            for (i in sortedWords.indices) {
+                val (leftChars, leftLength) = sortedWords[i]
+                for (j in i + 1 until sortedWords.size) {
+                    val (rightChars, rightLength) = sortedWords[j]
+
+                    if (leftLength * rightLength <= result) {
+                        break
+                    }
+
+                    if (leftChars and rightChars == 0) {
+                        result = leftLength * rightLength
+                        break
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxProduct(
+            arrayOf(
+                "edadc",
+                "ebbfe",
+                "aacdde",
+                "dfe",
+                "cb",
+                "fddddff",
+                "fabca",
+                "adccac",
+                "ece",
+                "ccaf",
+                "feba",
+                "bcb",
+                "edadc",
+                "aea",
+                "bacb",
+                "acefa",
+                "fcebffd",
+                "dfeebca",
+                "bedcbaa",
+                "deaccc",
+                "abedc",
+                "dadff",
+                "eef",
+                "ddebbb",
+                "abecab",
+                "cd",
+                "abdeee",
+                "eedce",
+                "deef",
+                "dceaddd",
+                "ced",
+                "fbbf",
+                "ba",
+                "eefeda",
+                "fb",
+                "cddc",
+                "adafbf",
+                "dded",
+                "aadbf",
+                "caefbaf",
+                "ccebf",
+                "dbb",
+                "ee",
+                "dadcecc",
+                "ddbcabb",
+                "afeaa",
+                "ec",
+                "aad",
+                "efde",
+                "cbcda",
+                "cdbdafd",
+                "cddc",
+                "ecaaa",
+                "ae",
+                "cfc",
+                "bccac",
+                "cdcc",
+                "abbbf",
+                "fcdface",
+                "ddbcdc",
+                "bfebb",
+                "daed",
+                "bc",
+                "dc",
+                "ecdfc",
+                "eeb",
+                "bb",
+                "dad",
+                "caecb",
+                "fbe",
+                "bbbc",
+                "cacea",
+                "dbc",
+                "fbe",
+                "bcfffbd",
+                "aeda",
+                "cff",
+                "ddfc",
+                "ea",
+                "bdfd",
+                "ccb",
+                "cb",
+                "ae",
+                "ceabefa",
+                "dcea",
+                "cbaed",
+                "bfedf",
+                "fa",
+                "ccd",
+                "fece",
+                "bceef",
+                "acabca",
+                "dafa",
+                "fdeec",
+                "dac",
+                "cae",
+                "adeeadb",
+                "ecacc",
+                "acfe",
+                "de"
+            )
+        ).also { println(it) }
+    }
+}
+

b6e51095574117edcb2c44eaa74a185d2ddb3128
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-01 11:04:39 +0800

    Problem 2373

---
+++ src/main/kotlin/p23xx/Problem2373.kt
@@ -0,0 +1,34 @@
+package p23xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun largestLocal(grid: Array<IntArray>): Array<IntArray> {
+            val result = Array(grid.size - 2) {
+                IntArray(grid[it].size - 2)
+            }
+
+            grid.forEachIndexed { rowIndex, row ->
+                row.forEachIndexed { columnIndex, num ->
+                    for (r in rowIndex - 2..rowIndex) {
+                        for (c in columnIndex - 2..columnIndex) {
+                            result.getOrNull(r)?.getOrNull(c)?.also {
+                                result[r][c] = it.coerceAtLeast(num)
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().largestLocal(
+            emptyArray()
+        ).also { println(it) }
+    }
+}
+

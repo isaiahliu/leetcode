d7435f09788a4eb96968efb199405eec9129caba
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-01 10:56:49 +0800

    Problem 0316

---
+++ src/main/kotlin/p03xx/Problem0316.kt
@@ -0,0 +1,59 @@
+package p03xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun removeDuplicateLetters(s: String): String {
+            val lastIndices = IntArray(26)
+            s.forEachIndexed { index, c ->
+                lastIndices[c - 'a'] = index
+            }
+
+            val queue = LinkedList<Char>()
+
+            loop@ for ((index, c) in s.withIndex()) {
+                if (queue.isEmpty()) {
+                    queue.push(c)
+                } else {
+                    val iter = queue.iterator()
+
+                    var hasLower = false
+                    var previousHigher = false
+                    inner@ while (iter.hasNext()) {
+                        when (val t = iter.next()) {
+                            c -> {
+                                if (hasLower && !previousHigher) {
+                                    queue.remove(c)
+                                } else {
+                                    continue@loop
+                                }
+                                break@inner
+                            }
+
+                            else -> {
+                                previousHigher = if (c > t) {
+                                    hasLower = true
+                                    false
+                                } else {
+                                    previousHigher || t > c && lastIndices[t - 'a'] < index
+                                }
+                            }
+                        }
+                    }
+                    queue.push(c)
+                }
+            }
+
+            return queue.reversed().joinToString("")
+        }
+    }
+
+    measureTimeMillis {
+        Solution().removeDuplicateLetters(
+            "mitnlruhznjfyzmtmfnstsxwktxlboxutbic"
+        ).also { println(it) }
+    }
+}
+

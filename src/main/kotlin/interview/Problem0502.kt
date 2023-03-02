e64b104b264bd0bf0cdde957a7ac9651632c576e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-02 09:15:27 +0800

    Problem 0502

---
+++ src/main/kotlin/interview/Problem0502.kt
@@ -0,0 +1,42 @@
+package interview
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun printBin(num: Double): String {
+            val result = StringBuilder()
+
+            var t = num * 2
+            while (t > 0.0 && result.length < 31) {
+                if (t >= 1) {
+                    result.append(1)
+                    t -= 1
+                } else {
+                    result.append(0)
+                }
+
+                t *= 2
+            }
+
+            return when {
+                t > 0 -> {
+                    "ERROR"
+                }
+
+                result.isEmpty() -> {
+                    "0"
+                }
+
+                else -> {
+                    "0.${result}"
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().printBin(0.625).toList().also { println(it) }
+    }
+}
+

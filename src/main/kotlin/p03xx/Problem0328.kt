32decca19b36d8cfa962e32077a428d2f684d2b0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-02 17:30:20 +0800

    Problem 0328

---
+++ src/main/kotlin/p03xx/Problem0328.kt
@@ -0,0 +1,41 @@
+package p03xx
+
+import util.ListNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun oddEvenList(head: ListNode?): ListNode? {
+            val oddRoot = ListNode(1)
+            val evenRoot = ListNode(0)
+
+            var oddTail = oddRoot
+            var evenTail = evenRoot
+
+            var t = head
+            var n = 1
+            while (t != null) {
+                if (n % 2 == 0) {
+                    evenTail.next = t
+                    evenTail = t
+                } else {
+                    oddTail.next = t
+                    oddTail = t
+                }
+
+                t = t.next
+                n++
+            }
+
+            oddTail.next = evenRoot.next
+            evenTail.next = null
+
+            return oddRoot.next
+        }
+    }
+
+    measureTimeMillis {
+        Solution().oddEvenList(null).also { println(it) }
+    }
+}
+

790956fdebb034a865e654b769f476aafc3b31ad
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-03 13:27:21 +0800

    Problem 0334

---
+++ src/main/kotlin/p03xx/Problem0334.kt
@@ -0,0 +1,45 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun increasingTriplet(nums: IntArray): Boolean {
+            if (nums.size < 3) {
+                return false
+            }
+
+            var pre = nums[0]
+            var index = 1
+
+            var first = pre
+            var second = Int.MAX_VALUE
+            while (index < nums.size) {
+                val num = nums[index++]
+                if (num > pre) {
+                    if (num > second) {
+                        return true
+                    } else {
+                        first = pre
+                        second = num
+                    }
+                } else {
+                    if (num in first + 1 until second) {
+                        second = num
+                    }
+                }
+
+                pre = num
+            }
+
+            return false
+        }
+    }
+
+    measureTimeMillis {
+        Solution().increasingTriplet(
+            intArrayOf(1, 5, 0, 4, 1, 3)
+        ).also { println(it) }
+    }
+}
+

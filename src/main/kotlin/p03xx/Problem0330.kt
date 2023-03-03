9ed7e1b5b1c0dc945ade91aad6ab044840613dd0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-03 10:13:57 +0800

    Problem 0330

---
+++ src/main/kotlin/p03xx/Problem0330.kt
@@ -0,0 +1,42 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minPatches(nums: IntArray, n: Int): Int {
+            if (n == 1) {
+                return if (1 in nums) 0 else 1
+            }
+
+            var result = 0
+
+            var currentMax = 0L
+            var index = 0
+            while (currentMax < n) {
+                val currentNum = nums.getOrElse(index) { Int.MAX_VALUE }.toLong()
+
+                when {
+                    currentNum > currentMax + 1 -> {
+                        result++
+                        currentMax = currentMax * 2 + 1
+                    }
+
+                    else -> {
+                        currentMax += currentNum
+                        index++
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minPatches(
+            intArrayOf(1, 2, 2), 5
+        ).also { println(it) }
+    }
+}
+

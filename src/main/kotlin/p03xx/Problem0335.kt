734b4c9f82945f293d057c9e24954a3fa9d84b94
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-03 15:44:27 +0800

    Problem 0335

---
+++ src/main/kotlin/p03xx/Problem0335.kt
@@ -0,0 +1,41 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isSelfCrossing(distance: IntArray): Boolean {
+            var dec = false
+
+            for (i in 2 until distance.size) {
+                val opp = distance[i - 2]
+                val cur = distance[i]
+
+                if (dec) {
+                    if (cur >= opp) {
+                        return true
+                    }
+                } else {
+                    if (cur <= opp) {
+                        dec = true
+
+                        val t = distance.getOrNull(i - 4) ?: 0
+
+                        if (t + cur >= opp) {
+                            distance[i - 1] -= distance.getOrNull(i - 3) ?: 0
+                        }
+                    }
+                }
+            }
+
+            return false
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isSelfCrossing(
+            intArrayOf(1, 1, 1, 2, 1)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

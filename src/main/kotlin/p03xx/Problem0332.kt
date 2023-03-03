52988ba73485ff1d9da2b7e1ffb9e406b92f6d1d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-03 11:43:51 +0800

    Problem 0332

---
+++ src/main/kotlin/p03xx/Problem0332.kt
@@ -0,0 +1,65 @@
+package p03xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findItinerary(tickets: List<List<String>>): List<String> {
+            val ticketMap = sortedMapOf<String, SortedMap<String, MutableList<Int>>>()
+
+            tickets.forEachIndexed { index, (from, to) ->
+                ticketMap.computeIfAbsent(from) { sortedMapOf() }.computeIfAbsent(to) { arrayListOf() }.add(index)
+            }
+
+            val result = arrayListOf("JFK")
+            fun dfs(from: String, route: List<Int>) {
+                if (result.size > 1) {
+                    return
+                }
+
+                if (route.size == tickets.size) {
+                    route.forEach { index ->
+                        result.add(tickets[index][1])
+                    }
+
+                    return
+                }
+
+                ticketMap[from]?.forEach { (to, i) ->
+                    i.forEach {
+                        if (it !in route) {
+                            dfs(to, route + it)
+                        }
+                    }
+                }
+            }
+
+            ticketMap["JFK"]?.forEach { (to, i) ->
+                i.forEach {
+                    dfs(to, listOf(it))
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findItinerary(
+            listOf(
+                listOf("EZE", "AXA"),
+                listOf("TIA", "ANU"),
+                listOf("ANU", "JFK"),
+                listOf("JFK", "ANU"),
+                listOf("ANU", "EZE"),
+                listOf("TIA", "ANU"),
+                listOf("AXA", "TIA"),
+                listOf("TIA", "JFK"),
+                listOf("ANU", "TIA"),
+                listOf("JFK", "TIA")
+            )
+        ).also { println(it) }
+    }
+}
+

4a20abbd808046a00b8628a500923b7ec6cfaa3e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-06 15:24:38 +0800

    Problem 0374

---
+++ src/main/kotlin/p03xx/Problem0374.kt
@@ -0,0 +1,48 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    abstract class GuessGame {
+        val pick = 1702766719
+        fun guess(num: Int): Int {
+            return when {
+                pick < num -> -1
+                pick > num -> 1
+                else -> 0
+            }
+        }
+
+        abstract fun guessNumber(n: Int): Int
+    }
+
+    class Solution : GuessGame() {
+        override fun guessNumber(n: Int): Int {
+            var left = 1
+            var right = n
+
+            while (true) {
+                val mid = left + (right - left) / 2
+
+                when (guess(mid)) {
+                    1 -> {
+                        left = mid + 1
+                    }
+
+                    -1 -> {
+                        right = mid - 1
+                    }
+
+                    0 -> {
+                        return mid
+                    }
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().guessNumber(2126753390).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

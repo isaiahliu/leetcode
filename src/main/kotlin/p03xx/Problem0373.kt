0b6aff9779a6b546a66396496181a4f7af41b003
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-06 15:12:09 +0800

    Problem 0373

---
+++ src/main/kotlin/p03xx/Problem0373.kt
@@ -0,0 +1,36 @@
+package p03xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun kSmallestPairs(nums1: IntArray, nums2: IntArray, k: Int): List<List<Int>> {
+            val queue = PriorityQueue<List<Int>>(compareBy { nums1[it[0]] + nums2[it[1]] })
+
+            nums1.indices.forEach { queue.offer(listOf(it, 0)) }
+
+            val result = arrayListOf<List<Int>>()
+            repeat(k) {
+                if (queue.isEmpty()) {
+                    return result
+                }
+
+                val min = queue.poll()
+                result += min.let { listOf(nums1[it[0]], nums2[it[1]]) }
+                (min[1] + 1).takeIf { it < nums2.size }?.also {
+                    queue.add(listOf(min[0], it))
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().kSmallestPairs(
+            intArrayOf(1, 7, 11), intArrayOf(2, 4, 6), 3
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

c25452175c9f9222149696f98fb5620338451c45
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-06 16:43:06 +0800

    Problem 0375

---
+++ src/main/kotlin/p03xx/Problem0375.kt
@@ -0,0 +1,52 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun getMoneyAmount(n: Int): Int {
+            if (n <= 2) {
+                return n - 1
+            }
+
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+
+            fun find(left: Int, right: Int): Int {
+                when {
+                    left >= right -> return 0
+                    right - left == 1 -> return left
+                    right - left == 2 -> return left + 1
+                }
+
+                val cacheKey = left to right
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                var min = Int.MAX_VALUE
+
+                for (mid in left + 1 until right) {
+                    val leftSum = find(left, mid - 1)
+                    val rightSum = find(mid + 1, right)
+
+                    val sum = mid + leftSum.coerceAtLeast(rightSum)
+
+                    min = min.coerceAtMost(sum)
+                }
+
+                cache[cacheKey] = min
+
+                return min
+            }
+
+            return find(1, n)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().getMoneyAmount(
+            10
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

ee95f0b06bb7ab989e93b6aa2d3d42e5ef78fd17
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-07 17:10:09 +0800

    Problem 0396

---
+++ src/main/kotlin/p03xx/Problem0396.kt
@@ -0,0 +1,33 @@
+package p03xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxRotateFunction(nums: IntArray): Int {
+            val sum = nums.sum()
+
+            var max = 0
+            nums.forEachIndexed { index, i ->
+                max += i * index
+            }
+
+            var pre = max
+            repeat(nums.size - 1) {
+                pre += sum
+                pre -= nums[nums.size - it - 1] * nums.size
+
+                max = max.coerceAtLeast(pre)
+            }
+
+            return max
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxRotateFunction(
+            intArrayOf(4, 3, 2, 6)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

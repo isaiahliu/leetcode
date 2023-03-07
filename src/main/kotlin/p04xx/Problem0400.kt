26137d9421d9bcd5e7f9c0d81bed1004e96e4043
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-07 18:09:18 +0800

    Problem 0400

---
+++ src/main/kotlin/p04xx/Problem0400.kt
@@ -0,0 +1,36 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findNthDigit(n: Int): Int {
+            var digitCount = 1
+            var start = 1L
+            var end = 9L
+
+            var t = n.toLong()
+            while (t > (end - start + 1) * digitCount) {
+                t -= (end - start + 1) * digitCount
+                digitCount += 1
+                start *= 10
+                end *= 10
+                end += 9
+            }
+
+            val targetNum = start + (t - 1) / digitCount
+
+            return targetNum.toString().let {
+                it[(t.toInt() - 1) % digitCount]
+            } - '0'
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findNthDigit(
+            Int.MAX_VALUE
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

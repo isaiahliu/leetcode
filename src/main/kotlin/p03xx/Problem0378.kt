19e35c938bfe4e13576d8b14b914bc46609afba6
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-07 14:05:45 +0800

    Problem 0378

---
+++ src/main/kotlin/p03xx/Problem0378.kt
@@ -0,0 +1,40 @@
+package p03xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun kthSmallest(matrix: Array<IntArray>, k: Int): Int {
+            val queue = PriorityQueue<Pair<Int, Int>>(compareBy { it.first })
+
+            val indices = IntArray(matrix.size)
+
+            matrix.forEachIndexed { index, row ->
+                queue.add(row[0] to index)
+            }
+
+            var result = 0
+            repeat(k) {
+                val (num, index) = queue.poll()
+
+                result = num
+
+                indices[index]++
+
+                matrix[index].getOrNull(indices[index])?.also {
+                    queue.offer(it to index)
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().kthSmallest(
+            arrayOf(), 4
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

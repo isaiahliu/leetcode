b79a81fe6c5df6223e5fa9e53243f0d0693c3d10
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-08 16:44:07 +0800

    Problem 0414

---
+++ src/main/kotlin/p04xx/Problem0414.kt
@@ -0,0 +1,51 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun thirdMax(nums: IntArray): Int {
+            var b1: Int? = null
+            var b2: Int? = null
+            var b3: Int? = null
+
+            nums.forEach {
+                when {
+                    b1 == null -> b1 = it
+                    b1!! < it -> {
+                        b3 = b2
+                        b2 = b1
+                        b1 = it
+                    }
+
+                    b1 == it -> {}
+                    b2 == null -> {
+                        b2 = it
+                    }
+
+                    b2!! < it -> {
+                        b3 = b2
+                        b2 = it
+                    }
+
+                    b2 == it -> {}
+
+                    b3 == null -> b3 = it
+                    b3!! < it -> {
+                        b3 = it
+                    }
+                }
+            }
+
+            return b3 ?: b1 ?: 0
+        }
+    }
+
+    measureTimeMillis {
+        Solution().thirdMax(
+            intArrayOf(1, 2, 3, 4)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

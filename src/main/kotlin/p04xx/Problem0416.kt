4f6801db14099eeaeb64b42fdc1fb1bdc99b8a34
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-08 16:58:07 +0800

    Problem 0416

---
+++ src/main/kotlin/p04xx/Problem0416.kt
@@ -0,0 +1,52 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canPartition(nums: IntArray): Boolean {
+            nums.sort()
+
+            val sum = nums.sum()
+
+            if (sum and 1 > 0) {
+                return false
+            }
+
+            val cache = hashSetOf<Pair<Int, Int>>()
+
+            fun walk(index: Int, target: Int): Boolean {
+                if (target == 0) {
+                    return true
+                }
+
+                if (index >= nums.size) {
+                    return false
+                }
+
+                if (!cache.add(index to target)) {
+                    return false
+                }
+
+                var result = false
+                val num = nums[index]
+
+                if (num <= target) {
+                    result = walk(index + 1, target) || walk(index + 1, target - num)
+                }
+
+                return result
+            }
+
+            return walk(0, sum / 2)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().canPartition(
+            intArrayOf(1, 2, 3, 5)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

56d94ac85f7a07f073713c3665b645ca3d631b6e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-08 11:07:49 +0800

    Problem 0406

---
+++ src/main/kotlin/p04xx/Problem0406.kt
@@ -0,0 +1,67 @@
+package p04xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun reconstructQueue(people: Array<IntArray>): Array<IntArray> {
+            val map = TreeMap<Int, SortedSet<Int>>()
+
+            people.forEach { (key, value) ->
+                map.computeIfAbsent(key) { sortedSetOf() }.add(value)
+            }
+
+            val result = arrayListOf<IntArray>()
+
+            fun process(preHeight: Int, requireCount: Int) {
+                var processedCount = 0
+
+                var t = preHeight
+                while (processedCount < requireCount) {
+                    val (height, s) = map.higherEntry(t)
+
+                    val c = s.first()
+                    val higherCount = result.count { it[0] >= height }
+
+                    if (c - higherCount + 1 > requireCount - processedCount) {
+                        t = height
+                        continue
+                    }
+
+                    if (s.size == 1) {
+                        map.remove(height)
+                    } else {
+                        s.remove(c)
+                    }
+
+
+                    if (c > higherCount) {
+                        process(height, c - higherCount)
+                    }
+                    result.add(intArrayOf(height, c))
+                    processedCount += c - higherCount + 1
+                }
+            }
+
+            process(map.firstKey() - 1, people.size)
+
+            return result.toTypedArray()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().reconstructQueue(
+            arrayOf(
+                intArrayOf(7, 0),
+                intArrayOf(4, 4),
+                intArrayOf(7, 1),
+                intArrayOf(5, 0),
+                intArrayOf(6, 1),
+                intArrayOf(5, 2)
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

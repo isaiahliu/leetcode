171b9f7190a7851311fa2637869712a2f633e080
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-08 09:18:15 +0800

    Problem 2704

---
+++ src/main/kotlin/interview/Problem2704.kt
@@ -0,0 +1,36 @@
+package interview
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxValue(grid: Array<IntArray>): Int {
+            if (grid.isEmpty() || grid[0].isEmpty()) {
+                return 0
+            }
+
+            for (i in 1 until grid.size) {
+                grid[i][0] += grid[i - 1][0]
+            }
+
+            for (i in 1 until grid[0].size) {
+                grid[0][i] += grid[0][i - 1]
+            }
+
+            for (i in 1 until grid.size) {
+                val row = grid[i]
+
+                for (j in 1 until row.size) {
+                    grid[i][j] += grid[i - 1][j].coerceAtLeast(grid[i][j - 1])
+                }
+            }
+
+            return grid[grid.size - 1][grid[0].size - 1]
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxValue(emptyArray()).also { println(it) }
+    }
+}
+

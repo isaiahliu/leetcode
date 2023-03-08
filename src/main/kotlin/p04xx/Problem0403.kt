831199e0fd6f33369a765ce93231df2c98a4c009
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-08 09:34:26 +0800

    Problem 0403

---
+++ src/main/kotlin/p04xx/Problem0403.kt
@@ -0,0 +1,65 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canCross(stones: IntArray): Boolean {
+            val stoneSet = stones.map { it.toLong() }.toSet()
+
+            if (1 !in stoneSet) {
+                return false
+            }
+
+            val target = stones[stones.size - 1].toLong()
+
+            val visited = hashSetOf<Pair<Long, Int>>()
+
+            var success = false
+            fun jump(from: Long, step: Int) {
+                if (success) {
+                    return
+                }
+
+                if (from == target) {
+                    success = true
+                    return
+                }
+
+                if (from > target) {
+                    return
+                }
+
+                if (!visited.add(from to step)) {
+                    return
+                }
+
+                (from + step + 1).takeIf { it in stoneSet }?.let {
+                    jump(from + step + 1, step + 1)
+                }
+
+                (from + step).takeIf { it in stoneSet }?.let {
+                    jump(from + step, step)
+                }
+
+                if (step > 1) {
+                    (from + step - 1).takeIf { it in stoneSet }?.let {
+                        jump(from + step - 1, step - 1)
+                    }
+                }
+            }
+
+            jump(1, 1)
+
+            return success
+        }
+    }
+
+    measureTimeMillis {
+        Solution().canCross(
+            intArrayOf(0, 1, 3, 5, 6, 8, 12, 17)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

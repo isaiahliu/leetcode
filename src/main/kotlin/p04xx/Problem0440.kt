fae513146a4f396c3d07442f7a2765db9a0ffe6c
isaiahliu <isaiahliu@outlook.com>
2023-03-09 21:01:16 +0800

    Problem 0440

---
+++ src/main/kotlin/p04xx/Problem0440.kt
@@ -0,0 +1,71 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findKthNumber(n: Int, k: Int): Int {
+            val special = hashSetOf<Int>()
+            var t = n / 10
+            while (t > 0) {
+                special.add(t)
+                t /= 10
+            }
+
+            var result = 1
+            t = k - 1
+
+            var treeSize = n.toString().map { "1" }.joinToString("").toInt() - 1
+
+            while (t > 0) {
+                when {
+                    result == n -> {
+                        result /= 10
+                    }
+
+                    result in special -> {
+                        special.remove(result)
+
+                        result *= 10
+                        t--
+
+                        treeSize /= 10
+                        treeSize--
+                    }
+
+                    t > treeSize -> {
+                        t -= treeSize
+
+                        while (result % 10 == 9) {
+                            result /= 10
+                            treeSize++
+                            treeSize *= 10
+                        }
+
+                        result++
+                        t--
+                    }
+
+                    else -> {
+                        result *= 10
+                        treeSize /= 10
+                        treeSize--
+
+                        t--
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findKthNumber(
+            10,
+            3
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

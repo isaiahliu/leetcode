6bda7961e77f8c3f579044a895ac70d221ed71dd
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-09 15:41:20 +0800

    Problem 0438

---
+++ src/main/kotlin/p04xx/Problem0438.kt
@@ -0,0 +1,54 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findAnagrams(s: String, p: String): List<Int> {
+            val result = arrayListOf<Int>()
+            val pCharCount = p.toCharArray().distinct().count()
+
+            val pCounts = IntArray(26)
+            p.forEach {
+                pCounts[it - 'a']++
+            }
+
+            var diffCharCount = pCharCount
+
+            val sCounts = IntArray(26)
+
+            for (i in s.indices) {
+                s.getOrNull(i - p.length)?.also {
+                    sCounts[it - 'a']--
+
+                    if (sCounts[it - 'a'] == pCounts[it - 'a']) {
+                        diffCharCount--
+                    } else if (sCounts[it - 'a'] == pCounts[it - 'a'] - 1) {
+                        diffCharCount++
+                    }
+                }
+
+                sCounts[s[i] - 'a']++
+                if (sCounts[s[i] - 'a'] == pCounts[s[i] - 'a']) {
+                    diffCharCount--
+                } else if (sCounts[s[i] - 'a'] == pCounts[s[i] - 'a'] + 1) {
+                    diffCharCount++
+                }
+
+                if (diffCharCount == 0) {
+                    result.add(i - p.length + 1)
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findAnagrams(
+            "cbaebabacd", "abc"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

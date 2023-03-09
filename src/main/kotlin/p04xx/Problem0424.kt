fed6dc048cb6558f1bebe0c4dc7c25355513eef9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-09 11:41:14 +0800

    Problem 0424

---
+++ src/main/kotlin/p04xx/Problem0424.kt
@@ -0,0 +1,73 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun characterReplacement(s: String, k: Int): Int {
+            var t = k
+
+            var leftIndex = 0
+            var leadChar = s[leftIndex]
+
+            val counts = IntArray(26)
+
+            var result = 0
+
+            var rightIndex = 0
+            while (rightIndex < s.length) {
+                val c = s[rightIndex]
+
+                if (c != leadChar) {
+                    if (t > 0) {
+                        t--
+                    } else {
+                        rightIndex--
+                        while (s[leftIndex] == leadChar) {
+                            leftIndex++
+                            counts[leadChar - 'A']--
+                        }
+
+                        t -= counts[leadChar - 'A']
+
+                        leadChar = s[leftIndex]
+                        t += counts[leadChar - 'A']
+
+                        while (t < 0) {
+                            if (s[rightIndex] != leadChar) {
+                                t++
+                            }
+
+                            counts[s[rightIndex] - 'A']--
+                            rightIndex--
+                        }
+
+                        rightIndex++
+                        continue
+                    }
+                }
+
+                rightIndex++
+                counts[c - 'A']++
+
+                result = result.coerceAtLeast(rightIndex - leftIndex)
+            }
+
+            result = result.coerceAtLeast(rightIndex - leftIndex + t.coerceAtMost(leftIndex))
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().characterReplacement(
+            "AAAAABBBBCBB",
+            4
+        ).also { println(it) }
+//        Solution().characterReplacement(
+//            input.first(), 3371
+//        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

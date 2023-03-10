ca07818f06d16d27bfa4a6441af978392e38dc21
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-10 14:44:11 +0800

    Problem 0446

---
+++ src/main/kotlin/p04xx/Problem0446.kt
@@ -0,0 +1,60 @@
+package p04xx
+
+import util.input
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numberOfArithmeticSlices(nums: IntArray): Int {
+            //key = targetNum
+            //value = interval, size, count
+            val targets = hashMapOf<Long, MutableMap<Pair<Long, Int>, Int>>()
+
+            fun addTarget(target: Long, interval: Long, size: Int, count: Int) {
+                val key = interval to size
+                val targetMap = targets.computeIfAbsent(target) {
+                    hashMapOf()
+                }
+
+                targetMap[key] = (targetMap[key] ?: 0) + count
+            }
+
+            for (i in nums.indices) {
+                val num = nums[i].toLong()
+
+                targets[num]?.entries?.sortedByDescending { it.key.second }?.onEach { (pair, count) ->
+                    val (interval, size) = pair
+
+                    val nextTarget = num + interval
+
+                    addTarget(nextTarget, interval, size + 1, count)
+                }
+
+                for (j in 0 until i) {
+                    val pre = nums[j].toLong()
+
+                    val interval = num - pre
+
+                    val target = num + interval
+
+                    addTarget(target, interval, 2, 1)
+                }
+            }
+
+            return targets.values.map {
+                it.filter { it.key.second >= 3 }.values.sum()
+            }.sum()
+        }
+    }
+
+
+    measureTimeMillis {
+//        Solution().numberOfArithmeticSlices(
+//            intArrayOf(1, 2, 3, 4, 5, 6)
+//        ).also { println(it) }
+
+        Solution().numberOfArithmeticSlices(
+            input.first().split(",").map { it.toInt() }.toIntArray()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

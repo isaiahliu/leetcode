999ca46cf6a13054dd7c97331b60b1379751ecb2
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-10 11:17:20 +0800

    Problem 0443

---
+++ src/main/kotlin/p04xx/Problem0443.kt
@@ -0,0 +1,47 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun compress(chars: CharArray): Int {
+            var writeIndex = 0
+
+            var lastChar = chars[0] - 1
+            var count = 0
+            chars.forEachIndexed { index, c ->
+                if (c == lastChar) {
+                    count++
+                } else {
+                    if (count > 1) {
+                        count.toString().forEach {
+                            chars[writeIndex++] = it
+                        }
+                    }
+
+                    lastChar = c
+
+                    chars[writeIndex++] = c
+
+                    count = 1
+                }
+            }
+
+            if (count > 1) {
+                count.toString().forEach {
+                    chars[writeIndex++] = it
+                }
+            }
+
+            return writeIndex
+        }
+    }
+
+    measureTimeMillis {
+        Solution().compress(
+            charArrayOf()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

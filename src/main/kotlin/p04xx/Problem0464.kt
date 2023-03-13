d274b90bb10c7f99cb59be3042978e86bbfc08d1
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-13 18:24:34 +0800

    Problem 0464

---
+++ src/main/kotlin/p04xx/Problem0464.kt
@@ -0,0 +1,56 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canIWin(maxChoosableInteger: Int, desiredTotal: Int): Boolean {
+            if (maxChoosableInteger >= desiredTotal) {
+                return true
+            }
+
+            if ((1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal) {
+                return false
+            }
+
+            val cache = hashMapOf<Int, Boolean>()
+
+            fun canWin(status: Int = 0, sum: Int = 0): Boolean {
+                if (status in cache) {
+                    return cache[status] ?: false
+                }
+
+                var result = false
+
+                for (i in 0 until maxChoosableInteger) {
+                    val p = 1 shl i
+                    val num = i + 1
+
+                    if (status and p == 0) {
+                        if (sum + num >= desiredTotal) {
+                            result = true
+                            break
+                        }
+
+                        if (sum + num < desiredTotal && !canWin(status + p, sum + num)) {
+                            result = true
+                            break
+                        }
+                    }
+                }
+
+                cache[status] = result
+
+                return result
+            }
+
+            return canWin()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().canIWin(
+            10, 11
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

059cfb15c0a8585e6482b2d6ccf5da7ade853821
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-14 18:20:27 +0800

    Problem 0473

---
+++ src/main/kotlin/p04xx/Problem0473.kt
@@ -0,0 +1,58 @@
+package p04xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun makesquare(matchsticks: IntArray): Boolean {
+            var length = matchsticks.sum()
+
+            if (length % 4 != 0) {
+                return false
+            }
+
+            length /= 4
+
+            matchsticks.sortDescending()
+
+            if (matchsticks[0] > length) {
+                return false
+            }
+
+            val stack = LinkedList<Int>()
+            stack.add(0)
+            loop@ while (stack.size < 4) {
+                val start = stack.poll() ?: return false
+                for (i in start + 1 until (1 shl matchsticks.size)) {
+                    if (stack.any { it and i > 0 }) {
+                        continue
+                    }
+
+                    var sum = 0
+
+                    matchsticks.forEachIndexed { index, num ->
+                        if (i and (1 shl index) > 0) {
+                            sum += num
+                        }
+                    }
+
+                    if (sum == length) {
+                        stack.push(i)
+                        stack.push(i)
+
+                        continue@loop
+                    }
+                }
+            }
+
+            return true
+        }
+    }
+
+    measureTimeMillis {
+        Solution().makesquare(
+            intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

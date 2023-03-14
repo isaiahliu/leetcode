7a8e11ddd3e4ac33095a7057d650f6b2cc310061
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-14 15:58:30 +0800

    Problem 0472

---
+++ src/main/kotlin/p04xx/Problem0472.kt
@@ -0,0 +1,64 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findAllConcatenatedWordsInADict(words: Array<String>): List<String> {
+            val result = hashSetOf<String>()
+
+            words.sortBy { it.length }
+
+            val wordSet = words.toSet()
+            val visited = hashSetOf<String>()
+            val match = hashSetOf<String>()
+
+            fun walk(word: String, route: Set<String>) {
+                if (!visited.add(word) && route.isNotEmpty()) {
+                    if (word in match) {
+                        route.forEach {
+                            match.add(it)
+
+                            if (it in wordSet) {
+                                result.add(it)
+                            }
+                        }
+                    }
+
+                    return
+                }
+
+                for (w in words) {
+                    if (w.length > word.length) {
+                        break
+                    } else if (w == word) {
+                        if (route.isNotEmpty()) {
+                            match.add(w)
+                            route.forEach {
+                                match.add(it)
+
+                                if (it in wordSet) {
+                                    result.add(it)
+                                }
+                            }
+                        }
+                    } else if (word.startsWith(w)) {
+                        walk(word.substring(w.length), route + word)
+                    }
+                }
+            }
+
+            for (i in words.size - 1 downTo 0) {
+                walk(words[i], emptySet())
+            }
+
+            return result.toList()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findAllConcatenatedWordsInADict(
+            arrayOf("cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat")
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

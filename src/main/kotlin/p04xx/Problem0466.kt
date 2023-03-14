8f2d1de026661d6a6c0d80dc154479583c316257
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-14 14:19:12 +0800

    Problem 0466

---
+++ src/main/kotlin/p04xx/Problem0466.kt
@@ -0,0 +1,43 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun getMaxRepetitions(s1: String, n1: Int, s2: String, n2: Int): Int {
+            val cache = hashMapOf<Int, Int>()
+            var index1 = 0
+            var index2 = 0
+
+            while (index1 / s1.length < n1) {
+                if (s1[index1 % s1.length] == s2[index2 % s2.length]) {
+                    index2++
+                }
+                index1++
+
+                if (index1 % s1.length == 0) {
+                    cache[index1 / s1.length] = index2 / s2.length
+
+                    if (index2 % s2.length == 0) {
+                        break
+                    }
+                }
+            }
+
+            val t = (index1 / s1.length)
+            var result = n1 / (index1 / s1.length) * (index2 / s2.length)
+
+            cache[n1 % t]?.also {
+                result += it
+            }
+
+            return result / n2
+        }
+    }
+
+    measureTimeMillis {
+        Solution().getMaxRepetitions(
+            "bacaba", 3, "abacab", 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

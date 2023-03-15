4435996109799571b8a64995825e0da292746be9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-15 17:57:00 +0800

    Problem 0486

---
+++ src/main/kotlin/p04xx/Problem0486.kt
@@ -0,0 +1,44 @@
+package p04xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun PredictTheWinner(nums: IntArray): Boolean {
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+
+            val totalScore = nums.sum()
+
+            fun dfs(leftIndex: Int, rightIndex: Int, remaining: Int): Int {
+                if (leftIndex == rightIndex) {
+                    return nums[leftIndex]
+                }
+
+                val cacheKey = leftIndex to rightIndex
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                val leftNum = nums[leftIndex]
+                var result = remaining - dfs(leftIndex + 1, rightIndex, remaining - leftNum)
+
+                val rightNum = nums[rightIndex]
+                result =
+                    result.coerceAtLeast(remaining - dfs(leftIndex, rightIndex - 1, remaining - rightNum))
+
+                cache[cacheKey] = result
+                return result
+            }
+
+            val bestScore = dfs(0, nums.size - 1, totalScore)
+
+            return bestScore * 2 >= totalScore
+        }
+    }
+
+    measureTimeMillis {
+        Solution().PredictTheWinner(
+            intArrayOf(1, 5, 2, 4, 6)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

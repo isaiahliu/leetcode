951a7d49b1489af136bd10118cae0311b6a6caa9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-15 14:05:59 +0800

    Problem 0478

---
+++ src/main/kotlin/p04xx/Problem0478.kt
@@ -0,0 +1,30 @@
+package p04xx
+
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution(val radius: Double, val x_center: Double, val y_center: Double) {
+        fun randPoint(): DoubleArray {
+            val angle = Random.nextDouble(PI * 2)
+            val length = sqrt(Random.nextDouble(radius * radius))
+
+            val deltaX = cos(angle) * length
+            val deltaY = sin(angle) * length
+
+            return doubleArrayOf(x_center + deltaX, y_center + deltaY)
+        }
+    }
+    measureTimeMillis {
+        val sol = Solution(1.0, 0.0, 0.0)
+        repeat(1000) {
+            sol.randPoint().toList().also { (x, y) ->
+                println("${x},${y} -- ${sqrt(x * x + y * y)}")
+            }
+        }
+    }.also { println("Time cost: ${it}ms") }
+}

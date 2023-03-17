95b311de13b1dcc5f5185f1416e3d621b5aba11e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-17 09:41:56 +0800

    Problem 2389

---
+++ src/main/kotlin/p23xx/Problem2389.kt
@@ -0,0 +1,48 @@
+package p23xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun answerQueries(nums: IntArray, queries: IntArray): IntArray {
+            nums.sort()
+
+            for (i in 1 until nums.size) {
+                nums[i] += nums[i - 1]
+            }
+
+            fun binarySearch(startIndex: Int, endIndex: Int, target: Int): Int {
+                if (startIndex > endIndex) {
+                    return -1
+                }
+
+                val midIndex = (startIndex + endIndex) / 2
+                val midNum = nums[midIndex]
+
+                return when {
+                    midNum < target -> {
+                        binarySearch(midIndex + 1, endIndex, target).coerceAtLeast(midIndex)
+                    }
+
+                    midNum > target -> {
+                        binarySearch(startIndex, midIndex - 1, target)
+                    }
+
+                    else -> {
+                        midIndex
+                    }
+                }
+            }
+
+            return IntArray(queries.size) {
+                binarySearch(0, nums.size - 1, queries[it]) + 1
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().answerQueries(
+            intArrayOf(), intArrayOf()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

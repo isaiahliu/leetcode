5d6d546754fcd63690216ca72c98c700e6670248
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-17 15:43:47 +0800

    Problem 0500

---
+++ src/main/kotlin/p05xx/Problem0500.kt
@@ -0,0 +1,42 @@
+package p05xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findWords(words: Array<String>): Array<String> {
+            val map = hashMapOf<Char, Int>()
+            "qwertyuiop".forEach { map[it] = 1 }
+            "asdfghjkl".forEach { map[it] = 2 }
+            "zxcvbnm".forEach { map[it] = 3 }
+
+            fun Char.line(): Int {
+                return if (this in 'A'..'Z') {
+                    map['a' + (this - 'A')]
+                } else {
+                    map[this]
+                } ?: 0
+            }
+
+            return words.filter {
+                val l = it[0].line()
+
+                for (i in 1 until it.length) {
+                    if (it[i].line() != l) {
+                        return@filter false
+                    }
+                }
+
+                true
+            }.toTypedArray()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findWords(
+            arrayOf(
+
+            )
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

f8017522686451a651d9d69d7b3343a10d29554f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-17 11:37:44 +0800

    Problem 0491

---
+++ src/main/kotlin/p04xx/Problem0491.kt
@@ -0,0 +1,55 @@
+package p04xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findSubsequences(nums: IntArray): List<List<Int>> {
+            class Node(val num: Int) {
+                val children = TreeMap<Int, Node>()
+
+                fun addNode(node: Node) {
+                    if (node.num !in children) {
+                        children[node.num] = node
+                    }
+                }
+
+                private var list: List<List<Int>>? = null
+
+                fun generateList(): List<List<Int>> {
+                    return list ?: run {
+                        val me = listOf(num)
+                        arrayListOf<List<Int>>().also { l ->
+                            children.values.forEach {
+                                l.addAll(it.generateList().map { me + it })
+                                l.add(me + listOf(it.num))
+                            }
+
+
+                            list = l
+                        }
+                    }
+                }
+            }
+
+            val nodes = TreeMap<Int, LinkedList<Node>>()
+
+            nums.map { Node(it) }.forEach { node ->
+                nodes.headMap(node.num, true).values.forEach {
+                    it.forEach { it.addNode(node) }
+                }
+
+                nodes.computeIfAbsent(node.num) { LinkedList() }.add(node)
+            }
+
+            return nodes.values.map { it.first().generateList() }.flatten()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findSubsequences(
+            intArrayOf(1, 1, 1, 2, 2, 2)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

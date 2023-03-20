2d63b17fb0dd362bdedf4f7559c1748d3cb413f3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-20 17:15:47 +0800

    Problem 0523

--- src/main/kotlin/p05xx/Problem0523.kt
+++ src/main/kotlin/p05xx/Problem0523.kt
@@ -5,52 +5,22 @@ import kotlin.system.measureTimeMillis
 fun main() {
     class Solution {
         fun checkSubarraySum(nums: IntArray, k: Int): Boolean {
-            if (nums.size == 1) {
-                return false
+            var rem = 0
+
+            val remMap = hashMapOf(0 to -1)
+
+            nums.forEachIndexed { index, n ->
+                rem += n
+                rem %= k
+
+                remMap[rem]?.also {
+                    if (index - it > 1) {
+                        return true
+                    }
+                } ?: run { remMap[rem] = index }
             }
 
-            var sum = nums[0]
-
-            for (i in 1 until nums.size) {
-                if (nums[i] == 0 && nums[i - 1] == 0) {
-                    return true
-                }
-                sum += nums[i]
-
-            }
-
-            val cache = hashMapOf<Pair<Int, Int>, Boolean>()
-
-            fun find(startIndex: Int, endIndex: Int, sum: Int): Boolean {
-                if (startIndex == endIndex) {
-                    return false
-                }
-
-                if (sum % k == 0) {
-                    return true
-                }
-
-                if (sum < k) {
-                    return false
-                }
-
-                val cacheKey = startIndex to endIndex
-                if (cacheKey in cache) {
-                    return cache[cacheKey] ?: false
-                }
-
-                val result = find(startIndex + 1, endIndex, sum - nums[startIndex]) || find(
-                    startIndex,
-                    endIndex - 1,
-                    sum - nums[endIndex]
-                )
-
-                cache[cacheKey] = result
-
-                return result
-            }
-
-            return find(0, nums.lastIndex, sum)
+            return false
         }
     }
 

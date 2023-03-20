5879f503d1ff4089542281cccd2278026c264c45
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-20 17:20:33 +0800

    Problem 0524

---
+++ src/main/kotlin/p05xx/Problem0524.kt
@@ -0,0 +1,35 @@
+package p05xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findLongestWord(s: String, dictionary: List<String>): String {
+            fun subContains(str1: String, str2: String): Boolean {
+                var index1 = 0
+                var index2 = 0
+
+                while (index1 < str1.length && index2 < str2.length) {
+                    if (str1[index1++] == str2[index2]) {
+                        index2++
+                    }
+                }
+
+                return index2 == str2.length
+            }
+
+            dictionary.filter { it.length <= s.length }.groupBy { it.length }.entries.sortedByDescending { it.key }
+                .forEach { (_, strs) ->
+                    strs.sorted().firstOrNull { subContains(s, it) }?.also {
+                        return it
+                    }
+                }
+
+            return ""
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findLongestWord("", listOf()).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

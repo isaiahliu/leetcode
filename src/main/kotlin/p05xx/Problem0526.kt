08e2cfaca350e37880a5311f2f05daac8dd34f91
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-20 18:16:15 +0800

    Problem 0526

---
+++ src/main/kotlin/p05xx/Problem0526.kt
@@ -0,0 +1,43 @@
+package p05xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countArrangement(n: Int): Int {
+            val stack = LinkedList<Int>()
+            stack.push(1)
+
+            var num = 2
+
+            var result = 0
+
+            while (true) {
+                if (stack.size == n || num > n) {
+                    if (stack.size == n) {
+                        result++
+                        stack.pop()
+                    }
+
+                    num = (stack.poll() ?: break) + 1
+                } else {
+                    if (num !in stack && (num % (stack.size + 1) == 0 || (stack.size + 1) % num == 0)) {
+                        stack.push(num)
+                        num = 1
+                    } else {
+                        num++
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countArrangement(
+            15
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

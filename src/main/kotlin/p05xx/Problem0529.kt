5134f42b47f58893cc7fff6074c8098bed7489a0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-21 09:49:35 +0800

    Problem 0529

---
+++ src/main/kotlin/p05xx/Problem0529.kt
@@ -0,0 +1,48 @@
+package p05xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun updateBoard(board: Array<CharArray>, click: IntArray): Array<CharArray> {
+            val clickTask = LinkedList(listOf(click))
+
+            while (clickTask.isNotEmpty()) {
+                val (r, c) = clickTask.pop()
+
+                when (board.getOrNull(r)?.getOrNull(c)) {
+                    'M' -> {
+                        board[r][c] = 'X'
+                        return board
+                    }
+
+                    'E' -> {
+                        val around = arrayOf(
+                            r - 1 to c - 1, r - 1 to c, r - 1 to c + 1,
+                            r to c - 1, r to c + 1,
+                            r + 1 to c - 1, r + 1 to c, r + 1 to c + 1
+                        )
+
+                        val mineCount =
+                            around.mapNotNull { board.getOrNull(it.first)?.getOrNull(it.second) }.count { it == 'M' }
+
+                        if (mineCount == 0) {
+                            board[r][c] = 'B'
+
+                            clickTask.addAll(around.map { intArrayOf(it.first, it.second) })
+                        } else {
+                            board[r][c] = '0' + mineCount
+                        }
+                    }
+                }
+            }
+
+            return board
+        }
+    }
+
+    measureTimeMillis {
+        Solution().updateBoard(arrayOf(), intArrayOf()).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

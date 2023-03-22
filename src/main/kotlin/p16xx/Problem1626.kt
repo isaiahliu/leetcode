f84324da3a7c2f111fbfbbbc6a06dcc5deb0b37c
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-22 09:50:07 +0800

    Problem 1626

---
+++ src/main/kotlin/p16xx/Problem1626.kt
@@ -0,0 +1,32 @@
+package p16xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun bestTeamScore(scores: IntArray, ages: IntArray): Int {
+            val dp = TreeMap<Int, Int>()
+            scores.mapIndexed { index, score -> ages[index] to score }
+                .sortedWith(compareBy<Pair<Int, Int>> { it.second }.thenBy { it.first }).forEach { (age, score) ->
+                    val sum = (dp.lowerEntry(age + 1)?.value ?: 0) + score
+
+                    dp[age] = sum
+
+                    while (true) {
+                        dp.higherEntry(age)?.takeIf { it.value <= sum }?.also {
+                            dp.remove(it.key)
+                        } ?: break
+                    }
+                }
+
+            return dp.lastEntry().value
+        }
+    }
+
+    measureTimeMillis {
+        Solution().bestTeamScore(
+            intArrayOf(4, 5, 6, 5), intArrayOf(2, 1, 2, 1)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

4cc490e3f5fedd7eb03f9ef1dac66ef44af89b70
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-29 15:10:39 +0800

    Problem 0572

---
+++ src/main/kotlin/p05xx/Problem0572.kt
@@ -0,0 +1,51 @@
+package p05xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isSubtree(root: TreeNode?, subRoot: TreeNode?): Boolean {
+            subRoot ?: return true
+            fun TreeNode?.sub(target: TreeNode?): Boolean {
+                return when {
+                    this == null && target == null -> true
+                    target == null -> false
+                    this == null -> false
+                    this.`val` != target.`val` -> false
+                    else -> left.sub(target.left) && right.sub(target.right)
+                }
+
+            }
+
+            fun TreeNode?.dfs(): Boolean {
+                return when {
+                    this == null -> false
+                    `val` == subRoot.`val` && sub(subRoot) -> true
+                    else -> left.dfs() || right.dfs()
+                }
+            }
+
+            return root.dfs()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isSubtree(
+            TreeNode(
+                3,
+                TreeNode(
+                    4,
+                    TreeNode(1),
+                    TreeNode(2)
+                ),
+                TreeNode(5)
+            ), TreeNode(
+                4,
+                TreeNode(1),
+                TreeNode(2)
+            )
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

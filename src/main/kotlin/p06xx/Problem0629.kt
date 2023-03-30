599f019e0f71bb81ed5616a711ea33f16ce2b687
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-30 17:38:47 +0800

    Problem 0629

---
+++ src/main/kotlin/p06xx/Problem0629.kt
@@ -0,0 +1,47 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun kInversePairs(n: Int, k: Int): Int {
+            return when {
+                k == 0 -> {
+                    1
+                }
+
+                k == 1 -> {
+                    n - 1
+                }
+
+                n <= 1 -> {
+                    0
+                }
+
+                else -> {
+                    val m = 1000000007
+                    var dp = LongArray(k + 1)
+
+                    dp[0] = 1
+
+                    repeat(n - 1) { n ->
+                        for (i in 1 until dp.size) {
+                            dp[i] = (dp[i] + dp[i - 1]) % m
+                        }
+
+                        dp = LongArray(k + 1) {
+                            (dp[it] - (dp.getOrNull(it - n - 2) ?: 0) + m) % m
+                        }
+                    }
+
+                    return dp[dp.lastIndex].toInt()
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().kInversePairs(1000, 1000).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

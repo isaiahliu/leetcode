ce626095a89339cd7e47576e6fc3432d4d0f6ae1
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-30 14:26:25 +0800

    Problem 0611

---
+++ src/main/kotlin/p06xx/Problem0611.kt
@@ -0,0 +1,46 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun triangleNumber(nums: IntArray): Int {
+            nums.sort()
+
+            fun binarySearch(startIndex: Int, endIndex: Int, target: Int): Int? {
+                if (startIndex > endIndex) {
+                    return null
+                }
+
+                val midIndex = (startIndex + endIndex) / 2
+                val midNum = nums[midIndex]
+
+                return if (midNum >= target) {
+                    binarySearch(startIndex, midIndex - 1, target)
+                } else {
+                    binarySearch(midIndex + 1, endIndex, target) ?: midIndex
+                }
+            }
+
+            var result = 0
+            for (i in nums.indices) {
+                for (j in i + 1 until nums.size) {
+                    val sum = nums[i] + nums[j]
+
+                    binarySearch(j + 1, nums.lastIndex, sum)?.also {
+                        result += it - j
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().triangleNumber(
+            intArrayOf(2, 2, 3, 4)
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

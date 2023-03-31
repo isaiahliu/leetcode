6b14a8136840b1b77c22c5ddacb99e62055c70ec
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-31 16:11:15 +0800

    Problem 0662

---
+++ src/main/kotlin/p06xx/Problem0662.kt
@@ -0,0 +1,36 @@
+package p06xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun widthOfBinaryTree(root: TreeNode?): Int {
+            val depthRange = hashMapOf<Int, LongArray>()
+
+            fun TreeNode.dfs(depth: Int, startIndex: Long) {
+                val range = depthRange.computeIfAbsent(depth) {
+                    longArrayOf(startIndex, startIndex)
+                }
+
+                range[0] = range[0].coerceAtMost(startIndex)
+                range[1] = range[1].coerceAtLeast(startIndex)
+
+                left?.dfs(depth + 1, startIndex * 2)
+                right?.dfs(depth + 1, startIndex * 2 + 1)
+            }
+
+            root?.dfs(0, 0)
+
+            return depthRange.values.map { (left, right) -> right - left + 1 }.max().toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().widthOfBinaryTree(
+            TreeNode(
+                1, null, TreeNode(2, null, TreeNode(9))
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

faf2547560cb22fd3e8f8b5c17f1dfd08bb071d6
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-31 18:18:31 +0800

    Problem 0665

---
+++ src/main/kotlin/p06xx/Problem0665.kt
@@ -0,0 +1,43 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun checkPossibility(nums: IntArray): Boolean {
+            if (nums.size <= 2) {
+                return true
+            }
+
+            var dec = false
+            var max = nums[0]
+            for (i in 1 until nums.size) {
+                if (nums[i] < max) {
+                    if (dec) {
+                        return false
+                    } else {
+                        dec = true
+
+                        if ((nums.getOrNull(i + 1) ?: return true) < max) {
+                            if (nums[i] < (nums.getOrNull(i - 2) ?: nums[i])) {
+                                return false
+                            } else {
+                                max = nums[i]
+                            }
+                        }
+                    }
+                } else {
+                    max = nums[i]
+                }
+            }
+
+            return true
+        }
+    }
+
+    measureTimeMillis {
+        Solution().checkPossibility(
+            intArrayOf(5, 7, 1, 8)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

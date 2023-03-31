9800fc0b9cf04083262a85174e51824e0ec8bb8a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-31 15:52:54 +0800

    Problem 0661

---
+++ src/main/kotlin/p06xx/Problem0661.kt
@@ -0,0 +1,58 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun imageSmoother(img: Array<IntArray>): Array<IntArray> {
+            img.forEachIndexed { r, row ->
+                row.forEachIndexed { c, _ ->
+                    row[c] += row.getOrNull(c - 1) ?: 0
+                }
+
+                row.forEachIndexed { c, _ ->
+                    row[c] += img.getOrNull(r - 1)?.getOrNull(c) ?: 0
+                }
+            }
+
+            return Array(img.size) { r ->
+                IntArray(img[r].size) { c ->
+                    val sum = img[(r + 1).coerceAtMost(img.lastIndex)][(c + 1).coerceAtMost(img[0].lastIndex)] -
+                            (img.getOrNull(r - 2)?.getOrNull((c + 1).coerceAtMost(img[0].lastIndex)) ?: 0) -
+                            (img.getOrNull((r + 1).coerceAtMost(img.lastIndex))?.getOrNull(c - 2) ?: 0) +
+                            (img.getOrNull(r - 2)?.getOrNull(c - 2) ?: 0)
+
+                    var rCount = 3
+                    if (r == 0) {
+                        rCount--
+                    }
+
+                    if (r == img.lastIndex) {
+                        rCount--
+                    }
+
+                    var cCount = 3
+                    if (c == 0) {
+                        cCount--
+                    }
+
+                    if (c == img[0].lastIndex) {
+                        cCount--
+                    }
+
+                    sum / (rCount * cCount)
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().imageSmoother(
+            arrayOf(
+                intArrayOf(100, 200, 100),
+                intArrayOf(200, 50, 200),
+                intArrayOf(100, 200, 100)
+            )
+        ).map { it.toList() }.also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

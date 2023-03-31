6953976772ba884acb2259131a9a910f133197d4
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-31 09:30:58 +0800

    Problem 2367

--- src/main/kotlin/p23xx/Problem2367.kt
+++ src/main/kotlin/p23xx/Problem2367.kt
@@ -7,15 +7,16 @@ fun main() {
         fun arithmeticTriplets(nums: IntArray, diff: Int): Int {
             val queue = IntArray(201)
 
+            var result = 0
             nums.forEach { n ->
-                queue.getOrNull(n - diff)?.also {
-                    queue[n] = it
-                    queue[n - diff] = 0
+                queue[n] = (queue.getOrNull(n - diff) ?: 0) + 1
+
+                if (queue[n] >= 3) {
+                    result++
                 }
-                queue[n]++
             }
 
-            return queue.map { it - 2 }.filter { it > 0 }.sum()
+            return result
         }
     }
 

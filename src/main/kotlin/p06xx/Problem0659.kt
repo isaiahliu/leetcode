335c8959db244b39948653f452a74a69a4696fda
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-31 15:31:09 +0800

    Problem 0659

---
+++ src/main/kotlin/p06xx/Problem0659.kt
@@ -0,0 +1,46 @@
+package p06xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isPossible(nums: IntArray): Boolean {
+            var pre = nums[0]
+
+            val previousQueue = PriorityQueue<Int>()
+            val currentQueue = PriorityQueue<Int>()
+
+            nums.forEach { num ->
+                if (num != pre) {
+                    if (previousQueue.poll()?.takeIf { it < 3 } != null) {
+                        return false
+                    }
+
+                    previousQueue.clear()
+                    if (num != pre + 1) {
+                        if (currentQueue.poll()?.takeIf { it < 3 } != null) {
+                            return false
+                        }
+                    } else {
+                        previousQueue.addAll(currentQueue)
+                    }
+
+                    currentQueue.clear()
+
+                    pre = num
+                }
+
+                currentQueue.add((previousQueue.poll() ?: 0) + 1)
+            }
+
+            return previousQueue.poll()?.takeIf { it < 3 } == null && currentQueue.poll()?.takeIf { it < 3 } == null
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isPossible(
+            intArrayOf(1, 2, 3, 4, 4, 5)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

befd25d31256a3069dc3130d8f56395a16873d7e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-03-31 10:10:29 +0800

    Problem 0632

---
+++ src/main/kotlin/p06xx/Problem0632.kt
@@ -0,0 +1,45 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun smallestRange(nums: List<List<Int>>): IntArray {
+            val indices = Array(nums.size) { 0 }
+            val lastNums = Array(nums.size) { -9999999 }
+
+            val range = nums.indices
+            var minSize = 1000000
+            val result = intArrayOf(0, 0)
+            while (range.any { indices[it] < nums[it].size }) {
+                val min = range.mapNotNull { nums[it].getOrNull(indices[it]) }.min()
+
+                range.forEach {
+                    if (nums[it].getOrNull(indices[it]) == min) {
+                        lastNums[it] = min
+                        indices[it]++
+
+                        val l = lastNums.min()
+
+                        if (min - l < minSize) {
+                            minSize = min - l
+                            result[0] = l
+                            result[1] = min
+                        }
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().smallestRange(
+            listOf(
+                listOf(4, 10, 15, 24, 26), listOf(0, 9, 12, 20), listOf(5, 18, 22, 30)
+            )
+        ).toList().also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

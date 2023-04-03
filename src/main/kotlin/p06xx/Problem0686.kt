54262a83d0c2661cdad62503c3283916408a5309
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-03 18:33:34 +0800

    Problem 0686

--- src/main/kotlin/p06xx/Problem0686.kt
+++ src/main/kotlin/p06xx/Problem0686.kt
@@ -1,14 +1,27 @@
 package p06xx
 
+import kotlin.math.sign
 import kotlin.system.measureTimeMillis
 
 fun main() {
     class Solution {
         fun repeatedStringMatch(a: String, b: String): Int {
-            for (i in (b.length / a.length).coerceAtLeast(1) until b.length / a.length + 3) {
-                if (b in a.repeat(i)) {
-                    return i
+            fun find(start: Int): Int? {
+                var index = 0
+
+                while (index < b.length) {
+                    if (a[(start + index) % a.length] == b[index]) {
+                        index++
+                    } else {
+                        return null
+                    }
                 }
+
+                return (start + b.length) / a.length + ((start + b.length) % a.length).sign
+            }
+
+            repeat(a.length) {
+                return find(it) ?: return@repeat
             }
 
             return -1

e41b4f6bf5fdacdb9244d17a26d98a78cd9a8b59
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-03 17:23:22 +0800

    Problem 0684

---
+++ src/main/kotlin/p06xx/Problem0684.kt
@@ -0,0 +1,45 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findRedundantConnection(edges: Array<IntArray>): IntArray {
+            val pointGroups = arrayListOf<MutableSet<Int>>()
+
+            val result = intArrayOf(0, 0)
+
+            edges.forEach { (from, to) ->
+                val fromGroup = pointGroups.firstOrNull { from in it } ?: hashSetOf(from).also { pointGroups.add(it) }
+                val toGroup = pointGroups.firstOrNull { to in it } ?: hashSetOf(to).also { pointGroups.add(it) }
+
+                if (fromGroup == toGroup) {
+                    result[0] = from
+                    result[1] = to
+                } else {
+                    fromGroup.addAll(toGroup)
+                    pointGroups.remove(toGroup)
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findRedundantConnection(
+            arrayOf(
+                intArrayOf(9, 10),
+                intArrayOf(5, 8),
+                intArrayOf(2, 6),
+                intArrayOf(1, 5),
+                intArrayOf(3, 8),
+                intArrayOf(4, 9),
+                intArrayOf(8, 10),
+                intArrayOf(4, 10),
+                intArrayOf(6, 8),
+                intArrayOf(7, 9)
+            )
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

232f47ef6215ed3316b0fb6a08cdef826362a7d7
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-03 10:11:47 +0800

    Problem 0667

---
+++ src/main/kotlin/p06xx/Problem0667.kt
@@ -0,0 +1,35 @@
+package p06xx
+
+import kotlin.math.sign
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun constructArray(n: Int, k: Int): IntArray {
+            val result = IntArray(n) { it + 1 }
+
+            if (k > 1) {
+                var currentIndex = 1
+                var currentNum = k + 1
+                var t = k
+
+                while (t != 0) {
+                    result[currentIndex++] = currentNum
+
+                    t -= t.sign
+                    t = -t
+
+                    currentNum += t
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().constructArray(
+            3, 2
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

9e6716b70649240eba3bed9adda48b53939eba8d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-03 17:00:52 +0800

    Problem 0679

--- src/main/kotlin/p06xx/Problem0679.kt
+++ src/main/kotlin/p06xx/Problem0679.kt
@@ -6,17 +6,15 @@ import kotlin.system.measureTimeMillis
 fun main() {
     class Solution {
         fun judgePoint24(cards: IntArray): Boolean {
-            val dp1 = Array(4) {
-                setOf(cards[it].toDouble())
+            val dp = Array(0b1111) {
+                hashSetOf<Double>()
             }
-            val dp2 = hashMapOf<Pair<Int, Int>, Set<Double>>()
-            val dp3 = hashMapOf<Pair<Pair<Int, Int>, Int>, Set<Double>>()
 
-            fun join(set1: Set<Double>, set2: Set<Double>): Set<Double> {
+            fun join(set1: Set<Double>?, set2: Set<Double>?): Set<Double> {
                 val res = hashSetOf<Double>()
 
-                set1.forEach { s1 ->
-                    set2.forEach { s2 ->
+                set1?.forEach { s1 ->
+                    set2?.forEach { s2 ->
                         res.add(s1 + s2)
                         res.add(s1 - s2)
                         res.add(s2 - s1)
@@ -29,52 +27,37 @@ fun main() {
                 return res
             }
 
-            dp2[0 to 1] = join(dp1[0], dp1[1])
-            dp2[0 to 2] = join(dp1[0], dp1[2])
-            dp2[0 to 3] = join(dp1[0], dp1[3])
-            dp2[1 to 2] = join(dp1[1], dp1[2])
-            dp2[1 to 3] = join(dp1[1], dp1[3])
-            dp2[2 to 3] = join(dp1[2], dp1[3])
+            repeat(0b1111) { num ->
+                repeat(4) { d ->
+                    if (num and (1 shl d) > 0) {
+                        val target = num - (1 shl d)
 
-            fun Pair<Pair<Int, Int>, Int>.process(): Set<Double> {
-                val (ab, c) = this
-                val (a, b) = ab
-
-                val result = hashSetOf<Double>()
-
-                result.addAll(join(dp1[a], dp2[b to c].orEmpty()))
-                result.addAll(join(dp1[b], dp2[a to c].orEmpty()))
-                result.addAll(join(dp1[c], dp2[a to b].orEmpty()))
-
-                return result
+                        if (target == 0) {
+                            dp[num].add(cards[d].toDouble())
+                        } else {
+                            dp[num].addAll(join(dp[1 shl d], dp[target]))
+                        }
+                    }
+                }
             }
 
-            arrayOf(
-                0 to 1 to 2,
-                0 to 1 to 3,
-                0 to 2 to 3,
-                1 to 2 to 3
-            ).forEach {
-                dp3[it] = it.process()
-            }
-
-            val dp4 = hashSetOf<Double>()
-
-            dp4.addAll(join(dp1[0], dp3[1 to 2 to 3].orEmpty()))
-            dp4.addAll(join(dp1[1], dp3[0 to 2 to 3].orEmpty()))
-            dp4.addAll(join(dp1[2], dp3[0 to 1 to 3].orEmpty()))
-            dp4.addAll(join(dp1[3], dp3[0 to 1 to 2].orEmpty()))
-            dp4.addAll(join(dp2[0 to 1].orEmpty(), dp2[2 to 3].orEmpty()))
-            dp4.addAll(join(dp2[0 to 2].orEmpty(), dp2[1 to 3].orEmpty()))
-            dp4.addAll(join(dp2[0 to 3].orEmpty(), dp2[1 to 2].orEmpty()))
-
-            return dp4.any {
-                (it - 24.0).absoluteValue < 0.000001
+            return arrayOf(
+                0b0001 to 0b1110,
+                0b0010 to 0b1101,
+                0b0100 to 0b1011,
+                0b1000 to 0b0111,
+                0b0011 to 0b1100,
+                0b0101 to 0b1010,
+                0b1001 to 0b0110
+            ).any { (l, r) ->
+                join(dp[l], dp[r]).any {
+                    (it - 24.0).absoluteValue < 0.000001
+                }
             }
         }
     }
 
     measureTimeMillis {
-        Solution().judgePoint24(intArrayOf(1, 2, 1, 2)).also { println(it) }
+        Solution().judgePoint24(intArrayOf(4, 1, 8, 7)).also { println(it) }
     }.also { println("Time cost: ${it}ms") }
 }

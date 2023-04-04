790368638c43a4e851886b71294229a6407111fb
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-04 14:52:35 +0800

    Problem 0688

---
+++ src/main/kotlin/p06xx/Problem0688.kt
@@ -0,0 +1,44 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun knightProbability(n: Int, k: Int, row: Int, column: Int): Double {
+            var map = hashMapOf<Pair<Int, Int>, Double>()
+            map[row to column] = 1.0
+
+            val range = 0 until n
+            repeat(k) {
+                val newMap = hashMapOf<Pair<Int, Int>, Double>()
+
+                map.forEach { (p, rate) ->
+                    val (r, c) = p
+
+                    arrayOf(
+                        r - 2 to c - 1,
+                        r - 1 to c - 2,
+                        r + 1 to c - 2,
+                        r + 2 to c - 1,
+                        r - 2 to c + 1,
+                        r - 1 to c + 2,
+                        r + 1 to c + 2,
+                        r + 2 to c + 1
+                    ).filter { it.first in range && it.second in range }.forEach {
+                        newMap[it] = (newMap[it] ?: 0.0) + rate / 8
+                    }
+                }
+
+                map = newMap
+            }
+
+            return map.values.sum()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().knightProbability(
+            8, 30, 6, 4
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

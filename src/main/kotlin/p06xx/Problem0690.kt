0b197eb8e6c86e729ba33221e06c6456081474d3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-04 15:45:14 +0800

    Problem 0690

---
+++ src/main/kotlin/p06xx/Problem0690.kt
@@ -0,0 +1,41 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Employee {
+        var id: Int = 0
+        var importance: Int = 0
+        var subordinates: List<Int> = listOf()
+    }
+
+    class Solution {
+        fun getImportance(employees: List<Employee?>, id: Int): Int {
+            val cache = hashMapOf<Int, Int>()
+
+            val empl = employees.filterNotNull().associateBy { it.id }
+
+            fun Int.calculate(): Int {
+                if (this in cache) {
+                    return cache[this] ?: 0
+                }
+
+                val employee = empl[this] ?: return 0
+
+                val result = employee.importance + employee.subordinates.map { it.calculate() }.sum()
+
+                cache[this] = result
+
+                return result
+            }
+
+            return id.calculate()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().getImportance(
+            listOf(), 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

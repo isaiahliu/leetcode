7107eb1b57b4ec09816a36943511d2a55db997f9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-04 16:51:10 +0800

    Problem 0698

---
+++ src/main/kotlin/p06xx/Problem0698.kt
@@ -0,0 +1,67 @@
+package p06xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canPartitionKSubsets(nums: IntArray, k: Int): Boolean {
+            if (k == 1) {
+                return true
+            }
+
+            val sum = nums.sum()
+            if (sum % k != 0) {
+                return false
+            }
+
+            val avg = sum / k
+            if (nums.any { it > avg }) {
+                return false
+            }
+
+            val dp = Array(k) {
+                hashSetOf<Int>()
+            }
+
+            for (i in 1 until (1 shl nums.size)) {
+                val num = nums.indices.map {
+                    if (i and (1 shl it) > 0) {
+                        nums[it]
+                    } else {
+                        0
+                    }
+                }.sum()
+
+                when {
+                    num == avg -> {
+                        dp[1].add(i)
+                    }
+
+                    num % avg == 0 -> {
+                        loop@ for (left in 1 until dp.size) {
+                            val right = num / avg - left
+
+                            if (left > right) {
+                                break
+                            }
+
+                            for (t in dp[left]) {
+                                if (t and i == t && i - t in dp[right]) {
+                                    (dp.getOrNull(left + right) ?: return true).add(i)
+                                    break@loop
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false
+        }
+    }
+
+    measureTimeMillis {
+        Solution().canPartitionKSubsets(
+            intArrayOf(1, 1, 1, 1, 2, 2, 2, 2), 2
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

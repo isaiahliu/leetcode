f29285e87bf77be00bce3b750cbc160619d6035a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-06 14:17:37 +0800

    Problem 0712

---
+++ src/main/kotlin/p07xx/Problem0712.kt
@@ -0,0 +1,50 @@
+package p07xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minimumDeleteSum(s1: String, s2: String): Int {
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+
+            fun find(index1: Int, index2: Int): Int {
+                val cacheKey = index1 to index2
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+                val result = when {
+                    index1 == s1.length && index2 == s2.length -> {
+                        0
+                    }
+
+                    index1 == s1.length -> {
+                        find(index1, index2 + 1) + s2[index2].code
+                    }
+
+                    index2 == s2.length -> {
+                        find(index1 + 1, index2) + s1[index1].code
+                    }
+
+                    s1[index1] == s2[index2] -> {
+                        find(index1 + 1, index2 + 1)
+                    }
+
+                    else -> {
+                        (find(index1 + 1, index2) + s1[index1].code).coerceAtMost(
+                            find(index1, index2 + 1) + s2[index2].code
+                        )
+                    }
+                }
+
+                cache[cacheKey] = result
+                return result
+            }
+
+            return find(0, 0)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minimumDeleteSum("sea", "eat").also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

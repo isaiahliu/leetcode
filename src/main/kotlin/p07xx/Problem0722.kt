07c63387361b7f59db075d47b4bdd3d3582466b7
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-06 17:55:25 +0800

    Problem 0722

---
+++ src/main/kotlin/p07xx/Problem0722.kt
@@ -0,0 +1,71 @@
+package p07xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun removeComments(source: Array<String>): List<String> {
+            val result = arrayListOf<String>()
+
+            val str = StringBuilder()
+            var inBlockComment = false
+
+            loop@ for (line in source) {
+                var index = 0
+                while (index < line.length) {
+                    val c = line[index]
+                    val next = line.getOrNull(index + 1)
+                    when {
+                        inBlockComment && c == '*' && next == '/' -> {
+                            index++
+                            inBlockComment = false
+                        }
+
+                        inBlockComment -> {
+                        }
+
+                        c == '/' && next == '/' -> {
+                            break
+                        }
+
+                        c == '/' && next == '*' -> {
+                            inBlockComment = true
+                            index++
+                        }
+
+                        else -> {
+                            str.append(c)
+                        }
+                    }
+
+                    index++
+                }
+
+                if (!inBlockComment) {
+                    if (str.isNotEmpty()) {
+                        result.add(str.toString())
+                        str.clear()
+                    }
+                }
+            }
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().removeComments(
+            arrayOf(
+                "main() {",
+                "  Node* p;",
+                "  /* declare a Node",
+                "  /*float f = 2.0",
+                "   p->val = f;",
+                "   /**/",
+                "   p->val = 1;",
+                "   //*/ cout << success;*/",
+                "}",
+                " "
+            )
+        ).joinToString("\n").also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

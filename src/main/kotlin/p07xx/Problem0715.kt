08f886975574435d9c99758400babb4a82bfa83a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-06 15:37:46 +0800

    Problem 0715

---
+++ src/main/kotlin/p07xx/Problem0715.kt
@@ -0,0 +1,67 @@
+package p07xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class RangeModule {
+        val map = TreeMap<Int, Int>()
+
+        fun addRange(left: Int, right: Int) {
+            var l = left
+            var r = right
+            map.lowerEntry(left + 1)?.takeIf { it.value >= l }?.also {
+                if (it.value >= right) {
+                    return
+                } else {
+                    l = it.key
+                }
+            }
+
+            while (true) {
+                val next = map.higherEntry(left)?.takeIf { it.key <= right + 1 } ?: break
+
+                map.remove(next.key)
+                r = r.coerceAtLeast(next.value)
+            }
+
+            map[l] = r
+        }
+
+        fun queryRange(left: Int, right: Int): Boolean {
+            return map.lowerEntry(left + 1)?.value?.takeIf {
+                it >= right
+            } != null
+        }
+
+        fun removeRange(left: Int, right: Int) {
+            map.lowerEntry(left)?.takeIf { it.value > left }?.also {
+                map[it.key] = left
+                map[left] = it.value
+            }
+
+            while (true) {
+                map.higherEntry(left - 1)?.takeIf { it.key < right }?.also { (key, value) ->
+                    map.remove(key)
+                    if (value > right) {
+                        map[right] = value
+                    }
+                } ?: break
+            }
+        }
+    }
+
+    measureTimeMillis {
+        val sol = RangeModule()
+        sol.addRange(6, 8).also { println(sol.map) }
+        sol.removeRange(7, 8).also { println(sol.map) }
+        sol.removeRange(8, 9).also { println(sol.map) }
+        sol.addRange(8, 9).also { println(sol.map) }
+        sol.removeRange(1, 3).also { println(sol.map) }
+        sol.addRange(1, 8).also { println(sol.map) }
+
+        sol.queryRange(2, 4).also { println(it) }
+        sol.queryRange(2, 9).also { println(it) }
+        sol.queryRange(4, 6).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

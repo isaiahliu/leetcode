07bb653f944ceff1b450b3254f5a1e596ebba95f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-07 11:38:56 +0800

    Problem 0730

---
+++ src/main/kotlin/p07xx/Problem0730.kt
@@ -0,0 +1,58 @@
+package p07xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countPalindromicSubsequences(s: String): Int {
+            val m = 1000000007
+
+            val indices = Array(4) { TreeSet<Int>() }
+            s.forEachIndexed { index, c ->
+                indices[c - 'a'].add(index)
+            }
+
+            val cache = hashMapOf<Pair<Int, Int>, Long>()
+            fun count(startIndex: Int, endIndex: Int): Long {
+                val cacheKey = startIndex to endIndex
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                var result = 0L
+
+                indices.forEach {
+                    val charStart = it.higher(startIndex - 1) ?: return@forEach
+                    val charEnd = it.lower(endIndex + 1) ?: return@forEach
+
+                    when {
+                        charStart == charEnd -> {
+                            result += 1
+                        }
+
+                        charStart < charEnd -> {
+                            result += count(charStart + 1, charEnd - 1) + 2
+                        }
+                    }
+                }
+
+                result %= m
+
+                cache[cacheKey] = result
+
+                return result
+            }
+
+            return count(0, s.lastIndex).toInt().also {
+                val a = 1
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countPalindromicSubsequences(
+            "abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

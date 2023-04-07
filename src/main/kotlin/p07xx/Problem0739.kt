c3546495a53e1985c772a6ec9e655788b093d38b
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-07 14:49:55 +0800

    Problem 0739

---
+++ src/main/kotlin/p07xx/Problem0739.kt
@@ -0,0 +1,32 @@
+package p07xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun dailyTemperatures(temperatures: IntArray): IntArray {
+            val queue = LinkedList<Pair<Int, Int>>()
+
+            for (i in temperatures.lastIndex downTo 0) {
+                val temp = temperatures[i]
+
+                while (queue.peek()?.first?.takeIf { it <= temp } != null) {
+                    queue.pop()
+                }
+
+                temperatures[i] = queue.peek()?.second?.let { it - i } ?: 0
+
+                queue.push(temp to i)
+            }
+
+            return temperatures
+        }
+    }
+
+    measureTimeMillis {
+        Solution().dailyTemperatures(
+            intArrayOf(73, 74, 75, 71, 69, 72, 76, 73)
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

1d812fec2115b2cab1db34e60aa9ce9810ad6316
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-07 11:41:14 +0800

    Problem 0730

--- src/main/kotlin/p07xx/Problem0730.kt
+++ src/main/kotlin/p07xx/Problem0730.kt
@@ -15,33 +15,46 @@ fun main() {
 
             val cache = hashMapOf<Pair<Int, Int>, Long>()
             fun count(startIndex: Int, endIndex: Int): Long {
-                val cacheKey = startIndex to endIndex
-                if (cacheKey in cache) {
-                    return cache[cacheKey] ?: 0
-                }
+                when {
+                    startIndex > endIndex -> {
+                        return 0
+                    }
 
-                var result = 0L
+                    startIndex == endIndex -> {
+                        return 1
+                    }
 
-                indices.forEach {
-                    val charStart = it.higher(startIndex - 1) ?: return@forEach
-                    val charEnd = it.lower(endIndex + 1) ?: return@forEach
-
-                    when {
-                        charStart == charEnd -> {
-                            result += 1
+                    else -> {
+                        val cacheKey = startIndex to endIndex
+                        if (cacheKey in cache) {
+                            return cache[cacheKey] ?: 0
                         }
 
-                        charStart < charEnd -> {
-                            result += count(charStart + 1, charEnd - 1) + 2
+                        var result = 0L
+
+                        indices.forEach {
+                            val charStart = it.higher(startIndex - 1) ?: return@forEach
+                            val charEnd = it.lower(endIndex + 1) ?: return@forEach
+
+                            when {
+                                charStart == charEnd -> {
+                                    result += 1
+                                }
+
+                                charStart < charEnd -> {
+                                    result += count(charStart + 1, charEnd - 1) + 2
+                                }
+                            }
                         }
+
+                        result %= m
+
+                        cache[cacheKey] = result
+
+                        return result
                     }
                 }
 
-                result %= m
-
-                cache[cacheKey] = result
-
-                return result
             }
 
             return count(0, s.lastIndex).toInt()

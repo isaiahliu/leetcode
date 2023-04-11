1e1a5e2836df0d6d13295c979913c764894cfee9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-11 14:09:44 +0800

    Problem 0746

--- src/main/kotlin/p07xx/Problem0746.kt
+++ src/main/kotlin/p07xx/Problem0746.kt
@@ -5,13 +5,11 @@ import kotlin.system.measureTimeMillis
 fun main() {
     class Solution {
         fun minCostClimbingStairs(cost: IntArray): Int {
-            val dp = IntArray(cost.size + 1)
-
-            for (i in 2 until dp.size) {
-                dp[i] = (dp[i - 2] + cost[i - 2]).coerceAtMost(dp[i - 1] + cost[i - 1])
+            for (i in 2 until cost.size) {
+                cost[i] += cost[i - 2].coerceAtMost(cost[i - 1])
             }
 
-            return dp[cost.size]
+            return cost[cost.lastIndex].coerceAtMost(cost[cost.lastIndex - 1])
         }
     }
 

166ec5f275587b96a2785467dc33a223d2b49447
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-12 15:00:45 +0800

    Problem 0767

---
+++ src/main/kotlin/p07xx/Problem0767.kt
@@ -0,0 +1,57 @@
+package p07xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun reorganizeString(s: String): String {
+            val queue = PriorityQueue<Pair<Int, Char>>(compareByDescending { it.first })
+            s.toList().groupingBy { it }.eachCount().forEach { (c, count) ->
+                queue.add(count to c)
+            }
+
+            queue.peek()?.first?.let {
+                if (it > (s.length - it + 1)) {
+                    return ""
+                }
+            }
+
+            val result = StringBuilder()
+            var lastChar = ' '
+
+            while (queue.isNotEmpty()) {
+                val (topCount, topChar) = queue.poll()
+
+                if (topChar == lastChar) {
+                    val (secCount, secChar) = queue.poll()
+                    queue.add(topCount to topChar)
+
+                    result.append(secChar)
+
+                    if (secCount > 1) {
+                        queue.add(secCount - 1 to secChar)
+                    }
+
+                    lastChar = secChar
+                } else {
+                    result.append(topChar)
+
+                    if (topCount > 1) {
+                        queue.add(topCount - 1 to topChar)
+                    }
+
+                    lastChar = topChar
+                }
+            }
+
+            return result.toString()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().reorganizeString(
+            "aaab"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

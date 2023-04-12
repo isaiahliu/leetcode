57525121202e76b9af85a61b5e448b93b5b50814
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-12 17:00:58 +0800

    Problem 0773

---
+++ src/main/kotlin/p07xx/Problem0773.kt
@@ -0,0 +1,58 @@
+package p07xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun slidingPuzzle(board: Array<IntArray>): Int {
+            val target = "123450"
+            val invalidTarget = "123540"
+
+            val init = board.joinToString("") { it.joinToString("") }
+
+            val visited = hashSetOf(init)
+            val tasks = hashSetOf(init)
+
+            var result = 0
+            fun String.switch(pos1: Int, pos2: Int): String? {
+                if (this[pos1] != '0' && this[pos2] != '0') {
+                    return null
+                }
+                val m = hashMapOf(pos1 to pos2, pos2 to pos1)
+                return "${this[m[0] ?: 0]}${this[m[1] ?: 1]}${this[m[2] ?: 2]}${this[m[3] ?: 3]}${this[m[4] ?: 4]}${this[m[5] ?: 5]}"
+            }
+
+            while (tasks.isNotEmpty()) {
+                tasks.toSet().also { tasks.clear() }.forEach {
+                    when (it) {
+                        target -> return result
+                        invalidTarget -> return -1
+                        else -> {
+                            val nexts = hashSetOf<String>()
+
+                            it.switch(0, 1)?.also { nexts.add(it) }
+                            it.switch(0, 3)?.also { nexts.add(it) }
+                            it.switch(1, 2)?.also { nexts.add(it) }
+                            it.switch(1, 4)?.also { nexts.add(it) }
+                            it.switch(2, 5)?.also { nexts.add(it) }
+                            it.switch(3, 4)?.also { nexts.add(it) }
+                            it.switch(4, 5)?.also { nexts.add(it) }
+
+                            tasks.addAll(nexts.filter { visited.add(it) })
+                        }
+                    }
+                }
+
+                result++
+            }
+
+            return -1
+        }
+    }
+
+    measureTimeMillis {
+        Solution().slidingPuzzle(
+            arrayOf()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

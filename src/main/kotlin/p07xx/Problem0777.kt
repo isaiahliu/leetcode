cdd8d429c4ab0c38ae9812edc97393581e1db686
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-12 17:25:01 +0800

    Problem 0777

---
+++ src/main/kotlin/p07xx/Problem0777.kt
@@ -0,0 +1,40 @@
+package p07xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canTransform(start: String, end: String): Boolean {
+            if (start.length != end.length) {
+                return false
+            }
+
+            val l = start.mapIndexed { index, c -> c to index }.filter { it.first != 'X' }
+            val r = end.mapIndexed { index, c -> c to index }.filter { it.first != 'X' }
+
+            if (l.size != r.size) {
+                return false
+            }
+
+            var index = 0
+            while (index < l.size) {
+                val (lc, li) = l[index]
+                val (rc, ri) = r[index++]
+
+                when {
+                    lc != rc -> return false
+                    lc == 'L' && li < ri -> return false
+                    lc == 'R' && li > ri -> return false
+                }
+            }
+
+            return true
+        }
+    }
+
+    measureTimeMillis {
+        Solution().canTransform(
+            "", ""
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

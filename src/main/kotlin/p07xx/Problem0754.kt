f0b4e7ad577e798d93c41a4c47bdc0bc8e8b14ba
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-12 10:34:54 +0800

    Problem 0754

---
+++ src/main/kotlin/p07xx/Problem0754.kt
@@ -0,0 +1,50 @@
+package p07xx
+
+import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun reachNumber(target: Int): Int {
+            val posTarget = target.absoluteValue
+
+            fun binarySearch(start: Int, end: Int): Int? {
+                if (start > end) {
+                    return null
+                }
+
+                val midNum = (start + end) / 2
+                val midSum = (1 + midNum) * midNum / 2
+
+                return when {
+                    midSum == posTarget -> {
+                        return midNum
+                    }
+
+                    midSum < posTarget -> {
+                        binarySearch(midNum + 1, end)
+                    }
+
+                    else -> {
+                        binarySearch(start, midNum - 1) ?: midNum
+                    }
+                }
+            }
+
+            val num = binarySearch(1, 45000) ?: return 0
+            val sum = (1 + num) * num / 2
+
+            return if (sum > posTarget && (sum - posTarget) % 2 == 1) {
+                num + (num % 2) + 1
+            } else {
+                num
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().reachNumber(
+            5
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

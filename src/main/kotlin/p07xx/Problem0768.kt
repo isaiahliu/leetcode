90c16bc4b81c245246ef3182ab976b3994ee14df
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-12 15:13:44 +0800

    Problem 0768

---
+++ src/main/kotlin/p07xx/Problem0768.kt
@@ -0,0 +1,29 @@
+package p07xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxChunksToSorted(arr: IntArray): Int {
+            val queue = LinkedList<Pair<Int, Int>>()
+
+            arr.forEach { num ->
+                var last = num
+                while (queue.isNotEmpty() && queue.peekLast().second > num) {
+                    last = last.coerceAtLeast(queue.pollLast().second)
+                }
+
+                queue.add(num to last)
+            }
+
+            return queue.size
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxChunksToSorted(
+            intArrayOf(1, 0, 1, 3, 2)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

91d592d68eaef500a9e1448d77dd14f75da61359
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-13 17:42:04 +0800

    Problem 0795

---
+++ src/main/kotlin/p07xx/Problem0795.kt
@@ -0,0 +1,46 @@
+package p07xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numSubarrayBoundedMax(nums: IntArray, left: Int, right: Int): Int {
+            var result = 0
+
+            var size = 0
+            var lastMatchIndex: Int? = null
+            nums.forEachIndexed { index, num ->
+                when {
+                    num > right -> {
+                        size = 0
+                        lastMatchIndex = null
+                    }
+
+                    num < left -> {
+                        size++
+
+                        lastMatchIndex?.also {
+                            result += size - (index - it)
+
+                        }
+                    }
+
+                    else -> {
+                        size++
+
+                        result += size
+                        lastMatchIndex = index
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numSubarrayBoundedMax(
+            intArrayOf(), 1, 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

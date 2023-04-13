c8c836f5e721619c504199466bc473f5c9f26556
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-13 18:39:28 +0800

    Problem 0799

---
+++ src/main/kotlin/p07xx/Problem0799.kt
@@ -0,0 +1,38 @@
+package p07xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun champagneTower(poured: Int, query_row: Int, query_glass: Int): Double {
+            val glasses = Array(query_row + 2) {
+                DoubleArray(it + 1)
+            }
+
+            glasses[0][0] = poured.toDouble()
+
+            for (r in 0..query_row) {
+                glasses[r].forEachIndexed { c, d ->
+                    if (d > 1) {
+                        glasses[r][c] = 1.0
+
+                        ((d - 1) / 2).also {
+                            glasses[r + 1][c] += it
+                            glasses[r + 1][c + 1] += it
+                        }
+                    }
+                }
+            }
+
+            return glasses[query_row][query_glass]
+        }
+    }
+
+    measureTimeMillis {
+        Solution().champagneTower(
+            100000009,
+            33,
+            17
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

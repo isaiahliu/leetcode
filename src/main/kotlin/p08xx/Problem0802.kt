94550e06d96ea17b97b970c56da4c7face2211c0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-14 13:02:06 +0800

    Problem 0802

---
+++ src/main/kotlin/p08xx/Problem0802.kt
@@ -0,0 +1,58 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun eventualSafeNodes(graph: Array<IntArray>): List<Int> {
+            val terminals = hashSetOf<Int>()
+
+            val froms = Array(graph.size) { hashSetOf<Int>() }
+
+            val graphMap = graph.mapIndexed { index, tos ->
+                if (tos.isEmpty()) {
+                    terminals.add(index)
+                }
+
+                tos.forEach {
+                    froms[it].add(index)
+                }
+
+                tos.toMutableSet()
+            }
+
+            val result = arrayListOf<Int>()
+
+            while (terminals.isNotEmpty()) {
+                terminals.toSet().also { terminals.clear() }.forEach { t ->
+                    result += t
+
+                    froms[t].forEach { from ->
+                        graphMap[from].also {
+                            it.remove(t)
+
+                            if (it.isEmpty()) {
+                                terminals.add(from)
+                            }
+                        }
+                    }
+                }
+            }
+            return result.sorted()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().eventualSafeNodes(
+            arrayOf(
+                intArrayOf(1, 2),
+                intArrayOf(2, 3),
+                intArrayOf(5),
+                intArrayOf(0),
+                intArrayOf(5),
+                intArrayOf(),
+                intArrayOf(),
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

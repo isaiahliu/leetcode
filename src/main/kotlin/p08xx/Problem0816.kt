57897762404c469b1adaa75b9c75755e3c5cf740
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-17 17:51:19 +0800

    Problem 0816

---
+++ src/main/kotlin/p08xx/Problem0816.kt
@@ -0,0 +1,63 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun ambiguousCoordinates(s: String): List<String> {
+            val cache = hashMapOf<String, List<String>>()
+
+            fun String.validInteger(): Boolean {
+                return this.length == 1 || this[0] != '0'
+            }
+
+            fun String.validDecimal(): Boolean {
+                return this[this.lastIndex] != '0'
+            }
+
+            fun process(str: String): List<String> {
+                if (str in cache) {
+                    return cache[str].orEmpty()
+                }
+
+                val result = arrayListOf<String>()
+
+                if (str.validInteger()) {
+                    result.add(str)
+                }
+
+                for (i in 1 until str.length) {
+                    val int = str.substring(0 until i)
+                    val dec = str.substring(i until str.length)
+
+                    if (int.validInteger() && dec.validDecimal()) {
+                        result.add("${int}.${dec}")
+                    }
+                }
+
+                return result
+            }
+
+            val result = arrayListOf<String>()
+
+            for (i in 1 until s.lastIndex - 1) {
+                val left = process(s.substring(1..i))
+                val right = process(s.substring(i + 1 until s.lastIndex))
+
+                left.forEach { l ->
+                    right.forEach { r ->
+                        result += "(${l}, ${r})"
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().ambiguousCoordinates(
+            "(123)"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

d855163a69ceed906cef847e74a8e5a1b7e4e1af
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-17 16:59:31 +0800

    Problem 0813

---
+++ src/main/kotlin/p08xx/Problem0813.kt
@@ -0,0 +1,36 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun largestSumOfAverages(nums: IntArray, k: Int): Double {
+            for (i in 1 until nums.size) {
+                nums[i] += nums[i - 1]
+            }
+
+            val dp = Array(nums.size) {
+                DoubleArray(k)
+            }
+
+            nums.indices.forEach { index ->
+                dp[index][0] = nums[index].toDouble() / (index + 1)
+
+                for (group in 1 until k.coerceAtMost(index + 1)) {
+                    for (i in group..index) {
+                        dp[index][group] =
+                            dp[index][group].coerceAtLeast(dp[i - 1][group - 1] + (nums[index] - nums[i - 1]).toDouble() / (index - i + 1))
+                    }
+                }
+            }
+
+            return dp[nums.lastIndex][k - 1]
+        }
+    }
+
+    measureTimeMillis {
+        Solution().largestSumOfAverages(
+            intArrayOf(4, 1, 7, 5, 6, 2, 3), 4
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

d125de71720e56f56d8792aa2b04f05a4d97f2e2
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-17 16:39:38 +0800

    Problem 0812

---
+++ src/main/kotlin/p08xx/Problem0812.kt
@@ -0,0 +1,43 @@
+package p08xx
+
+import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun largestTriangleArea(points: Array<IntArray>): Double {
+            fun calculateSquare(i: Int, j: Int, k: Int): Double {
+                val (x1, y1) = points[i]
+                val (x2, y2) = points[j]
+                val (x3, y3) = points[k]
+
+                return 0.5 * (x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2).absoluteValue
+            }
+
+            val n = points.size
+            var result = 0.0
+            for (i in 0 until n) {
+                for (j in i + 1 until n) {
+                    for (k in j + 1 until n) {
+                        result = result.coerceAtLeast(
+                            calculateSquare(i, j, k)
+                        )
+                    }
+                }
+            }
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().largestTriangleArea(
+            arrayOf(
+                intArrayOf(0, 0),
+                intArrayOf(0, 1),
+                intArrayOf(1, 0),
+                intArrayOf(0, 2),
+                intArrayOf(2, 0),
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

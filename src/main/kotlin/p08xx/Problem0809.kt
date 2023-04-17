03c55838e24a1782a3eb46c54e33a9a7324fe862
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-17 15:49:57 +0800

    Problem 0809

---
+++ src/main/kotlin/p08xx/Problem0809.kt
@@ -0,0 +1,57 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun expressiveWords(s: String, words: Array<String>): Int {
+            fun String.match(target: String): Boolean {
+                var l = 0
+                var r = 0
+
+                //this -> hello
+                //target -> heeelllloo
+                var groupCount = 0
+                var extend = false
+                while (l < this.length || r < target.length) {
+                    val left = this.getOrNull(l)
+                    val right = target.getOrNull(r) ?: return false
+                    when {
+                        left == right -> {
+                            if (extend && groupCount == 2) {
+                                return false
+                            }
+
+                            extend = false
+                            if (right == target.getOrNull(r - 1)) {
+                                groupCount++
+                            } else {
+                                groupCount = 1
+                            }
+                            l++
+                        }
+
+                        right == target.getOrNull(r - 1) -> {
+                            extend = true
+                            groupCount++
+                        }
+
+                        else -> return false
+                    }
+                    r++
+                }
+
+                return !extend || groupCount != 2
+            }
+
+            return words.count { it.match(s) }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().expressiveWords(
+            "dddiiiinnssssssoooo",
+            arrayOf("dinnssoo", "ddinso", "ddiinnso", "ddiinnssoo", "ddiinso", "dinsoo", "ddiinsso", "dinssoo", "dinso")
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

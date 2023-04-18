45a99f2c7d3ade60157d4bd7b424328102881f80
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-18 17:42:31 +0800

    Problem 0838

---
+++ src/main/kotlin/p08xx/Problem0838.kt
@@ -0,0 +1,68 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun pushDominoes(dominoes: String): String {
+            val chars = dominoes.toCharArray()
+
+            var right = hashSetOf<Int>()
+            var left = hashSetOf<Int>()
+
+            chars.forEachIndexed { index, c ->
+                when (c) {
+                    'L' -> {
+                        if (!right.remove(index)) {
+                            left.add(index)
+                        }
+                    }
+
+                    'R' -> {
+                        if (!left.remove(index)) {
+                            right.add(index)
+                        }
+                    }
+                }
+            }
+
+            while (left.isNotEmpty() || right.isNotEmpty()) {
+                val newRight = hashSetOf<Int>()
+                val newLeft = hashSetOf<Int>()
+
+                right.forEach {
+                    if (chars.getOrNull(it + 1) == '.') {
+                        newRight.add(it + 1)
+                    }
+                }
+
+                left.forEach {
+                    if (chars.getOrNull(it - 1) == '.') {
+                        if (!newRight.remove(it - 1)) {
+                            newLeft.add(it - 1)
+                        }
+                    }
+                }
+
+                newRight.forEach {
+                    chars[it] = 'R'
+                }
+
+                newLeft.forEach {
+                    chars[it] = 'L'
+                }
+
+                right = newRight
+                left = newLeft
+            }
+
+            return String(chars)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().pushDominoes(
+            "RR.L"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

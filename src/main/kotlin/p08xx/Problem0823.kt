8d3115c0eb43b18312709ee08483fad957dd8241
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-18 11:28:16 +0800

    Problem 0823

---
+++ src/main/kotlin/p08xx/Problem0823.kt
@@ -0,0 +1,42 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numFactoredBinaryTrees(arr: IntArray): Int {
+            val m = 1000000007
+            arr.sort()
+
+            val results = hashMapOf<Int, Long>()
+
+            arr.forEach { num ->
+                var result = 1L
+
+                for (left in arr) {
+                    if (left * left > num) {
+                        break
+                    }
+
+                    val right = num / left
+
+                    if (left * right == num && right in results) {
+                        val c = if (left == right) 1 else 2
+
+                        result += (results[left] ?: 0L) * (results[right] ?: 0L) * c % m
+                    }
+                }
+
+                results[num] = result % m
+            }
+
+            return (results.values.sum() % m).toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numFactoredBinaryTrees(
+            intArrayOf()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

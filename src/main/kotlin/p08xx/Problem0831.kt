f330eb90d32f66086a13531ca0d2f4a3b2be8e25
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-18 14:25:23 +0800

    Problem 0831

---
+++ src/main/kotlin/p08xx/Problem0831.kt
@@ -0,0 +1,32 @@
+package p08xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maskPII(s: String): String {
+            "(\\w+)@(\\w+\\.\\w+)".toRegex().matchEntire(s)?.also {
+                val (name, domain) = it.groupValues.drop(1).map { it.lowercase(Locale.getDefault()) }
+
+                return "${name[0]}*****${name[name.lastIndex]}@${domain}"
+            }
+
+            "(\\d{0,3})\\d{6}(\\d{4})".toRegex().matchEntire(s.replace("\\D".toRegex(), ""))?.also {
+                val (area, last4) = it.groupValues.drop(1)
+
+                return "${
+                    area.takeIf { it.isNotEmpty() }?.let { "+${"*".repeat(it.length)}-" }.orEmpty()
+                }***-***-${last4}"
+            }
+
+            return s
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maskPII(
+            "1(234)567-8901"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

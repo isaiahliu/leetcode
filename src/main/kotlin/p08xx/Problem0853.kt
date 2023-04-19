2a5448d60c0c27aadd02e71da0eee507032d66bf
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-19 17:40:57 +0800

    Problem 0853

---
+++ src/main/kotlin/p08xx/Problem0853.kt
@@ -0,0 +1,60 @@
+package p08xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
+            val map = TreeMap<Int, Int>()
+
+            position.forEachIndexed { index, p ->
+                map[p] = speed[index]
+            }
+
+            var result = speed.size
+            while (map.isNotEmpty()) {
+                var current = target
+                var finishedHour = -1.0
+
+                while (true) {
+                    val (p, s) = map.lowerEntry(current) ?: break
+
+                    map.remove(p)
+
+                    val nextP = p + s
+                    when {
+                        map.higherKey(p)?.takeIf { it <= nextP } != null -> {
+                            result--
+                        }
+
+                        nextP >= target -> {
+                            val hour = (target - p).toDouble() / s
+                            if (hour > finishedHour) {
+                                finishedHour = hour
+                            } else {
+                                result--
+                            }
+                        }
+
+                        else -> {
+                            map[nextP] = s
+                        }
+                    }
+
+                    current = p
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().carFleet(
+            31,
+            intArrayOf(5, 26, 18, 25, 29, 21, 22, 12, 19, 6),
+            intArrayOf(7, 6, 6, 4, 3, 4, 9, 7, 6, 4),
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

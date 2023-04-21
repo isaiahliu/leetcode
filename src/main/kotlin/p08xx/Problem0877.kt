73a3ab6dfb44ceb6e215c3e69e6ac9491d6a1a9c
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-21 17:04:49 +0800

    Problem 0877

---
+++ src/main/kotlin/p08xx/Problem0877.kt
@@ -0,0 +1,40 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun stoneGame(piles: IntArray): Boolean {
+            val total = piles.sum()
+
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+            fun pick(startIndex: Int, endIndex: Int, remaining: Int): Int {
+                val cacheKey = startIndex to endIndex
+                return when {
+                    startIndex == endIndex -> remaining
+                    cacheKey in cache -> cache[cacheKey] ?: 0
+                    else -> {
+                        val pickLeft = pick(startIndex + 1, endIndex, remaining - piles[startIndex])
+                        val pickRight = pick(startIndex, endIndex - 1, remaining - piles[endIndex])
+
+                        val result = remaining - pickLeft.coerceAtMost(pickRight)
+
+                        cache[cacheKey] = result
+                        result
+                    }
+                }
+            }
+
+            val alice = pick(0, piles.lastIndex, total)
+
+            return alice * 2 > total
+        }
+    }
+
+    measureTimeMillis {
+        Solution().stoneGame(
+            intArrayOf(2, 3)
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

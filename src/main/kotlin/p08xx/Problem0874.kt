38541f0dee5ade6ebe20640c20293eff50a0e90f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-21 15:51:45 +0800

    Problem 0874

---
+++ src/main/kotlin/p08xx/Problem0874.kt
@@ -0,0 +1,59 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun robotSim(commands: IntArray, obstacles: Array<IntArray>): Int {
+            val set = obstacles.map { (x, y) -> x to y }.toSet()
+
+            var x = 0
+            var y = 0
+
+            var deltaX = 0
+            var deltaY = 1
+
+            var result = 0
+            commands.forEach {
+                when (it) {
+                    -2 -> {
+                        val t = deltaY
+                        deltaY = deltaX
+                        deltaX = -t
+                    }
+
+                    -1 -> {
+                        val t = deltaX
+                        deltaX = deltaY
+                        deltaY = -t
+                    }
+
+                    else -> {
+                        for (step in 0 until it) {
+                            x += deltaX
+                            y += deltaY
+
+                            if (x to y in set) {
+                                x -= deltaX
+                                y -= deltaY
+
+                                break
+                            }
+
+                            result = result.coerceAtLeast(x * x + y * y)
+                        }
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().robotSim(
+            intArrayOf(4, -1, 3), arrayOf()
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

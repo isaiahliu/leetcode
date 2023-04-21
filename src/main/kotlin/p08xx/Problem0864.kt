b6889b864fb5f41eb7440b547b7921801285a8cf
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-21 11:50:50 +0800

    Problem 0864

---
+++ src/main/kotlin/p08xx/Problem0864.kt
@@ -0,0 +1,90 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun shortestPathAllKeys(grid: Array<String>): Int {
+            fun Int.pickKey(key: Char): Int {
+                return this or (1 shl (key - 'a'))
+            }
+
+            fun Int.openLock(lock: Char): Int? {
+                return if (this and (1 shl (lock - 'A')) > 0) {
+                    this or (1 shl (lock - 'A' + 6))
+                } else {
+                    null
+                }
+            }
+
+            var allKeys = 0
+
+            val visited = hashSetOf<Pair<Pair<Int, Int>, Int>>()
+            val tasks = hashSetOf<Pair<Pair<Int, Int>, Int>>()
+            loop@ for ((r, row) in grid.withIndex()) {
+                for ((c, p) in row.withIndex()) {
+                    when (p) {
+                        '@' -> {
+                            visited.add(r to c to 0)
+                            tasks.add(r to c to 0)
+                        }
+
+                        in 'a'..'f' -> {
+                            allKeys = allKeys or (1 shl (p - 'a'))
+                        }
+                    }
+                }
+            }
+
+            fun Int.done(): Boolean {
+                return this or allKeys == this
+            }
+
+            var result = 0
+            while (tasks.isNotEmpty()) {
+                tasks.toSet().also { tasks.clear() }.forEach { (pos, status) ->
+                    if (status.done()) {
+                        return result
+                    }
+                    val (r, c) = pos
+
+                    arrayOf(r - 1 to c, r + 1 to c, r to c - 1, r to c + 1).filter {
+                        it.first in grid.indices && it.second in grid[0].indices
+                    }.forEach {
+                        var newStatus = status
+                        when (val p = grid[it.first][it.second]) {
+                            in 'a'..'f' -> {
+                                newStatus = status.pickKey(p)
+                            }
+
+                            in 'A'..'F' -> {
+                                newStatus = status.openLock(p) ?: return@forEach
+                            }
+
+                            '#' -> {
+                                return@forEach
+                            }
+                        }
+
+                        if (visited.add(it to newStatus)) {
+                            tasks.add(it to newStatus)
+                        }
+                    }
+                }
+
+                result++
+            }
+
+            return -1
+        }
+    }
+
+    measureTimeMillis {
+        Solution().shortestPathAllKeys(
+            arrayOf(
+                "@Aa"
+            )
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

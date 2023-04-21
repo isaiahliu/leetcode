b86c39a611cd6d0534c3507bc8b6fc9fa4e02fb8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-21 15:41:49 +0800

    Problem 0873

---
+++ src/main/kotlin/p08xx/Problem0873.kt
@@ -0,0 +1,50 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun lenLongestFibSubseq(arr: IntArray): Int {
+            val nums = arr.toSet()
+
+            val visited = hashSetOf<Pair<Int, Int>>()
+
+            var result = 0
+
+            for (i in arr.indices) {
+                for (j in i + 1 until arr.size) {
+                    var num1 = arr[i]
+                    var num2 = arr[j]
+
+                    var next = num1 + num2
+
+                    if (next !in nums || num2 to next in visited) {
+                        continue
+                    }
+
+                    var size = 2
+
+                    while (next in nums) {
+                        size++
+
+                        visited.add(num2 to next)
+                        num1 = num2
+                        num2 = next
+                        next += num1
+                    }
+
+                    result = result.coerceAtLeast(size)
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().lenLongestFibSubseq(
+            intArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

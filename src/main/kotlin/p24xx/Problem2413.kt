c80207c0dda94dad0798f8bb134db38996134eb4
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-21 09:11:23 +0800

    Problem 2413

---
+++ src/main/kotlin/p24xx/Problem2413.kt
@@ -0,0 +1,18 @@
+package p24xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun smallestEvenMultiple(n: Int): Int {
+            return n * (1 + n % 2)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().smallestEvenMultiple(
+            5
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

8d26c8aa676c40a2456e07b8eb301dd82d0604e8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-23 09:49:17 +0800

    Problem 0878

---
+++ src/main/kotlin/p08xx/Problem0878.kt
@@ -0,0 +1,52 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun nthMagicalNumber(n: Int, a: Int, b: Int): Int {
+            val m = 1000000007
+            if (a == b) {
+                return ((a.toLong() * n) % m).toInt()
+            }
+
+            val gcd = a.toBigInteger().gcd(b.toBigInteger()).toLong()
+
+            val min = (a.coerceAtMost(b)).toLong()
+            val max = (a.coerceAtLeast(b)).toLong()
+
+            if (n == 1) {
+                return min.toInt()
+            }
+
+            fun binarySearch(start: Long, end: Long): Long {
+                val mid = start + (end - start) / 2
+
+                val count = mid / min + mid / max - mid / (min * max / gcd)
+
+                return when {
+                    count < n -> {
+                        binarySearch(mid + 1, end)
+                    }
+
+                    count > n -> {
+                        binarySearch(start, mid - 1)
+                    }
+
+                    else -> {
+                        (mid / min * min).coerceAtLeast(mid / max * max)
+                    }
+                }
+            }
+
+            return (binarySearch(min, (max * n)) % m).toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().nthMagicalNumber(
+            5, 2, 4
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

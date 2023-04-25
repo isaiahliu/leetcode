17f97804e71be182f559d01e13274dc3d38ff1f1
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-25 13:51:12 +0800

    Problem 0886

---
+++ src/main/kotlin/p08xx/Problem0886.kt
@@ -0,0 +1,55 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun possibleBipartition(n: Int, dislikes: Array<IntArray>): Boolean {
+            val dislikeMap = hashMapOf<Int, MutableSet<Int>>()
+
+            dislikes.forEach { (a, b) ->
+                dislikeMap.computeIfAbsent(a) { hashSetOf() }.add(b)
+                dislikeMap.computeIfAbsent(b) { hashSetOf() }.add(a)
+            }
+
+            val groups = Array(2) { hashSetOf<Int>() }
+
+            while (dislikeMap.isNotEmpty()) {
+                val (first, dl) = dislikeMap.entries.first()
+                groups[0].add(first)
+                groups[1].addAll(dl)
+                dislikeMap.remove(first)
+
+                val tasks = dl.toMutableSet()
+
+                var group = 0
+                while (tasks.isNotEmpty()) {
+                    tasks.toSet().also { tasks.clear() }.forEach {
+                        dislikeMap[it]?.forEach {
+                            if (it in groups[1 - group]) {
+                                return false
+                            } else if (groups[group].add(it)) {
+                                tasks.add(it)
+                            }
+                        }
+                        dislikeMap.remove(it)
+                    }
+
+                    group = 1 - group
+                }
+            }
+
+            return true
+        }
+    }
+
+    measureTimeMillis {
+        Solution().possibleBipartition(
+            4, arrayOf(
+                intArrayOf(1, 2),
+                intArrayOf(1, 3),
+                intArrayOf(2, 3),
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

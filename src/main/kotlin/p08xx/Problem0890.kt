c4dc49cd634f5cb9f6db83936bbb6dbd06c796e9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-25 17:24:48 +0800

    Problem 0890

---
+++ src/main/kotlin/p08xx/Problem0890.kt
@@ -0,0 +1,35 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findAndReplacePattern(words: Array<String>, pattern: String): List<String> {
+            fun String.similar(target: String): Boolean {
+                val map1 = hashMapOf<Char, Char>()
+                val map2 = hashMapOf<Char, Char>()
+                indices.forEach {
+                    val l = this[it]
+                    val r = target[it]
+
+                    if (map1[l] == null && map2[r] == null) {
+                        map1[l] = r
+                        map2[r] = l
+                    } else if (map1[l] != r || map2[r] != l) {
+                        return false
+                    }
+
+                }
+                return true
+            }
+
+            return words.filter { it.similar(pattern) }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findAndReplacePattern(
+            arrayOf(), ""
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

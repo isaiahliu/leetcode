87b841128af08af3cad5b328935e6995c79c2495
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-25 17:02:33 +0800

    Problem 0887

---
+++ src/main/kotlin/p08xx/Problem0887.kt
@@ -0,0 +1,82 @@
+package p08xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun superEggDrop(k: Int, n: Int): Int {
+            if (n == 1) {
+                return 1
+            }
+
+            val dp = Array(n) {
+                IntArray(k + 1)
+            }
+
+            repeat(k) {
+                dp[0][it + 1] = 1
+            }
+
+            for (action in 1 until n) {
+                for (egg in 1..k) {
+                    dp[action][egg] = dp[action - 1][egg - 1] + dp[action - 1][egg] + 1
+                }
+
+                if (dp[action][k] >= n) {
+                    return action + 1
+                }
+            }
+
+            return 0
+        }
+
+        fun superEggDrop2(k: Int, n: Int): Int {
+            val dp = Array(n + 1) {
+                IntArray(k) { Int.MAX_VALUE }
+            }
+
+            repeat(k) {
+                dp[0][it] = 0
+            }
+
+            repeat(n) {
+                dp[it + 1][0] = it + 1
+            }
+
+            for (floor in 1..n) {
+                for (egg in 1 until k) {
+                    var left = 0
+                    var right = floor - 1
+                    while (right - left > 1) {
+                        val mid = (left + right) / 2
+
+                        val dp1 = dp[mid][egg]
+                        val dp2 = dp[floor - mid - 1][egg - 1]
+
+                        if (dp1 > dp2) {
+                            right = mid - 1
+                        } else if (dp1 < dp2) {
+                            left = mid + 1
+                        } else {
+                            left = mid
+                            right = mid
+                        }
+                    }
+
+                    dp[floor][egg] = dp[left][egg].coerceAtLeast(dp[floor - left - 1][egg - 1])
+                        .coerceAtMost(dp[right][egg].coerceAtLeast(dp[floor - right - 1][egg - 1])) + 1
+                }
+            }
+
+            return dp[n][k - 1]
+        }
+    }
+
+    measureTimeMillis {
+        Solution().superEggDrop(3, 6).also { println(it) }
+        Solution().superEggDrop(2, 2).also { println(it) }
+        Solution().superEggDrop(2, 6).also { println(it) }
+        Solution().superEggDrop(2, 9).also { println(it) }
+        Solution().superEggDrop(3, 14).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

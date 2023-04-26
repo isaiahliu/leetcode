42f2f5291354f90a4646002feb436f9d55d77cdb
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-26 17:31:12 +0800

    Problem 0908

---
+++ src/main/kotlin/p09xx/Problem0908.kt
@@ -0,0 +1,25 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun smallestRangeI(nums: IntArray, k: Int): Int {
+            var min = Int.MAX_VALUE
+            var max = Int.MIN_VALUE
+
+            nums.forEach {
+                min = min.coerceAtMost(it)
+                max = max.coerceAtLeast(it)
+            }
+
+            return (max - min - k * 2).coerceAtLeast(0)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().smallestRangeI(
+            intArrayOf(71, 55, 82, 55), 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

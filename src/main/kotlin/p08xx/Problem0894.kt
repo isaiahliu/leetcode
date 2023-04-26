c60a28c113bf307bdfb844582d7a5d372df1a847
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-26 11:00:56 +0800

    Problem 0894

---
+++ src/main/kotlin/p08xx/Problem0894.kt
@@ -0,0 +1,45 @@
+package p08xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        val cache = hashMapOf<Int, List<TreeNode>>()
+
+        fun allPossibleFBT(n: Int): List<TreeNode?> {
+            if (n % 2 == 0) {
+                return emptyList()
+            }
+
+            if (n == 1) {
+                return listOf(TreeNode(0))
+            }
+
+            if (n in cache) {
+                return cache[n].orEmpty()
+            }
+
+            return (1 until n step 2).map { leftCount ->
+                val rightCount = n - leftCount - 1
+
+                allPossibleFBT(leftCount).map { l ->
+                    allPossibleFBT(rightCount).map { r ->
+                        TreeNode(0).also {
+                            it.left = l
+                            it.right = r
+                        }
+                    }
+                }.flatten()
+            }.flatten().also {
+                cache[n] = it
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().allPossibleFBT(
+            5
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

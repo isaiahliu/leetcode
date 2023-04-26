df86fb259a20f099beb70d5a408843ce3e15a7ae
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-26 16:51:05 +0800

    Problem 0906

---
+++ src/main/kotlin/p09xx/Problem0906.kt
@@ -0,0 +1,69 @@
+package p09xx
+
+import kotlin.math.sqrt
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun superpalindromesInRange(left: String, right: String): Int {
+            fun Long.isPalindrome(): Boolean {
+                return this.toString().let { it == it.reversed() }
+            }
+
+            fun Long.nextPalindrome(): Long {
+                val numStr = this.toString()
+                return if (numStr == numStr.reversed()) {
+                    var newNum = this
+
+                    if (numStr.length == 1) {
+                        newNum++
+                    } else {
+                        val addNum = ("1${"0".repeat(numStr.length / 2)}").toLong()
+
+                        newNum = (this / addNum + 1) * addNum
+
+                    }
+                    if (newNum.isPalindrome()) {
+                        newNum
+                    } else {
+                        newNum.nextPalindrome()
+                    }
+                } else {
+                    val firstHalf = numStr.take(numStr.length / 2)
+
+                    val newNum = "${firstHalf}${
+                        numStr.drop(firstHalf.length).dropLast(firstHalf.length)
+                    }${firstHalf.reversed()}".toLong()
+
+                    if (newNum > this) {
+                        newNum
+                    } else {
+                        newNum.nextPalindrome()
+                    }
+                }
+            }
+
+            var t = sqrt(left.toDouble()).toLong() - 1
+            val rightLong = right.toLong()
+            var result = 0
+            while (true) {
+                t = t.nextPalindrome()
+
+                val pow = 1L * t * t
+                if (pow > rightLong) {
+                    break
+                } else if (pow >= left.toLong() && pow.isPalindrome()) {
+                    result++
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().superpalindromesInRange(
+            "1", "2"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

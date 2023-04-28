b223bea0c9a96063fba473360c6a1bcad5116630
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-28 14:32:46 +0800

    Problem 0925

---
+++ src/main/kotlin/p09xx/Problem0925.kt
@@ -0,0 +1,41 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isLongPressedName(name: String, typed: String): Boolean {
+            var l = 0
+            var r = 0
+
+            fun moveR(): Boolean {
+                var moved = false
+                while (r < typed.length && typed[r] == typed.getOrNull(r - 1)) {
+                    moved = true
+                    r++
+                }
+
+                return moved
+            }
+
+            while (l < name.length && r < typed.length) {
+                if (name[l] == typed[r]) {
+                    l++
+                    r++
+                } else if (!moveR()) {
+                    return false
+                }
+            }
+
+            moveR()
+
+            return l == name.length && r == typed.length
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isLongPressedName(
+            "", ""
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

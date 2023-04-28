2f251579a9239e6a54e94323e2cea8229a13f47a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-28 17:59:54 +0800

    Problem 0935

---
+++ src/main/kotlin/p09xx/Problem0935.kt
@@ -0,0 +1,50 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun knightDialer(n: Int): Int {
+            val m = 1000000007L
+
+            val dp = Array(n) { i ->
+                LongArray(10) {
+                    if (i == 0) {
+                        1L
+                    } else {
+                        0L
+                    }
+                }
+            }
+
+            val moves = arrayOf(
+                intArrayOf(4, 6),
+                intArrayOf(6, 8),
+                intArrayOf(7, 9),
+                intArrayOf(4, 8),
+                intArrayOf(3, 9, 0),
+                intArrayOf(),
+                intArrayOf(1, 7, 0),
+                intArrayOf(2, 6),
+                intArrayOf(1, 3),
+                intArrayOf(2, 4),
+            )
+            for (turn in 1 until n) {
+                moves.forEachIndexed { index, froms ->
+                    froms.forEach {
+                        dp[turn][index] += dp[turn - 1][it]
+                    }
+
+                    dp[turn][index] %= m
+                }
+            }
+
+            return (dp[dp.lastIndex].sum() % m).toInt()
+        }
+    }
+    measureTimeMillis {
+        Solution().knightDialer(
+            1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

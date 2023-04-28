5d83cf5071b96458f5ed425578ade33d19061209
isaiahliu <ziqiang.liu@enoch-car.com>
2023-04-28 15:03:09 +0800

    Problem 0926

---
+++ src/main/kotlin/p09xx/Problem0926.kt
@@ -0,0 +1,39 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minFlipsMonoIncr(s: String): Int {
+            val oneCounts = IntArray(s.length + 1)
+            val zeroCounts = IntArray(s.length + 1)
+
+            val sum = IntArray(2)
+            for (i in s.indices) {
+                sum[0] += '1' - s[s.lastIndex - i]
+                sum[1] += s[i] - '0'
+
+                zeroCounts[s.lastIndex - i] = sum[0]
+                oneCounts[i + 1] = sum[1]
+            }
+
+            if (sum[0] == 0 || sum[1] == 0) {
+                return 0
+            }
+
+            var result = Int.MAX_VALUE
+
+            for (i in oneCounts.indices) {
+                result = result.coerceAtMost(zeroCounts[i] + oneCounts[i])
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minFlipsMonoIncr(
+            "00011000"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

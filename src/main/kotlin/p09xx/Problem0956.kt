c42c33c2f4f8fa5e05f0874e5f1468b28a137ef6
isaiahliu <isaiahliu@outlook.com>
2023-05-15 13:38:57 +0700

    Problem 0956

---
+++ src/main/kotlin/p09xx/Problem0956.kt
@@ -0,0 +1,34 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun tallestBillboard(rods: IntArray): Int {
+            val map = hashMapOf(0 to 0)
+
+            for (num in rods) {
+                map.toMap().forEach { (key, value) ->
+                    val l = key - num
+                    val r = key + num
+
+                    val score = value + num
+                    if (map[l]?.takeIf { it > score } == null) {
+                        map[l] = score
+                    }
+                    if (map[r]?.takeIf { it > score } == null) {
+                        map[r] = score
+                    }
+                }
+            }
+
+            return (map[0] ?: 0) / 2
+        }
+    }
+
+    measureTimeMillis {
+        Solution().tallestBillboard(
+            intArrayOf(1, 2, 3, 4, 5, 6)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

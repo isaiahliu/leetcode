d89ebacf1d1dcc8e97477afad7ae53cdf4489051
isaiahliu <isaiahliu@outlook.com>
2023-05-20 14:32:22 +0700

    Problem 0963

---
+++ src/main/kotlin/p09xx/Problem0963.kt
@@ -0,0 +1,49 @@
+package p09xx
+
+import kotlin.math.sqrt
+import kotlin.system.measureTimeMillis
+
+
+fun main() {
+    class Solution {
+        fun minAreaFreeRect(points: Array<IntArray>): Double {
+            val set = points.map { it[0] to it[1] }.toSet()
+
+            var result = Double.MAX_VALUE
+            for (i in points.indices) {
+                val (x1, y1) = points[i]
+
+                for (j in i + 1 until points.size) {
+                    val (x2, y2) = points[j]
+
+                    for (k in j + 1 until points.size) {
+                        val (x3, y3) = points[k]
+                        val p4 = x2 + x3 - x1 to y2 + y3 - y1
+
+                        if (p4 in set) {
+                            val dot: Int = (x2 - x1) * (x3 - x1) +
+                                    (y2 - y1) * (y3 - y1)
+                            if (dot == 0) {
+                                result =
+                                    result.coerceAtMost(sqrt(((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)).toDouble() * ((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1))))
+                            }
+                        }
+                    }
+                }
+            }
+
+            return if (result < Double.MAX_VALUE) result else 0.0
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minAreaFreeRect(
+            arrayOf(
+                intArrayOf(1, 2),
+                intArrayOf(2, 1),
+                intArrayOf(1, 0),
+                intArrayOf(0, 1),
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

f83e16193228e66b391bd2e4cc07609666ce1004
isaiahliu <isaiahliu@outlook.com>
2023-05-20 15:48:00 +0700

    Problem 0964

---
+++ src/main/kotlin/p09xx/Problem0964.kt
@@ -0,0 +1,51 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun leastOpsExpressTarget(x: Int, target: Int): Int {
+            if (x == 1) {
+                return target - 1
+            }
+
+            fun dfs(num: Int): Int {
+                return when {
+                    num < x -> {
+                        (2 * num - 1).coerceAtMost((x - num) * 2)
+                    }
+
+                    num == 0 -> {
+                        0
+                    }
+
+                    else -> {
+                        var p = 1
+                        var sum = x.toLong()
+                        while (sum * x <= num) {
+                            sum *= x
+                            p++
+                        }
+
+                        var min = dfs(num - sum.toInt()) + p
+
+                        if (sum * x - num < num) {
+                            min = min.coerceAtMost(p + 1 + dfs(sum.toInt() * x - num))
+                        }
+
+                        min
+                    }
+                }
+
+            }
+            return dfs(target)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().leastOpsExpressTarget(
+            79,
+            155800339
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

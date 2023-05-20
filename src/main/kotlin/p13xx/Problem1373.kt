6be4795ac16490af4f9273b2aa5c3c266134f51c
isaiahliu <isaiahliu@outlook.com>
2023-05-20 09:59:46 +0700

    Problem 1373

---
+++ src/main/kotlin/p13xx/Problem1373.kt
@@ -0,0 +1,57 @@
+package p13xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxSumBST(root: TreeNode?): Int {
+            var result = 0
+
+            fun TreeNode.dfs(): Pair<Pair<Int, Int>, Int>? {
+                var minValue = `val`
+                var maxValue = `val`
+
+                val leftResult = left?.dfs()
+                val rightResult = right?.dfs()
+
+                var sum = `val`
+                left?.also {
+                    leftResult?.also { (m, s) ->
+                        if (`val` <= m.second) {
+                            return null
+                        } else {
+                            minValue = minValue.coerceAtMost(m.first)
+                            sum += s
+                        }
+                    } ?: return null
+                }
+
+                right?.also {
+                    rightResult?.also { (m, s) ->
+                        if (`val` >= m.first) {
+                            return null
+                        } else {
+                            maxValue = maxValue.coerceAtLeast(m.second)
+                            sum += s
+                        }
+                    } ?: return null
+                }
+
+                result = result.coerceAtLeast(sum)
+
+                return (minValue to maxValue to sum)
+            }
+
+            root?.dfs()
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxSumBST(
+            null
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

be94e5edffe8fa28b3acada11335e4cd524a3fba
isaiahliu <isaiahliu@outlook.com>
2023-05-20 12:01:50 +0700

    Problem 0960

---
+++ src/main/kotlin/p09xx/Problem0960.kt
@@ -0,0 +1,39 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minDeletionSize(strs: Array<String>): Int {
+            fun lte(index1: Int, index2: Int): Boolean {
+                return strs.all {
+                    it[index1] <= it[index2]
+                }
+            }
+
+            val dp = IntArray(strs[0].length) { 1 }
+
+            for (i in 1 until strs[0].length) {
+                var max = 0
+
+                for (target in i - 1 downTo 0) {
+                    if (dp[target] > max && lte(target, i)) {
+                        max = dp[target]
+                    }
+                }
+
+                dp[i] = max + 1
+            }
+
+            return strs[0].length - dp.max()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minDeletionSize(
+            arrayOf(
+                "babca", "bbazb"
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

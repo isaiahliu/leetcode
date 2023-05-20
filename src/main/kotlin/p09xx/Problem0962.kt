5220407eab171f0af22760b95b66532ed9d73cf7
isaiahliu <isaiahliu@outlook.com>
2023-05-20 13:48:44 +0700

    Problem 0962

---
+++ src/main/kotlin/p09xx/Problem0962.kt
@@ -0,0 +1,41 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxWidthRamp(nums: IntArray): Int {
+            fun binarySearch(startIndex: Int, endIndex: Int, target: Int): Int {
+                if (startIndex > endIndex) {
+                    return Int.MAX_VALUE
+                }
+
+                val midIndex = (startIndex + endIndex) / 2
+                val midNum = nums[midIndex]
+
+                return if (midNum > target) {
+                    binarySearch(midIndex + 1, endIndex, target)
+                } else {
+                    midIndex.coerceAtMost(binarySearch(startIndex, midIndex - 1, target))
+                }
+            }
+
+            var result = 0
+            for (i in 1 until nums.size) {
+                if (nums[i] >= nums[i - 1]) {
+                    result = result.coerceAtLeast(i - binarySearch(0, i - 1, nums[i]))
+
+                    nums[i] = nums[i - 1]
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxWidthRamp(
+            intArrayOf(6, 0, 8, 2, 1, 5)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

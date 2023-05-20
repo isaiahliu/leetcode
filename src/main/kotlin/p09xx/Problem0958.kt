f88793149cfd3d920b0b03a7033fe6a5672728ce
isaiahliu <isaiahliu@outlook.com>
2023-05-20 10:32:03 +0700

    Problem 0958

---
+++ src/main/kotlin/p09xx/Problem0958.kt
@@ -0,0 +1,53 @@
+package p09xx
+
+import util.TreeNode
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isCompleteTree(root: TreeNode?): Boolean {
+            val bfs = LinkedList<TreeNode>()
+
+            root?.also { bfs.add(it) }
+
+            var lastLevel = bfs.isEmpty()
+            while (!lastLevel) {
+                repeat(bfs.size) {
+                    val n = bfs.pop()
+
+                    val left = n.left
+                    val right = n.right
+
+                    if (left != null) {
+                        if (lastLevel) {
+                            return false
+                        } else {
+                            bfs.add(left)
+                        }
+                    } else {
+                        lastLevel = true
+                    }
+
+                    if (right != null) {
+                        if (lastLevel) {
+                            return false
+                        } else {
+                            bfs.add(right)
+                        }
+                    } else {
+                        lastLevel = true
+                    }
+                }
+            }
+
+            return bfs.all { it.left == null && it.right == null }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isCompleteTree(
+            null
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

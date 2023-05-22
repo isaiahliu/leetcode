a7ede8db9f3df87fd4f2aedf34dbfe728ff1007a
isaiahliu <isaiahliu@outlook.com>
2023-05-22 13:45:12 +0700

    Problem 0972

---
+++ src/main/kotlin/p09xx/Problem0972.kt
@@ -0,0 +1,98 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isRationalEqual(s: String, t: String): Boolean {
+            class Num(str: String) {
+                var integer: Int = 0
+                var nonRepeating: String = ""
+                var repeating: String = ""
+
+                init {
+                    var step = 0
+
+                    str.forEach {
+                        when (it) {
+                            '.' -> step = 1
+                            '(', ')' -> step = 2
+                            else -> {
+                                when (step) {
+                                    0 -> {
+                                        integer *= 10
+                                        integer += it - '0'
+                                    }
+
+                                    1 -> {
+                                        nonRepeating += it
+                                    }
+
+                                    2 -> {
+                                        repeating += it
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    while (nonRepeating.isNotEmpty() && repeating.isNotEmpty() && nonRepeating[nonRepeating.lastIndex] == repeating[repeating.lastIndex]) {
+                        nonRepeating = nonRepeating.dropLast(1)
+
+                        repeating = repeating.takeLast(1) + repeating.dropLast(1)
+                    }
+
+                    loop@ for (repeatLength in 1..repeating.length / 2) {
+                        if (repeating.length % repeatLength == 0) {
+                            for (times in 1 until repeating.length / repeatLength) {
+                                for (index in 0 until repeatLength) {
+                                    if (repeating[index] != repeating[repeatLength * times + index]) {
+                                        continue@loop
+                                    }
+                                }
+                            }
+
+                            repeating = repeating.take(repeatLength)
+
+                            break
+                        }
+                    }
+
+                    if (repeating == "9") {
+                        if (nonRepeating.isEmpty()) {
+                            integer++
+                        } else {
+                            nonRepeating = (nonRepeating.toInt() + 1).toString().padStart(nonRepeating.length, '0')
+                        }
+
+                        repeating = ""
+                    }
+
+                    if (repeating == "0") {
+                        repeating = ""
+                    }
+
+                    if (repeating.isEmpty() && nonRepeating.toIntOrNull() == 0) {
+                        nonRepeating = ""
+                    }
+                }
+
+                override fun toString(): String {
+                    return "${integer}.${nonRepeating}(${repeating})"
+                }
+
+                fun sameAs(target: Num): Boolean {
+                    return integer == target.integer && nonRepeating == target.nonRepeating && repeating == target.repeating
+                }
+            }
+
+            return Num(s).also { println(it) }.sameAs(Num(t).also { println(it) })
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isRationalEqual(
+            "1.0", "1"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

fb1819449cbcb20b5541240016d5be3f63bfe6c1
isaiahliu <isaiahliu@outlook.com>
2023-05-22 17:27:09 +0700

    Problem 0977

---
+++ src/main/kotlin/p09xx/Problem0977.kt
@@ -0,0 +1,43 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun sortedSquares(nums: IntArray): IntArray {
+            var l = -1
+            var r = 0
+
+            while (r < nums.size && nums[r] < 0) {
+                l++
+                r++
+            }
+
+            return IntArray(nums.size) {
+                when {
+                    l < 0 -> {
+                        nums[r] * nums[r++]
+                    }
+
+                    r == nums.size -> {
+                        nums[l] * nums[l--]
+                    }
+
+                    -nums[l] < nums[r] -> {
+                        nums[l] * nums[l--]
+                    }
+
+                    else -> {
+                        nums[r] * nums[r++]
+                    }
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().sortedSquares(
+            intArrayOf(-3, -2, 0, 1, 2)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

4942d2f2328afb9f78185307d2ebb54a464f525e
isaiahliu <isaiahliu@outlook.com>
2023-05-22 12:48:08 +0700

    Problem 0969

---
+++ src/main/kotlin/p09xx/Problem0969.kt
@@ -0,0 +1,50 @@
+package p09xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun pancakeSort(arr: IntArray): List<Int> {
+            val pos = IntArray(arr.size)
+            arr.forEachIndexed { index, i ->
+                pos[i - 1] = index
+            }
+            val result = arrayListOf<Int>()
+
+            fun switch(length: Int) {
+                result.add(length)
+
+                for (i in 0..(length - 1) / 2) {
+                    val left = arr[i]
+                    val right = arr[length - 1 - i]
+
+                    arr[i] = right
+                    arr[length - 1 - i] = left
+
+                    pos[left - 1] = length - 1 - i
+                    pos[right - 1] = i
+                }
+            }
+
+            for (num in arr.lastIndex downTo 1) {
+                if (pos[num] == num) {
+                    continue
+                }
+
+                if (pos[num] > 0) {
+                    switch(pos[num] + 1)
+                }
+
+                switch(num + 1)
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().pancakeSort(
+            intArrayOf(3, 2, 4, 1)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

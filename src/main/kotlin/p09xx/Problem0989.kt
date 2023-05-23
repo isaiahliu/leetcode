f193ac820dc525faebbd03ca65f9d667847dd42d
isaiahliu <isaiahliu@outlook.com>
2023-05-23 16:48:45 +0700

    Problem 0989

---
+++ src/main/kotlin/p09xx/Problem0989.kt
@@ -0,0 +1,36 @@
+package p09xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun addToArrayForm(num: IntArray, k: Int): List<Int> {
+            var n = 0
+
+            var t = k
+
+            val result = LinkedList<Int>()
+
+            for (i in num.lastIndex downTo 0) {
+                n += num[i] + (t % 10)
+
+                result.push(n % 10)
+                n /= 10
+                t /= 10
+            }
+
+            if (n > 0) {
+                result.push(n)
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().addToArrayForm(
+            intArrayOf(1, 2, 0, 0), 34
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

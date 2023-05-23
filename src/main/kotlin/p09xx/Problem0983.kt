a67f31d1fef3486e393fa3ae00910bab98cbbdf3
isaiahliu <isaiahliu@outlook.com>
2023-05-23 15:16:04 +0700

    Problem 0983

--- src/main/kotlin/p09xx/Problem0983.kt
+++ src/main/kotlin/p09xx/Problem0983.kt
@@ -9,32 +9,32 @@ fun main() {
             val map = TreeMap<Int, Int>()
             map[0] = 0
 
-            fun schedule(toDay: Int, totalCost: Int) {
-                while (true) {
-                    map.lowerEntry(toDay)?.takeIf { it.value >= totalCost }?.key?.also {
-                        map.remove(it)
-                    } ?: break
-                }
+            val periods = arrayOf(0, 6, 29)
 
-                if (map.higherEntry(toDay - 1)?.value?.takeIf { it < totalCost } == null) {
-                    map[toDay] = totalCost
+            fun schedule(today: Int, costed: Int) {
+                periods.forEachIndexed { index, p ->
+                    val endDay = today + p
+                    val totalCost = costed + costs[index]
+
+                    while (true) {
+                        map.lowerEntry(endDay)?.takeIf { it.value >= totalCost }?.key?.also {
+                            map.remove(it)
+                        } ?: break
+                    }
+
+                    if (map.higherEntry(endDay - 1)?.value?.takeIf { it < totalCost } == null) {
+                        map[endDay] = totalCost
+                    }
                 }
             }
 
-            schedule(days[0], costs[0])
-            schedule(days[0] + 6, costs[1])
-            schedule(days[0] + 29, costs[2])
+            schedule(days[0], 0)
 
             for (i in 1 until days.size) {
-                val lastDay = days[i - 1]
-
-                val (d, cost) = map.higherEntry(lastDay - 1) ?: return -1
-
                 val today = days[i]
-                if (d < today) {
-                    schedule(today, cost + costs[0])
-                    schedule(today + 6, cost + costs[1])
-                    schedule(today + 29, cost + costs[2])
+
+                map.higherEntry(days[i - 1] - 1)?.takeIf { it.key < today }?.value?.also {
+                    schedule(today, it)
                 }
             }
 

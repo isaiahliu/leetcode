523246a8c4ef629fb542de987ccca2691c3fc4f6
isaiahliu <isaiahliu@outlook.com>
2023-05-24 19:55:05 +0700

    Problem 0996

---
+++ src/main/kotlin/p09xx/Problem0996.kt
@@ -0,0 +1,58 @@
+package p09xx
+
+import kotlin.math.sqrt
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numSquarefulPerms(nums: IntArray): Int {
+            nums.sort()
+
+            fun isSquare(a: Int, b: Int): Boolean {
+                return (a + b).let {
+                    sqrt(it.toDouble()).toInt().let { it * it } == it
+                }
+            }
+
+            val connections = hashMapOf<Int, MutableSet<Int>>()
+
+            for (i in nums.indices) {
+                for (j in i + 1 until nums.size) {
+                    if (isSquare(nums[i], nums[j])) {
+                        connections.computeIfAbsent(i) { hashSetOf() }.add(j)
+                        connections.computeIfAbsent(j) { hashSetOf() }.add(i)
+                    }
+                }
+            }
+
+            var result = 0
+            fun dfs(index: Int, route: Set<Int>) {
+                if (route.size == nums.size) {
+                    result++
+                } else {
+                    connections[index]?.forEach {
+                        if (it !in route) {
+                            if (nums[it] != nums.getOrNull(it - 1) || it - 1 in route) {
+                                dfs(it, route + it)
+                            }
+                        }
+                    }
+                }
+            }
+
+            repeat(nums.size) {
+                if (nums[it] != nums.getOrNull(it - 1)) {
+                    dfs(it, setOf(it))
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numSquarefulPerms(
+            intArrayOf(2, 2, 2)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

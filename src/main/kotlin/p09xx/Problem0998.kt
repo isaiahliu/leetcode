b9520a39515e0d6d7bc54ee958f6bff42e529400
isaiahliu <isaiahliu@outlook.com>
2023-05-24 20:40:26 +0700

    Problem 0998

---
+++ src/main/kotlin/p09xx/Problem0998.kt
@@ -0,0 +1,34 @@
+package p09xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun insertIntoMaxTree(root: TreeNode?, `val`: Int): TreeNode {
+            val newNode = TreeNode(`val`)
+
+            return if (root == null || `val` > root.`val`) {
+                newNode.also { it.left = root }
+            } else {
+                fun TreeNode.dfs() {
+                    if (right?.takeIf { it.`val` > `val` } == null) {
+                        right = newNode.also { it.left = right }
+                    } else {
+                        right?.dfs()
+                    }
+                }
+
+                root.dfs()
+
+                root
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().insertIntoMaxTree(
+            null, 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

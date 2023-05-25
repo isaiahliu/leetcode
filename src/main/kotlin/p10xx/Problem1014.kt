12fc332d91e2a535b1933d97b9e07cc67e78e216
isaiahliu <isaiahliu@outlook.com>
2023-05-25 14:17:52 +0700

    Problem 1014

---
+++ src/main/kotlin/p10xx/Problem1014.kt
@@ -0,0 +1,30 @@
+package p10xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxScoreSightseeingPair(values: IntArray): Int {
+            var bestScore = -9999
+            var bestIndex = -1
+
+            var result = Int.MIN_VALUE
+            values.forEachIndexed { index, score ->
+                result = result.coerceAtLeast(bestScore + score + bestIndex - index)
+
+                if (score - bestScore > bestIndex - index) {
+                    bestScore = score
+                    bestIndex = index
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxScoreSightseeingPair(
+            intArrayOf(1, 2, 2)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

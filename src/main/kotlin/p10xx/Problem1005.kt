797ac25b3b177d8afbb5a934e7db7d8aa7b23919
isaiahliu <isaiahliu@outlook.com>
2023-05-25 11:30:31 +0700

    Problem 1005

---
+++ src/main/kotlin/p10xx/Problem1005.kt
@@ -0,0 +1,52 @@
+package p10xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun largestSumAfterKNegations(nums: IntArray, k: Int): Int {
+            var remaining = k
+            var result = 0
+
+            var min = Int.MAX_VALUE
+
+            nums.sorted().forEach {
+                result += when {
+                    remaining == 0 -> it
+                    it < 0 -> {
+                        remaining--
+                        min = -it
+                        -it
+                    }
+
+                    remaining % 2 == 0 -> {
+                        remaining = 0
+                        it
+                    }
+
+                    it > min -> {
+                        remaining = 0
+                        it - min * 2
+                    }
+
+                    else -> {
+                        remaining = 0
+                        -it
+                    }
+                }
+            }
+
+            if (remaining % 2 == 1) {
+                result -= min * 2
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().largestSumAfterKNegations(
+            intArrayOf(-4, -2, -3), 4
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

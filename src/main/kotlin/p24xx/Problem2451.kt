8f9db5c0b43b2047a48a6b129f6ec03bcdddc363
isaiahliu <isaiahliu@outlook.com>
2023-05-25 09:08:39 +0700

    Problem 2451

--- src/main/kotlin/p24xx/Problem2451.kt
+++ src/main/kotlin/p24xx/Problem2451.kt
@@ -6,12 +6,12 @@ fun main() {
     class Solution {
         fun oddString(words: Array<String>): String {
             for (i in 1 until words[0].length) {
-                val map = words.indices.groupBy {
-                    words[it].let { it[i] - it[i - 1] }
+                val map = words.groupBy {
+                    it[i] - it[i - 1]
                 }
 
                 if (map.size > 1) {
-                    return map.values.firstOrNull { it.size == 1 }?.firstOrNull()?.let { words[it] }.orEmpty()
+                    return map.values.firstOrNull { it.size == 1 }?.firstOrNull().orEmpty()
                 }
             }
 

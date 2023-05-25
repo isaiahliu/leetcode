4c722cbb0cdb2236dcd8da8111962e9668b8df7c
isaiahliu <isaiahliu@outlook.com>
2023-05-25 09:07:47 +0700

    Problem 2451

--- src/main/kotlin/p24xx/Problem2451.kt
+++ src/main/kotlin/p24xx/Problem2451.kt
@@ -6,10 +6,8 @@ fun main() {
     class Solution {
         fun oddString(words: Array<String>): String {
             for (i in 1 until words[0].length) {
-                val map = hashMapOf<Int, MutableSet<Int>>()
-
-                words.forEachIndexed { index, s ->
-                    map.computeIfAbsent(s[i] - s[i - 1]) { hashSetOf() }.add(index)
+                val map = words.indices.groupBy {
+                    words[it].let { it[i] - it[i - 1] }
                 }
 
                 if (map.size > 1) {

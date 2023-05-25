1a5467deab40e50d6d81cd0690643bf065c719b1
isaiahliu <isaiahliu@outlook.com>
2023-05-25 13:50:07 +0700

    Problem 1011

---
+++ src/main/kotlin/p10xx/Problem1011.kt
@@ -0,0 +1,42 @@
+package p10xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun shipWithinDays(weights: IntArray, days: Int): Int {
+            fun binarySearch(min: Int, max: Int): Int {
+                if (min > max) {
+                    return Int.MAX_VALUE
+                }
+
+                val mid = (min + max) / 2
+
+                var d = 1
+                var sum = 0
+                weights.forEach {
+                    sum += it
+
+                    if (sum > mid) {
+                        d++
+                        sum = it
+                    }
+                }
+
+                return if (d > days) {
+                    binarySearch(mid + 1, max)
+                } else {
+                    binarySearch(min, mid - 1).coerceAtMost(mid)
+                }
+            }
+
+            return binarySearch(weights.max(), weights.sum())
+        }
+    }
+
+    measureTimeMillis {
+        Solution().shipWithinDays(
+            intArrayOf(), 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

2cb1d93f4301cc79420f17921dc31b0906cc4524
isaiahliu <isaiahliu@outlook.com>
2023-05-25 12:25:04 +0700

    Problem 1008

---
+++ src/main/kotlin/p10xx/Problem1008.kt
@@ -0,0 +1,39 @@
+package p10xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun bstFromPreorder(preorder: IntArray): TreeNode {
+            fun dfs(fromIndex: Int, toIndex: Int): TreeNode {
+                val root = TreeNode(preorder[fromIndex])
+
+                var leftEndIndex = fromIndex + 1
+                while (leftEndIndex < preorder.size && preorder[leftEndIndex] < root.`val`) {
+                    leftEndIndex++
+                }
+
+                leftEndIndex--
+
+                if (leftEndIndex > fromIndex) {
+                    root.left = dfs(fromIndex + 1, leftEndIndex)
+                }
+
+                if (toIndex > leftEndIndex) {
+                    root.right = dfs(leftEndIndex + 1, toIndex)
+                }
+
+                return root
+            }
+
+            return dfs(0, preorder.lastIndex)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().bstFromPreorder(
+            intArrayOf(8, 5, 1, 7, 10, 12)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

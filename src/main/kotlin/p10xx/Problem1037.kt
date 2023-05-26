9203568ad8569433bff649887e6af1767ee26310
isaiahliu <isaiahliu@outlook.com>
2023-05-26 12:42:17 +0700

    Problem 1037

---
+++ src/main/kotlin/p10xx/Problem1037.kt
@@ -0,0 +1,28 @@
+package p10xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun isBoomerang(points: Array<IntArray>): Boolean {
+            val (p1, p2, p3) = points
+
+            val (x1, y1) = p1
+            val (x2, y2) = p2
+            val (x3, y3) = p3
+
+            return (y2 - y1) * (x3 - x1) == (y3 - y1) * (x2 - x1)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().isBoomerang(
+            arrayOf(
+                intArrayOf(0, 1),
+                intArrayOf(1, 0),
+            )
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}

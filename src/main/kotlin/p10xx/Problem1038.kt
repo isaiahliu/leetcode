0ca041649c378f5b4284594b2445282c77381e4f
isaiahliu <isaiahliu@outlook.com>
2023-05-26 13:03:01 +0700

    Problem 1038

---
+++ src/main/kotlin/p10xx/Problem1038.kt
@@ -0,0 +1,36 @@
+package p10xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun bstToGst(root: TreeNode?): TreeNode? {
+            fun TreeNode.dfs(largerSum: Int): Int {
+                val rightSum = right?.dfs(largerSum) ?: 0
+
+                val leftSum = left?.dfs(largerSum + `val` + rightSum) ?: 0
+
+                val result = `val` + leftSum + rightSum
+
+                `val` += largerSum + rightSum
+
+                return result
+            }
+
+            return root?.also { it.dfs(0) }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().bstToGst(
+            TreeNode(
+                10,
+                TreeNode(1),
+                TreeNode(20)
+            )
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}

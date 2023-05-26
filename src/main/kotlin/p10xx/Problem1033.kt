d903052776677697d4f25fa6fbe64efae99428f5
isaiahliu <isaiahliu@outlook.com>
2023-05-26 10:49:03 +0700

    Problem 1033

---
+++ src/main/kotlin/p10xx/Problem1033.kt
@@ -0,0 +1,33 @@
+package p10xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numMovesStones(a: Int, b: Int, c: Int): IntArray {
+            val (x, y, z) = arrayOf(a, b, c).sorted()
+
+            var min = 0
+            var max = 0
+
+            if (z - x > 2) {
+                min = 1
+                if (y - x > 2 && z - y > 2) {
+                    min++
+                }
+
+                max = z - x - 2
+            }
+
+            return intArrayOf(min, max)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numMovesStones(
+            1, 2, 5
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}

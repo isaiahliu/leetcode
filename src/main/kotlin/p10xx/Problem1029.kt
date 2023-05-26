6a4096ad3ddd9c7aba2bdb7a220eabc3848a281b
isaiahliu <isaiahliu@outlook.com>
2023-05-26 10:29:34 +0700

    Problem 1029

---
+++ src/main/kotlin/p10xx/Problem1029.kt
@@ -0,0 +1,30 @@
+package p10xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun twoCitySchedCost(costs: Array<IntArray>): Int {
+            costs.sortBy { it[0] - it[1] }
+
+            val halfLength = costs.size / 2
+
+            var result = 0
+
+            repeat(halfLength) {
+                result += costs[it][0]
+                result += costs[halfLength + it][1]
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().twoCitySchedCost(
+            arrayOf()
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}

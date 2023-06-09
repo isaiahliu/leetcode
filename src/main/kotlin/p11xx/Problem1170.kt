76496e0380b0b2c1456d4164bf461d9f332152d2
isaiahliu <isaiahliu@outlook.com>
2023-06-10 00:56:51 +0800

    Problem 1170

---
+++ src/main/kotlin/p11xx/Problem1170.kt
@@ -0,0 +1,45 @@
+package p11xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numSmallerByFrequency(queries: Array<String>, words: Array<String>): IntArray {
+            fun String.f(): Int {
+                var min = this[0]
+                var count = 0
+                forEach {
+                    when {
+                        it < min -> {
+                            min = it
+                            count = 1
+                        }
+
+                        it == min -> {
+                            count++
+                        }
+                    }
+                }
+
+                return count
+            }
+
+            val dic = TreeMap<Int, Int>()
+            words.map { it.f() }.sortedDescending().forEachIndexed { index, i ->
+                dic[i] = index + 1
+            }
+
+            return queries.map {
+                dic.higherEntry(it.f())?.value ?: 0
+            }.toIntArray()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numSmallerByFrequency(
+            arrayOf("bba", "abaaaaaa", "aaaaaa", "bbabbabaab", "aba", "aa", "baab", "bbbbbb", "aab", "bbabbaabb"),
+            arrayOf("aaabbb", "aab", "babbab", "babbbb", "b", "bbbbbbbbab", "a", "bbbbbbbbbb", "baaabbaab", "aa")
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

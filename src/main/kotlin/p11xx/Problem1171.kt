bd80bdb46b11467d2ca566ea85c6be4ee3ba1878
isaiahliu <isaiahliu@outlook.com>
2023-06-11 18:07:30 +0800

    Problem 1171

---
+++ src/main/kotlin/p11xx/Problem1171.kt
@@ -0,0 +1,72 @@
+package p11xx
+
+import util.ListNode
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun removeZeroSumSublists(head: ListNode?): ListNode? {
+            val stack = LinkedList<ListNode>()
+
+            val sums = hashMapOf<Int, Int>()
+
+            var sum = 0
+            var t = head
+            while (t != null) {
+                if (t.`val` != 0) {
+                    sum += t.`val`
+
+                    when (sum) {
+                        0 -> {
+                            sums.clear()
+                            stack.clear()
+                        }
+
+                        in sums -> {
+                            sum -= t.`val`
+                            var poppedSum = t.`val`
+
+                            while (poppedSum != 0) {
+                                sums[sum]?.also {
+                                    if (it == 1) {
+                                        sums.remove(sum)
+                                    } else {
+                                        sums[sum] = it - 1
+                                    }
+                                }
+
+                                stack.pollLast().`val`.also {
+                                    poppedSum += it
+                                    sum -= it
+                                }
+                            }
+
+                            stack.peekLast()?.next = null
+                        }
+
+                        else -> {
+                            stack.peekLast()?.next = t
+                            stack.add(t)
+
+                            sums[sum] = (sums[sum] ?: 0) + 1
+                        }
+                    }
+                }
+
+                t = t.next
+            }
+
+            stack.peekLast()?.next = null
+            return stack.peekFirst()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().removeZeroSumSublists(
+            ListNode(1, ListNode(2, ListNode(3, ListNode(-3, ListNode(-2)))))
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}

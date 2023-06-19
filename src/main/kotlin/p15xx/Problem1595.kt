e5532f9ce521b232b400597f8bdc398f82484580
isaiahliu <isaiahliu@outlook.com>
2023-06-20 01:17:04 +0800

    Problem 1595

---
+++ src/main/kotlin/p15xx/Problem1595.kt
@@ -0,0 +1,73 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun connectTwoGroups(cost: List<List<Int>>): Int {
+            fun Int.forEachBit(consumer: (Int) -> Unit) {
+                var t = this
+
+                var index = 0
+                while (t > 0) {
+                    if (t % 2 == 1) {
+                        consumer(index)
+                    }
+
+                    t /= 2
+                    index++
+                }
+            }
+
+            val dp = Array(cost.size) {
+                IntArray(1 shl cost[0].size)
+            }
+
+            dp[0].also { dp0 ->
+                repeat(dp0.size) {
+                    var sum = 0
+                    it.forEachBit {
+                        sum += cost[0][it]
+                    }
+
+                    dp0[it] = sum
+                }
+            }
+
+            for (dpIndex in 1 until dp.size) {
+                val sums = dp[dpIndex]
+                val sumsPre = dp[dpIndex - 1]
+                for (status in 1 until sums.size) {
+                    var min = Int.MAX_VALUE
+                    status.forEachBit {
+                        val p = 1 shl it
+                        val num = cost[dpIndex][it]
+
+                        var t = sumsPre[status]
+                        if (status - p > 0) {
+                            t = t.coerceAtMost(sums[status - p]).coerceAtMost(sumsPre[status - p])
+                        }
+
+                        min = min.coerceAtMost(t + num)
+                    }
+
+                    sums[status] = min
+                }
+            }
+
+            return dp.last().last()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().connectTwoGroups(
+            listOf(
+                listOf(1, 3, 5),
+                listOf(4, 1, 1),
+                listOf(1, 5, 3),
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+
+

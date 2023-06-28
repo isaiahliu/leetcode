e9ab946f41678b6da75e2e1a0b75851b5c59d239
isaiahliu <isaiahliu@outlook.com>
2023-06-29 00:26:02 +0800

    Problem 1253

---
+++ src/main/kotlin/p12xx/Problem1253.kt
@@ -0,0 +1,52 @@
+package p12xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun reconstructMatrix(upper: Int, lower: Int, colsum: IntArray): List<List<Int>> {
+            val array1 = IntArray(colsum.size)
+            val array2 = IntArray(colsum.size)
+
+            var count1 = 0
+            colsum.forEachIndexed { index, n ->
+                if (n == 2) {
+                    array1[index] = 1
+                    count1++
+
+                }
+            }
+            if (count1 > upper) {
+                return emptyList()
+            }
+
+            var count2 = 0
+            colsum.forEachIndexed { index, s ->
+                if (s > 0) {
+                    if (count1 < upper && array1[index] == 0) {
+                        array1[index] = 1
+                        count1++
+                    } else {
+                        array2[index] = 1
+                        count2++
+                    }
+                }
+            }
+
+            return if (count1 == upper && count2 == lower) {
+                listOf(array1.toList(), array2.toList())
+            } else {
+                emptyList()
+            }
+        }
+    }
+
+    measureTimeMillis {
+        println(
+            Solution().reconstructMatrix(
+                5, 5, intArrayOf()
+            )
+        )
+    }.also { println("Time cost: ${it}ms") }
+}
+

426af93836fc7b542674c794e98aed4eab565211
isaiahliu <isaiahliu@outlook.com>
2023-07-08 21:56:53 +0800

    Problem 1084

---
+++ src/main/kotlin/p10xx/Problem1084.sql
@@ -0,0 +1,6 @@
+SELECT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s
+ON p.product_id = s.product_id
+GROUP BY p.product_id
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) < '2019-04-01'

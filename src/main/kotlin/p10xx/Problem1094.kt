426d3d276603766779e90edb28745a4c22d7d6a7
isaiahliu <isaiahliu@outlook.com>
2023-07-08 22:49:19 +0800

    Problem 1094

---
+++ src/main/kotlin/p10xx/Problem1094.kt
@@ -0,0 +1,50 @@
+package p10xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun carPooling(trips: Array<IntArray>, capacity: Int): Boolean {
+            val PICKUP = 1
+            val DROP = 2
+            val queue = PriorityQueue(compareBy<Pair<Int, Int>> { (index, type) ->
+                trips[index][type]
+            }.thenByDescending { it.second })
+
+            repeat(trips.size) {
+                queue.add(it to PICKUP)
+            }
+
+            var c = capacity
+            while (queue.isNotEmpty()) {
+                val (index, type) = queue.poll()
+
+                val (person, _, to) = trips[index]
+                when (type) {
+                    PICKUP -> {
+                        c -= person
+
+                        queue.add(index to DROP)
+
+                        if (c < 0) {
+                            return false
+                        }
+                    }
+
+                    DROP -> {
+                        c += person
+                    }
+                }
+            }
+
+            return true
+        }
+    }
+
+    measureTimeMillis {
+        Solution().carPooling(
+            arrayOf(), 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

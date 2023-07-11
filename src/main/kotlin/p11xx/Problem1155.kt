a81d793fa5ecd317326e070e91cc95a80a14bac3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-11 16:03:43 +0800

    Problem 1155

---
+++ src/main/kotlin/p11xx/Problem1155.kt
@@ -0,0 +1,43 @@
+package p11xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        val cache = hashMapOf<Pair<Int, Int>, Long>()
+
+        val m = 1000000007
+
+        fun numRollsToTarget(n: Int, k: Int, target: Int): Int {
+            if (target < n || target > k * n) {
+                return 0
+            }
+
+            if (n == 1) {
+                return 1
+            }
+
+            val cacheKey = n to target
+            if (cacheKey in cache) {
+                return cache[cacheKey]?.toInt() ?: 0
+            }
+
+            var result = 0L
+
+            repeat(k) {
+                result += numRollsToTarget(n - 1, k, target - it - 1)
+                result %= m
+            }
+
+            cache[cacheKey] = result
+
+            return result.toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numRollsToTarget(
+            1, 6, 3
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

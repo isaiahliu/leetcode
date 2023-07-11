2f1a6eaa25162289835b2bae5b62f010bcf7986a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-11 15:51:59 +0800

    Problem 1154

---
+++ src/main/kotlin/p11xx/Problem1154.kt
@@ -0,0 +1,26 @@
+package p11xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun dayOfYear(date: String): Int {
+            val monthDays = intArrayOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+            return "(\\d{4})-(\\d{2})-(\\d{2})".toRegex().matchEntire(date)?.groupValues?.drop(1)?.map { it.toInt() }
+                ?.let { (year, month, day) ->
+                    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                        monthDays[1]++
+                    }
+
+                    (0 until month - 1).map { monthDays[it] }.sum() + day
+                } ?: 0
+        }
+    }
+
+    measureTimeMillis {
+        Solution().dayOfYear(
+            ""
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

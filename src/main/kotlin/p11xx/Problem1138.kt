f2c3f50e5b90ce6a97720acb05959c1ca20d180a
isaiahliu <isaiahliu@outlook.com>
2023-07-11 10:09:31 +0800

    Problem 1138

---
+++ src/main/kotlin/p11xx/Problem1138.kt
@@ -0,0 +1,55 @@
+package p11xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun alphabetBoardPath(target: String): String {
+            val pos = intArrayOf(0, 0)
+
+            val result = StringBuilder()
+            target.forEach {
+                val r = (it - 'a') / 5
+                val c = (it - 'a') % 5
+
+                if (pos[0] > r) {
+                    repeat(pos[0] - r) {
+                        result.append('U')
+                    }
+                }
+
+                if (pos[1] > c) {
+                    repeat(pos[1] - c) {
+                        result.append('L')
+                    }
+                }
+
+                if (pos[0] < r) {
+                    repeat(r - pos[0]) {
+                        result.append('D')
+                    }
+                }
+
+                if (pos[1] < c) {
+                    repeat(c - pos[1]) {
+                        result.append('R')
+                    }
+                }
+
+                result.append('!')
+
+                pos[0] = r
+                pos[1] = c
+            }
+
+            return result.toString()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().alphabetBoardPath(
+            "leet"
+        ).also { println(it) }
+
+    }.also { println("Time cost: ${it}ms") }
+}

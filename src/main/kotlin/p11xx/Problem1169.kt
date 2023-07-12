5a52e380b39031f1778f12a6e26f1832c332bd0a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-12 14:11:56 +0800

    Problem 1169

---
+++ src/main/kotlin/p11xx/Problem1169.kt
@@ -0,0 +1,54 @@
+package p11xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun invalidTransactions(transactions: Array<String>): List<String> {
+            val result = hashSetOf<Int>()
+
+            val map = hashMapOf<String, LinkedList<Pair<Int, Pair<Int, String>>>>()
+
+            transactions.mapIndexedNotNull { index, s ->
+                val (name, time, amount, city) = s.split(",")
+
+                if (amount.toInt() > 1000) {
+                    result.add(index)
+                }
+                (index to name) to (time.toInt() to city)
+            }.sortedBy { it.second.first }.forEach { (p1, p2) ->
+                val (index, name) = p1
+                val (time, city) = p2
+
+                val txs = map.computeIfAbsent(name) { LinkedList() }
+
+                for ((targetIndex, tp) in txs) {
+                    val (targetTime, targetCity) = tp
+
+                    if (time - targetTime > 60) {
+                        break
+                    } else if (city != targetCity) {
+                        result.add(index)
+                        result.add(targetIndex)
+                    }
+                }
+
+                txs.push(index to (time to city))
+            }
+
+            return result.map { transactions[it] }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().invalidTransactions(
+            arrayOf(
+                "bob,175,221,amsterdam",
+                "bob,689,1910,barcelona",
+                "bob,820,596,bangkok",
+                "bob,832,1726,barcelona",
+            )
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

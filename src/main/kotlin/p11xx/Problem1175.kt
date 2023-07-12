c16daa647683d711501bf59ef6a39b31ebdc4c52
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-12 14:21:00 +0800

    Problem 1175

---
+++ src/main/kotlin/p11xx/Problem1175.kt
@@ -0,0 +1,62 @@
+package p11xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numPrimeArrangements(n: Int): Int {
+            fun Int.isPrime(): Boolean {
+                when {
+                    this == 1 -> {
+                        return false
+                    }
+
+                    this == 2 -> {
+                        return true
+                    }
+
+                    this % 2 == 0 -> {
+                        return false
+                    }
+
+                    else -> {
+                        for (i in 3 until this) {
+                            if (i * i > this) {
+                                break
+                            }
+
+                            if (this % i == 0) {
+                                return false
+                            }
+                        }
+
+                        return true
+                    }
+                }
+            }
+
+            val primeCount = (1..n).count { it.isPrime() }
+
+            var result = 1L
+            val m = 1000000007
+
+            repeat(primeCount) {
+                result *= (it + 1)
+                result %= m
+            }
+
+            repeat(n - primeCount) {
+                result *= (it + 1)
+                result %= m
+            }
+
+            return result.toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numPrimeArrangements(
+            5
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

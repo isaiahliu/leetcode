3df0c94f6fb972f76b0e902eaea9faff634c5f16
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-12 18:09:47 +0800

    Problem 1189

---
+++ src/main/kotlin/p11xx/Problem1189.kt
@@ -0,0 +1,20 @@
+package p11xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxNumberOfBalloons(text: String): Int {
+            val counts = text.groupingBy { it }.eachCount()
+
+            return (counts['b'] ?: 0).coerceAtMost(counts['a'] ?: 0).coerceAtMost((counts['l'] ?: 0) / 2)
+                .coerceAtMost((counts['o'] ?: 0) / 2).coerceAtMost(counts['n'] ?: 0)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxNumberOfBalloons(
+            ""
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

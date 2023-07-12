222d2fc33fc32a5aa055613a88d690a6ca269b26
isaiahliu <isaiahliu@outlook.com>
2023-07-13 00:27:59 +0800

    Problem 1192

---
+++ src/main/kotlin/p11xx/Problem1192.kt
@@ -0,0 +1,50 @@
+package p11xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun criticalConnections(n: Int, connections: List<List<Int>>): List<List<Int>> {
+            val reachable = Array(n) { hashSetOf<Int>() }
+
+            for ((from, to) in connections) {
+                reachable[from].add(to)
+                reachable[to].add(from)
+            }
+
+            val result = arrayListOf<List<Int>>()
+
+            val time = IntArray(n)
+            val low = IntArray(n)
+            var num = 0
+
+            fun dfs(to: Int, from: Int) {
+                time[to] = ++num
+                low[to] = time[to]
+
+                for (next in reachable[to]) {
+                    if (time[next] == 0) {
+                        dfs(next, to)
+
+                        low[to] = low[to].coerceAtMost(low[next])
+
+                        if (low[next] > time[to]) {
+                            result.add(listOf(to, next))
+                        }
+                    } else if (time[next] < time[to] && next != from) {
+                        low[to] = low[to].coerceAtMost(time[next])
+                    }
+                }
+            }
+            dfs(0, 0)
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().criticalConnections(
+            3, emptyList()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

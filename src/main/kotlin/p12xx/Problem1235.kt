3ad1c3a9e44395b732f830b235e16a695cc86e96
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-14 15:38:04 +0800

    Problem 1235

---
+++ src/main/kotlin/p12xx/Problem1235.kt
@@ -0,0 +1,35 @@
+package p12xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun jobScheduling(startTime: IntArray, endTime: IntArray, profit: IntArray): Int {
+            val total = TreeMap<Int, Int>()
+
+            startTime.indices.sortedBy { endTime[it] }.forEach { jobId ->
+                val start = startTime[jobId]
+                val end = endTime[jobId]
+                val p = profit[jobId]
+
+                val existingMax = total.lastEntry()?.value ?: 0
+
+                val currentMax = p + (total.lowerEntry(start + 1)?.value ?: 0)
+
+                if (currentMax > existingMax) {
+                    total[end] = currentMax
+                }
+            }
+
+            return total.lastEntry()?.value ?: 0
+        }
+    }
+
+    measureTimeMillis {
+        Solution().jobScheduling(
+            intArrayOf(), intArrayOf(), intArrayOf()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

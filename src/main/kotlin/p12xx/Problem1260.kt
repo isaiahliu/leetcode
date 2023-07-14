93117aa837d0bb6297d3518846eb1b5b87a03daa
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-14 18:13:28 +0800

    Problem 1260

---
+++ src/main/kotlin/p12xx/Problem1260.kt
@@ -0,0 +1,30 @@
+package p12xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun shiftGrid(grid: Array<IntArray>, k: Int): List<List<Int>> {
+            val m = grid.size
+            val n = grid[0].size
+            return List(m) { r ->
+                List(n) { c ->
+                    val targetC = ((c - k) % n + n) % n
+                    val targetR = ((r - (k - c + n - 1) / n) % m + m) % m
+
+                    grid[targetR][targetC]
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().shiftGrid(
+            arrayOf(
+                intArrayOf(1, 2, 3),
+                intArrayOf(4, 5, 6),
+                intArrayOf(7, 8, 9),
+            ), 1
+        ).joinToString("\n") { it.toList().toString() }.also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

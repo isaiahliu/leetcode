40577f8ea66839e80ad5ec7e036f027ed2caff29
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-14 16:38:34 +0800

    Problem 1248

---
+++ src/main/kotlin/p12xx/Problem1248.kt
@@ -0,0 +1,50 @@
+package p12xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numberOfSubarrays(nums: IntArray, k: Int): Int {
+            val evenLeftCounts = IntArray(nums.size)
+            val evenRightCounts = IntArray(nums.size)
+
+            val oddIndices = arrayListOf<Int>()
+
+            var count = 0
+            for (i in nums.indices) {
+                if (nums[i] % 2 == 0) {
+                    count++
+                } else {
+                    evenLeftCounts[i] = count
+                    count = 0
+
+                    oddIndices.add(i)
+                }
+            }
+
+            count = 0
+            for (i in nums.lastIndex downTo 0) {
+                if (nums[i] % 2 == 0) {
+                    count++
+                } else {
+                    evenRightCounts[i] = count
+                    count = 0
+                }
+            }
+
+            var result = 0
+            for (i in 0 until oddIndices.size - k + 1) {
+                result += (evenLeftCounts[oddIndices[i]] + 1) * (evenRightCounts[oddIndices[i + k - 1]] + 1)
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numberOfSubarrays(
+            intArrayOf(), 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

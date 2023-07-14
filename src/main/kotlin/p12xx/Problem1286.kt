2d7b736776d01076331fe85d74172459f6e4fd57
isaiahliu <isaiahliu@outlook.com>
2023-07-15 00:14:59 +0800

    Problem 1286

---
+++ src/main/kotlin/p12xx/Problem1286.kt
@@ -0,0 +1,50 @@
+package p12xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class CombinationIterator(private val characters: String, private val combinationLength: Int) {
+        val list = LinkedList<Int>()
+
+        init {
+            repeat(combinationLength) {
+                list.add(it)
+            }
+        }
+
+        fun next(): String {
+            val result = list.joinToString("") { characters[it].toString() }
+
+            while (true) {
+                val last = list.pollLast() ?: break
+
+                if (last + combinationLength - list.size < characters.length) {
+                    repeat(combinationLength - list.size) {
+                        list.add(last + it + 1)
+                    }
+
+                    break
+                }
+            }
+
+            return result
+        }
+
+        fun hasNext(): Boolean {
+            return list.isNotEmpty()
+        }
+    }
+
+    measureTimeMillis {
+        val itor = CombinationIterator("abc", 2)
+        itor.next().also { println(it) }
+        itor.hasNext().also { println(it) }
+        itor.next().also { println(it) }
+        itor.hasNext().also { println(it) }
+        itor.next().also { println(it) }
+        itor.hasNext().also { println(it) }
+        itor.next().also { println(it) }
+        itor.hasNext().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

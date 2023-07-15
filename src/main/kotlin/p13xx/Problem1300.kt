1541eec52e88f688ab930c4f1dd8bfac7975b44d
isaiahliu <isaiahliu@outlook.com>
2023-07-15 21:40:09 +0800

    Problem 1300

---
+++ src/main/kotlin/p13xx/Problem1300.kt
@@ -0,0 +1,56 @@
+package p13xx
+
+import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findBestValue(arr: IntArray, target: Int): Int {
+            val map = TreeMap<Int, Int>()
+
+            arr.forEach {
+                map[it] = (map[it] ?: 0) + 1
+            }
+
+            if (arr.sum() <= target) {
+                return map.lastKey()
+            }
+
+            var best = (target / arr.size).let { intArrayOf(it, it + 1) }.minBy {
+                (it * arr.size - target).absoluteValue
+            }
+
+            if (best <= map.firstKey()) {
+                return best
+            }
+
+            var remainingCount = arr.size
+            var existingSum = 0
+
+            while (true) {
+                val (key, value) = map.pollFirstEntry()
+
+                remainingCount -= value
+                existingSum += key * value
+
+                best = ((target - existingSum) / remainingCount).let { intArrayOf(it, it + 1) }.minBy {
+                    (it * remainingCount + existingSum - target).absoluteValue
+                }
+
+                if (best <= map.firstKey()) {
+                    return best
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findBestValue(
+            intArrayOf(2, 3, 5), 11
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

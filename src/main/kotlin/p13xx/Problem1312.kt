acf9fcb10ced177917cbb5c21e861e7c166f455a
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-17 09:26:08 +0800

    Problem 1312

---
+++ src/main/kotlin/p13xx/Problem1312.kt
@@ -0,0 +1,43 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minInsertions(s: String): Int {
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+
+            fun dfs(start: Int, end: Int): Int {
+                if (start >= end) {
+                    return 0
+                }
+
+                val cacheKey = start to end
+
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                val result = if (s[start] == s[end]) {
+                    dfs(start + 1, end - 1)
+                } else {
+                    dfs(start + 1, end).coerceAtMost(dfs(start, end - 1)) + 1
+                }
+
+                cache[cacheKey] = result
+                return result
+            }
+
+            return dfs(0, s.lastIndex)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minInsertions(
+            "mbadm"
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

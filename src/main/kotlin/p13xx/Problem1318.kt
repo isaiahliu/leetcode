8be2b8747dbe43c6e3cf5d5e867593bcc69bd615
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-17 10:37:07 +0800

    Problem 1318

---
+++ src/main/kotlin/p13xx/Problem1318.kt
@@ -0,0 +1,48 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minFlips(a: Int, b: Int, c: Int): Int {
+            var result = 0
+
+            var ta = a
+            var tb = b
+            var tc = c
+
+            while (ta > 0 || tb > 0 || tc > 0) {
+                val lastA = ta and 1
+                val lastB = tb and 1
+                val lastC = tc and 1
+
+                when (lastC) {
+                    lastA or lastB -> {}
+
+                    1 -> {
+                        result++
+                    }
+
+                    else -> {
+                        result += lastA + lastB
+                    }
+                }
+
+                ta /= 2
+                tb /= 2
+                tc /= 2
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minFlips(
+            1, 2, 3
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

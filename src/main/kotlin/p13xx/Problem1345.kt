c6afa3f9e65328c0d87838622af7b9e4bc04df1b
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-17 17:57:44 +0800

    Problem 1345

---
+++ src/main/kotlin/p13xx/Problem1345.kt
@@ -0,0 +1,43 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minJumps(arr: IntArray): Int {
+            val groups = arr.indices.groupBy { arr[it] }.toMutableMap()
+
+            val visited = hashSetOf(0)
+
+            val tasks = hashSetOf(0)
+
+            var result = 0
+            while (tasks.isNotEmpty()) {
+                tasks.toSet().also { tasks.clear() }.forEach {
+                    if (it == arr.lastIndex) {
+                        return result
+                    }
+
+                    (groups.remove(arr[it]).orEmpty() + (it - 1) + (it + 1)).forEach {
+                        if (it in arr.indices && visited.add(it)) {
+                            tasks.add(it)
+                        }
+                    }
+                }
+
+                result++
+            }
+
+            return -1
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minJumps(
+            IntArray(50000) {
+                7
+            }.also { it[it.lastIndex] = 11 }
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

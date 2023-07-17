240629037bca6ab9c26769a024ee628a6e2a27d0
isaiahliu <isaiahliu@outlook.com>
2023-07-18 00:17:04 +0800

    Problem 1851

---
+++ src/main/kotlin/p18xx/Problem1851.kt
@@ -0,0 +1,47 @@
+package p18xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minInterval(intervals: Array<IntArray>, queries: IntArray): IntArray {
+            var intervalIndex = 0
+            intervals.sortWith(compareBy<IntArray> { it[0] }.thenBy { it[1] })
+
+            val sorted = queries.indices.sortedBy { queries[it] }
+
+            val queue = PriorityQueue<IntArray>(compareBy { it[1] - it[0] })
+
+            val result = IntArray(queries.size)
+            sorted.forEach { index ->
+                val q = queries[index]
+
+                while (intervalIndex < intervals.size && intervals[intervalIndex][0] <= q) {
+                    queue.add(intervals[intervalIndex++])
+                }
+
+                while (queue.isNotEmpty() && queue.peek()[1] < q) {
+                    queue.poll()
+                }
+
+                result[index] = queue.peek()?.let { it[1] - it[0] + 1 } ?: -1
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minInterval(
+            arrayOf(
+                intArrayOf(1, 4),
+                intArrayOf(2, 4),
+                intArrayOf(3, 6),
+                intArrayOf(4, 4),
+            ),
+            intArrayOf(2, 3, 4, 5)
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

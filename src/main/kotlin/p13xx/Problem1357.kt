7ee8dd9dba64009237449ba7c6a4a02aa8252946
isaiahliu <isaiahliu@outlook.com>
2023-07-18 00:01:12 +0800

    Problem 1357

---
+++ src/main/kotlin/p13xx/Problem1357.kt
@@ -0,0 +1,37 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Cashier(
+        private val n: Int,
+        private val discount: Int,
+        private val products: IntArray,
+        private val prices: IntArray
+    ) {
+        val priceMap = products.indices.associate { products[it] to prices[it] }
+        var customerCount = 0
+
+        val discountRate = 1 - discount / 100.0
+        fun getBill(product: IntArray, amount: IntArray): Double {
+            customerCount++
+
+            val totalAmount = product.mapIndexed { index, p ->
+                (priceMap[p] ?: 0) * amount[index]
+            }.sum().toDouble()
+
+            return totalAmount * if (customerCount % n == 0) {
+                discountRate
+            } else {
+                1.0
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Cashier(5, 1, intArrayOf(), intArrayOf()).getBill(
+            intArrayOf(1, 1000000000), intArrayOf()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

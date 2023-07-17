6882f898c75e085825173122c870e9f6e4fdbcf3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-17 09:08:07 +0800

    Problem 1309

---
+++ src/main/kotlin/p13xx/Problem1309.kt
@@ -0,0 +1,38 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun freqAlphabets(s: String): String {
+            val result = StringBuilder()
+
+            var index = s.lastIndex
+
+            while (index >= 0) {
+                var charIndex = 0
+                if (s[index] == '#') {
+                    index--
+                    charIndex = s[index] - '0'
+                    index--
+                    charIndex += (s[index] - '0') * 10
+                } else {
+                    charIndex = s[index] - '0'
+                }
+                result.insert(0, 'a' + charIndex - 1)
+                index--
+            }
+
+            return result.toString()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().freqAlphabets(
+            ""
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

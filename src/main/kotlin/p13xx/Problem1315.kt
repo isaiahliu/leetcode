cc21be6618ac5d0909f87fc1fa420eed14d349ee
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-17 10:17:00 +0800

    Problem 1315

---
+++ src/main/kotlin/p13xx/Problem1315.kt
@@ -0,0 +1,38 @@
+package p13xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun sumEvenGrandparent(root: TreeNode?): Int {
+            var result = 0
+            fun TreeNode.dfs(status: Int) {
+                if (status and 0b10 > 0) {
+                    result += `val`
+                }
+
+                var newStatus = (status shl 1) and 0b11
+                if (`val` % 2 == 0) {
+                    newStatus++
+                }
+
+                left?.dfs(newStatus)
+                right?.dfs(newStatus)
+            }
+
+            root?.dfs(0)
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().sumEvenGrandparent(
+            null
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

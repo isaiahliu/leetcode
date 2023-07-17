33a2e9ccae311485d750989f6f50513869564a07
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-17 09:12:33 +0800

    Problem 1310

---
+++ src/main/kotlin/p13xx/Problem1310.kt
@@ -0,0 +1,26 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun xorQueries(arr: IntArray, queries: Array<IntArray>): IntArray {
+            for (i in 1 until arr.size) {
+                arr[i] = arr[i] xor arr[i - 1]
+            }
+
+            return queries.map { (left, right) ->
+                arr[right] xor (arr.getOrNull(left - 1) ?: 0)
+            }.toIntArray()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().xorQueries(
+            intArrayOf(), arrayOf()
+        ).also {
+            println(it)
+        }
+    }.also { println("Time cost: ${it}ms") }
+}
+

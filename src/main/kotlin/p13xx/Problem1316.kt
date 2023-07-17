52a0f9b9dd50c7c2cd06b63cd4c8ce060dcaf20b
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-17 10:30:55 +0800

    Problem 1316

--- src/main/kotlin/p13xx/Problem1316.kt
+++ src/main/kotlin/p13xx/Problem1316.kt
@@ -7,14 +7,22 @@ fun main() {
         fun distinctEchoSubstrings(text: String): Int {
             val result = hashSetOf<String>()
 
+            fun match(start1: Int, start2: Int, size: Int): Boolean {
+                repeat(size) {
+                    if (text[start1 + it] != text[start2 + it]) {
+                        return false
+                    }
+                }
+
+                return true
+            }
+
             var size = 1
 
             while (size * 2 <= text.length) {
                 for (i in 0 until text.length - size * 2 + 1) {
-                    val first = text.substring(i until i + size)
-                    val second = text.substring(i + size until i + size * 2)
-                    if (first == second) {
-                        result.add(first)
+                    if (match(i, i + size, size)) {
+                        result.add(text.substring(i until i + size))
                     }
                 }
 

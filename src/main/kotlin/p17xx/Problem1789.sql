065f17fb06226ddd71e3fd177fbdf3afbf101517
isaiahliu <isaiahliu@outlook.com>
2023-07-19 01:19:07 +0800

    Problem 1789

--- src/main/kotlin/p17xx/Problem1789.sql
+++ src/main/kotlin/p17xx/Problem1789.sql
@@ -1,4 +1,4 @@
-WITH t AS (SELECT employee_id, department_id, ROW_NUMBER() OVER(PARTITION BY employee_id ORDER BY primary_flag) AS r
+WITH t AS (SELECT employee_id, department_id, ROW_NUMBER() OVER(PARTITION BY employee_id ORDER BY IF(primary_flag = 'Y', 0, 1)) AS r
 FROM Employee)
 SELECT employee_id, department_id
 FROM t WHERE r = 1

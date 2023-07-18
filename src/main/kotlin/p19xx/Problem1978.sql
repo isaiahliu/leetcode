bb976ebfcda879db66134c9b75ff5036ed867dea
isaiahliu <isaiahliu@outlook.com>
2023-07-19 01:40:27 +0800

    Problem 1978

---
+++ src/main/kotlin/p19xx/Problem1978.sql
@@ -0,0 +1,6 @@
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Employees m
+ON e.manager_id = m.employee_id
+WHERE e.manager_id IS NOT NULL AND m.employee_id IS NULL AND e.salary < 30000
+ORDER BY e.employee_id

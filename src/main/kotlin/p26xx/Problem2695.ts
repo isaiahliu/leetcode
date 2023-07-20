2540e4c0f618e075721d05a9e1450be55c2f3d4c
isaiahliu <isaiahliu@outlook.com>
2023-07-20 23:48:54 +0800

    Problem 2695

---
+++ src/main/kotlin/p26xx/Problem2695.ts
@@ -0,0 +1,22 @@
+class ArrayWrapper {
+    n : number[]
+	constructor(nums: number[]) {
+        this.n = nums
+    }
+
+	valueOf() {
+        return this.n.reduce((a, b) => a + b, 0)
+    }
+
+	toString() {
+        return `[${this.n.join(",")}]`
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */

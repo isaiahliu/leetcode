030daebc8f58ee039c6d517008a8d61559b03165
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-21 18:32:16 +0800

    Problem 1360

---
+++ src/main/kotlin/p13xx/Problem1360.kt
@@ -0,0 +1,23 @@
+package p13xx
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun daysBetweenDates(date1: String, date2: String): Int {
+            return LocalDate.parse(date1, DateTimeFormatter.ISO_DATE)
+                .until(LocalDate.parse(date2, DateTimeFormatter.ISO_DATE), ChronoUnit.DAYS).toInt().absoluteValue
+        }
+    }
+
+    measureTimeMillis {
+        Solution().daysBetweenDates(
+            "2019-06-29", "2020-06-29"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

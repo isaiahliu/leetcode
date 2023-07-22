d1ddaed1fed11cf85a0455e2ed3433a3a158edf8
isaiahliu <isaiahliu@outlook.com>
2023-07-22 18:47:07 +0800

    Problem 1362

--- src/main/kotlin/p13xx/Problem1362.kt
+++ src/main/kotlin/p13xx/Problem1362.kt
@@ -1,54 +1,28 @@
 package p13xx
 
+import kotlin.math.sqrt
 import kotlin.system.measureTimeMillis
 
 fun main() {
     class Solution {
-        fun validateBinaryTreeNodes(n: Int, leftChild: IntArray, rightChild: IntArray): Boolean {
-            class Node {
-                var root: Node = this
-                    get() {
-                        if (field != this && field != field.root) {
-                            field = field.root
-                        }
-                        return field
-                    }
-            }
-
-            val nodes = Array(n) { Node() }
-            fun IntArray.fillParents(): Boolean {
-                forEachIndexed { index, i ->
-                    if (i >= 0) {
-                        val parent = nodes[index]
-                        val child = nodes[i]
-                        when {
-                            child.root != child -> {
-                                return false
-                            }
-
-                            parent.root == child.root -> {
-                                return false
-                            }
-
-                            else -> {
-                                child.root = parent
-                            }
-                        }
-                    }
+        fun closestDivisors(num: Int): IntArray {
+            for (left in sqrt((num + 2).toDouble()).toInt() downTo 1) {
+                if ((num + 1) % left == 0) {
+                    return intArrayOf(left, (num + 1) / left)
                 }
 
-                return true
+                if ((num + 2) % left == 0) {
+                    return intArrayOf(left, (num + 2) / left)
+                }
             }
 
-            return leftChild.fillParents() && rightChild.fillParents() && nodes.map { it.root }.toSet().size == 1
+            return intArrayOf()
         }
     }
 
     measureTimeMillis {
-        Solution().validateBinaryTreeNodes(
-            4,
-            intArrayOf(1, 0, 3, -1),
-            intArrayOf(-1, -1, -1, -1),
+        Solution().closestDivisors(
+            5
         ).also { println(it) }
     }.also { println("Time cost: ${it}ms") }
 }

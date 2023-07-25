48ef8f13dad0563f5bbbd1ca9d9d4113628684b8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-25 09:17:47 +0800

    Problem 1396

---
+++ src/main/kotlin/p13xx/Problem1396.kt
@@ -0,0 +1,35 @@
+package p13xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class UndergroundSystem {
+        val map = hashMapOf<Pair<String, String>, IntArray>()
+
+        val checkIns = hashMapOf<Int, Pair<String, Int>>()
+
+        fun checkIn(id: Int, stationName: String, t: Int) {
+            checkIns[id] = stationName to t
+        }
+
+        fun checkOut(id: Int, stationName: String, t: Int) {
+            checkIns.remove(id)?.also { (fromStation, fromTime) ->
+                map.computeIfAbsent(fromStation to stationName) { IntArray(2) }.also {
+                    it[0]++
+                    it[1] += t - fromTime
+                }
+            }
+        }
+
+        fun getAverageTime(startStation: String, endStation: String): Double {
+            return map[startStation to endStation]?.let { (count, time) -> time.toDouble() / count } ?: 0.0
+        }
+    }
+
+    measureTimeMillis {
+        UndergroundSystem().checkIn(
+            1, "", 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

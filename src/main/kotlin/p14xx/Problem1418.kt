eeb001a5e911f716d5ba1c002a422cd3f9acd52c
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-26 15:10:42 +0800

    Problem 1418

---
+++ src/main/kotlin/p14xx/Problem1418.kt
@@ -0,0 +1,45 @@
+package p14xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun displayTable(orders: List<List<String>>): List<List<String>> {
+            val foods = TreeMap<String, MutableMap<Int, Int>>()
+            val tables = TreeSet<Int>()
+
+            orders.forEach { (_, table, food) ->
+                val tableNum = table.toInt()
+                tables.add(tableNum)
+
+                foods.computeIfAbsent(food) { hashMapOf() }.also {
+                    it[tableNum] = (it[tableNum] ?: 0) + 1
+                }
+            }
+
+            val result = arrayListOf<List<String>>()
+
+            result.add(listOf("Table") + foods.keys)
+
+            tables.forEach {
+                val details = arrayListOf(it.toString())
+
+                foods.forEach { (_, map) ->
+                    details.add((map[it] ?: 0).toString())
+                }
+
+                result.add(details)
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().displayTable(
+            listOf()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

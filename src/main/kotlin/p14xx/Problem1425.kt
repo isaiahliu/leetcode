324ae117c334795d6a72196c6bafe42a11b6382f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-26 17:07:58 +0800

    Problem 1425

---
+++ src/main/kotlin/p14xx/Problem1425.kt
@@ -0,0 +1,450 @@
+package p14xx
+
+import util.input
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun constrainedSubsetSum(nums: IntArray, k: Int): Int {
+            var max = Int.MIN_VALUE
+
+            val notNegDistance = IntArray(nums.size) { -1 }
+
+            var d = -1
+
+            for (index in nums.indices) {
+                if (d >= 0) {
+                    d++
+                }
+
+                when {
+                    nums[index] >= 0 -> {
+                        notNegDistance[index] = d
+                        d = 0
+                    }
+
+                    d >= 0 -> {
+                        notNegDistance[index] = d
+                        max = max.coerceAtLeast(nums[index])
+                    }
+
+                    else -> {
+                        max = max.coerceAtLeast(nums[index])
+                    }
+                }
+
+            }
+
+            if (notNegDistance[notNegDistance.lastIndex] < 0) {
+                return max
+            }
+
+            val dp = IntArray(nums.size)
+
+            if (nums[0] > 0) {
+                dp[0] = nums[0]
+            }
+
+            var result = dp[0]
+
+            val dpQueue = PriorityQueue<Int>(compareByDescending { dp[it] })
+            dpQueue.add(0)
+
+            for (index in 1 until dp.size) {
+                val distance = notNegDistance[index]
+                val num = nums[index]
+                when {
+                    distance < 0 -> {
+                        dp[index] = num.coerceAtLeast(0)
+                    }
+
+                    distance < k -> {
+                        dp[index] = num + dp[index - distance]
+                    }
+
+                    else -> {
+                        while ((index - dpQueue.peek() > k)) {
+                            dpQueue.poll()
+                        }
+
+                        var last = dp[dpQueue.peek()]
+
+                        if (num >= 0) {
+                            last = last.coerceAtLeast(0)
+                        }
+
+                        dp[index] = last + num
+                    }
+                }
+
+                dpQueue.add(index)
+
+                result = result.coerceAtLeast(dp[index])
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().constrainedSubsetSum(
+            intArrayOf(
+                20,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                10
+            ), 19
+        ).also { println(it) }
+
+        val (p1, p2) = input
+        Solution().constrainedSubsetSum(
+            p1.split(",").map { it.toInt() }.toIntArray(), p2.toInt()
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

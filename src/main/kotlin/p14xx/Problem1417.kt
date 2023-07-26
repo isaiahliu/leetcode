59d3fa5d174f6dd9fc8e70019e9db123e12d47ff
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-26 15:02:36 +0800

    Problem 1417

---
+++ src/main/kotlin/p14xx/Problem1417.kt
@@ -0,0 +1,51 @@
+package p14xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun reformat(s: String): String {
+            val chars = LinkedList<Char>()
+            val nums = LinkedList<Char>()
+
+            s.forEach {
+                if (it in '0'..'9') {
+                    nums.add(it)
+                } else {
+                    chars.add(it)
+                }
+            }
+
+            return if (chars.size in nums.size - 1..nums.size + 1) {
+                val result = StringBuilder()
+
+                if (chars.size > nums.size) {
+                    result.append(chars.poll())
+                    repeat(nums.size) {
+                        result.append(nums.poll())
+                        result.append(chars.poll())
+                    }
+                } else {
+                    repeat(chars.size) {
+                        result.append(nums.poll())
+                        result.append(chars.poll())
+                    }
+
+                    nums.poll()?.also { result.append(it) }
+                }
+
+                result.toString()
+            } else {
+                ""
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().reformat(
+            "a0b1c2",
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

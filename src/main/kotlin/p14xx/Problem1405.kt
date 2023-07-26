c911b3bdde8fe3f1ffc55b43ad5e93ed252941ca
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-26 11:59:53 +0800

    Problem 1405

--- src/main/kotlin/p14xx/Problem1405.kt
+++ src/main/kotlin/p14xx/Problem1405.kt
@@ -5,30 +5,40 @@ import kotlin.system.measureTimeMillis
 fun main() {
     class Solution {
         fun longestDiverseString(a: Int, b: Int, c: Int): String {
-            val array = arrayOf(intArrayOf(a, 0), intArrayOf(b, 1), intArrayOf(c, 2))
+            val array = arrayOf(intArrayOf(0, a), intArrayOf(1, b), intArrayOf(2, c))
 
-            array.sortWith(compareByDescending { it[0] })
+            array.sortWith(compareByDescending { it[1] })
 
             var last = -1
 
             val result = StringBuilder()
 
-            while (array[0][0] > 0) {
-                if (array[0][1] != last) {
-                    repeat(array[0][0].coerceAtMost(2)) {
-                        result.append('a' + array[0][1])
-                        array[0][0]--
+            while (array[0][1] > 0) {
+                var idx = 0
+                var count = 2
+
+                when {
+                    array[0][0] != last -> {
                     }
 
-                    last = array[0][1]
-                } else if (array[1][0] > 0) {
-                    result.append('a' + array[1][1])
-                    array[1][0]--
-                    last = array[1][1]
-                } else {
-                    break
+                    array[1][1] > 0 -> {
+                        idx++
+                        count--
+                    }
+
+                    else -> {
+                        break
+                    }
                 }
-                array.sortWith(compareByDescending { it[0] })
+
+                repeat(array[idx][1].coerceAtMost(count)) {
+                    result.append('a' + array[idx][0])
+                    array[idx][1]--
+                }
+
+                last = array[idx][0]
+
+                array.sortWith(compareByDescending { it[1] })
             }
 
             return result.toString()

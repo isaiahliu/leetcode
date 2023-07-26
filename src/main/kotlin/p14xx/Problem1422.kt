bbf30a231d6295f5960fa1fd12b81596054fe6c0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-26 15:58:40 +0800

    Problem 1422

---
+++ src/main/kotlin/p14xx/Problem1422.kt
@@ -0,0 +1,35 @@
+package p14xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxScore(s: String): Int {
+            val leftZeroSum = IntArray(s.length - 1) { s.getOrNull(it)?.let { '1' - it } ?: 0 }
+            val rightOneSum = IntArray(s.length - 1) { s.getOrNull(it + 1)?.let { it - '0' } ?: 0 }
+
+            for (i in 1 until leftZeroSum.size) {
+                leftZeroSum[i] += leftZeroSum[i - 1]
+            }
+
+            for (i in rightOneSum.lastIndex - 1 downTo 0) {
+                rightOneSum[i] += rightOneSum[i + 1]
+            }
+
+            var result = 0
+
+            repeat(s.length - 1) {
+                result = result.coerceAtLeast(leftZeroSum[it] + rightOneSum[it])
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxScore(
+            "1111"
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

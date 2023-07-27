c8451d4dd933ff15d10f8f840c50e868ce35b158
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-27 13:51:18 +0800

    Problem 1451

---
+++ src/main/kotlin/p14xx/Problem1451.kt
@@ -0,0 +1,27 @@
+package p14xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun arrangeWords(text: String): String {
+            return text.split(" ").mapIndexed { index, s -> s to index }
+                .sortedWith(compareBy<Pair<String, Int>> { it.first.length }.thenBy { it.second })
+                .mapIndexed { index, (s, _) ->
+                    if (index == 0) {
+                        s.take(1).uppercase() + s.drop(1)
+                    } else {
+                        s.lowercase()
+                    }
+                }.joinToString(" ")
+        }
+    }
+
+    measureTimeMillis {
+        Solution().arrangeWords(
+            ""
+        ).also { println("${it} should be 153") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

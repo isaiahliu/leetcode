3ebfaced054188bdd43d0b77d2db1f6a74ad6a9b
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-27 11:33:00 +0800

    Problem 1446

---
+++ src/main/kotlin/p14xx/Problem1446.kt
@@ -0,0 +1,32 @@
+package p14xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxPower(s: String): Int {
+            var result = 1
+            var count = 0
+            var pre = s[0]
+
+            s.forEach {
+                if (it == pre) {
+                    result = result.coerceAtLeast(++count)
+                } else {
+                    pre = it
+                    count = 1
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxPower(
+            ""
+        ).also { println("${it} should be 153") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

924c31e557f1564f337beebd311fd419a33c8b14
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-27 13:45:31 +0800

    Problem 1449

---
+++ src/main/kotlin/p14xx/Problem1449.kt
@@ -0,0 +1,62 @@
+package p14xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun largestNumber(cost: IntArray, target: Int): String {
+            fun String?.greaterNum(target: String): String {
+                return when {
+                    this == null -> target
+                    this.length > target.length -> this
+                    this.length < target.length -> target
+                    this > target -> this
+                    else -> target
+                }
+            }
+
+            val costMap = hashMapOf<Int, Char>()
+
+            cost.forEachIndexed { index, i ->
+                costMap[i] = '1' + index
+            }
+
+            val cache = hashMapOf<Int, String?>()
+            fun dfs(remain: Int): String? {
+                if (remain < 0) {
+                    return null
+                }
+
+                if (remain == 0) {
+                    return ""
+                }
+
+                if (remain in cache) {
+                    return cache[remain]
+                }
+
+                var result: String? = null
+
+                costMap.forEach { (cost, num) ->
+                    dfs(remain - cost)?.also {
+                        result = result.greaterNum("${num}${it}")
+                    }
+                }
+
+                cache[remain] = result
+
+                return result
+            }
+
+            return dfs(target) ?: "0"
+        }
+    }
+
+    measureTimeMillis {
+        Solution().largestNumber(
+            intArrayOf(6, 10, 15, 40, 40, 40, 40, 40, 40), 47
+        ).also { println("${it} should be 153") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

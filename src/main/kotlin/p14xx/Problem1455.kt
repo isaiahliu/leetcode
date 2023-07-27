330e9b258f8a7063f48b8b87843fb2f741e54db8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-27 14:57:26 +0800

    Problem 1455

--- src/main/kotlin/p14xx/Problem1455.kt
+++ src/main/kotlin/p14xx/Problem1455.kt
@@ -5,15 +5,15 @@ import kotlin.system.measureTimeMillis
 fun main() {
     class Solution {
         fun isPrefixOfWord(sentence: String, searchWord: String): Int {
-            return sentence.split(" ").indexOfFirst {
+            return " $sentence".split(" ").indexOfFirst {
                 it.startsWith(searchWord)
-            }.takeIf { it >= 0 }?.let { it + 1 } ?: -1
+            }
         }
     }
 
     measureTimeMillis {
         Solution().isPrefixOfWord(
-            "", ""
+            "burg", "bur"
         ).also { println("${it} should be ${it}") }
 
     }.also { println("Time cost: ${it}ms") }

272d85b77c01466492f8f9c2d8f217bb2b6e87da
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-27 11:39:12 +0800

    Problem 1448

---
+++ src/main/kotlin/p14xx/Problem1448.kt
@@ -0,0 +1,35 @@
+package p14xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun goodNodes(root: TreeNode?): Int {
+            var result = 0
+
+            fun TreeNode.dfs(max: Int) {
+                if (`val` >= max) {
+                    result++
+                }
+
+                max.coerceAtLeast(`val`).also {
+                    left?.dfs(it)
+                    right?.dfs(it)
+                }
+            }
+
+            root?.dfs(Int.MIN_VALUE)
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().goodNodes(
+            null
+        ).also { println("${it} should be 153") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

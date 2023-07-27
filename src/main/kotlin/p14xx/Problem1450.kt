532c6d77a3a390f7421d1fe251cabfea75a700bf
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-27 13:47:16 +0800

    Problem 1450

---
+++ src/main/kotlin/p14xx/Problem1450.kt
@@ -0,0 +1,21 @@
+package p14xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun busyStudent(startTime: IntArray, endTime: IntArray, queryTime: Int): Int {
+            return startTime.indices.count {
+                queryTime in startTime[it]..endTime[it]
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().busyStudent(
+            intArrayOf(1, 2, 3), intArrayOf(3, 2, 7), 4
+        ).also { println("${it} should be 153") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

e76f410565e26045d6b8a53b211106fa8c3be59d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-27 15:02:02 +0800

    Problem 1456

---
+++ src/main/kotlin/p14xx/Problem1456.kt
@@ -0,0 +1,40 @@
+package p14xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxVowels(s: String, k: Int): Int {
+            val vowels = hashSetOf('a', 'e', 'i', 'o', 'u')
+
+            var left = 0
+            var right = 0
+
+            var result = 0
+            var count = 0
+            while (right < s.length) {
+                if (s[right++] in vowels) {
+                    count++
+                }
+
+                if (right - left > k) {
+                    if (s[left++] in vowels) {
+                        count--
+                    }
+                }
+
+                result = result.coerceAtLeast(count)
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxVowels(
+            "abciiidef", 4
+        ).also { println("${it} should be ${it}") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

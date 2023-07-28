782f981cf4de676b4087ffd2413e799b21e7ebb9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-28 09:37:25 +0800

    Problem 2050

---
+++ src/main/kotlin/p20xx/Problem2050.kt
@@ -0,0 +1,55 @@
+package p20xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minimumTime(n: Int, relations: Array<IntArray>, time: IntArray): Int {
+            val start = IntArray(n)
+            val parentCount = IntArray(n)
+            val children = Array(n) { hashSetOf<Int>() }
+
+            val roots = (0 until n).toMutableSet()
+
+            relations.forEach { (prev, next) ->
+                roots.remove(next - 1)
+                children[prev - 1].add(next - 1)
+                parentCount[next - 1]++
+            }
+
+            val list = LinkedList(roots)
+            var result = 0
+
+            while (true) {
+                val root = list.pollFirst() ?: break
+                val finishTime = start[root] + time[root]
+                result = result.coerceAtLeast(finishTime)
+
+                children[root].forEach {
+                    start[it] = start[it].coerceAtLeast(finishTime)
+
+                    if (--parentCount[it] == 0) {
+                        list.add(it)
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minimumTime(
+            5, arrayOf(
+                intArrayOf(1, 5),
+                intArrayOf(2, 5),
+                intArrayOf(3, 5),
+                intArrayOf(3, 4),
+                intArrayOf(4, 5),
+            ), intArrayOf(1, 2, 3, 4, 5)
+        ).also { println("${it} should be ${it}") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

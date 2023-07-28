d29f56ba580f1d4fcdaae731179e1fdec1765793
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-28 10:56:53 +0800

    Problem 1473

---
+++ src/main/kotlin/p14xx/Problem1473.kt
@@ -0,0 +1,61 @@
+package p14xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minCost(houses: IntArray, cost: Array<IntArray>, m: Int, n: Int, target: Int): Int {
+            //houseIndex to precolor -- diffArea to totalCost
+            val cache = hashMapOf<Pair<Int, Int>, Map<Int, Int>>()
+            fun dfs(houseIndex: Int, precolor: Int): Map<Int, Int> {
+                if (houseIndex == m) {
+                    return mapOf(0 to 0)
+                }
+
+                val cacheKey = houseIndex to precolor
+
+                if (cacheKey in cache) {
+                    return cache[cacheKey].orEmpty()
+                }
+
+                val result = hashMapOf<Int, Int>()
+
+                fun process(colorIndex: Int, paintCost: Int) {
+                    val baseDiff = if (colorIndex == precolor) 0 else 1
+
+                    dfs(houseIndex + 1, colorIndex).forEach { (d, t) ->
+                        val diff = d + baseDiff
+                        val total = t + paintCost
+
+                        result[diff] = result[diff]?.takeIf { it < total } ?: total
+                    }
+                }
+                if (houses[houseIndex] > 0) {
+                    process(houses[houseIndex] - 1, 0)
+                } else {
+                    cost[houseIndex].forEachIndexed(::process)
+                }
+
+                cache[cacheKey] = result
+
+                return result
+            }
+
+            return dfs(0, -1)[target] ?: -1
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minCost(
+            intArrayOf(0, 0, 0, 0, 0), arrayOf(
+                intArrayOf(1, 10),
+                intArrayOf(10, 1),
+                intArrayOf(10, 1),
+                intArrayOf(1, 10),
+                intArrayOf(5, 1),
+            ), 5, 2, 3
+        ).also { println("${it} should be ${it}") }
+
+    }.also { println("Time cost: ${it}ms") }
+}
+

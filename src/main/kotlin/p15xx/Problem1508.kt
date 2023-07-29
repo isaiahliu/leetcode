06dd3415986cb61d0fb23dfe90e6dd405871d63a
isaiahliu <isaiahliu@outlook.com>
2023-07-29 22:27:54 +0800

    Problem 1508

---
+++ src/main/kotlin/p15xx/Problem1508.kt
@@ -0,0 +1,41 @@
+package p15xx
+
+import util.Ints
+import util.get
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun rangeSum(nums: IntArray, n: Int, left: Int, right: Int): Int {
+            for (i in 1 until nums.size) {
+                nums[i] += nums[i - 1]
+            }
+
+            val sums = IntArray(n * (n + 1) / 2)
+            var sumIndex = 0
+            for (i in nums.indices) {
+                val left = nums.getOrNull(i - 1) ?: 0
+                for (j in i until nums.size) {
+                    sums[sumIndex++] = nums[j] - left
+                }
+            }
+            sums.sort()
+            var result = 0L
+            val m = 1000000007
+
+            for (i in left - 1 until right) {
+                result += sums[i]
+                result %= m
+            }
+
+            return result.toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().rangeSum(
+            Ints[1, 2, 3, 4], 4, 1, 2
+        ).also { println(it) }
+    }
+}
+

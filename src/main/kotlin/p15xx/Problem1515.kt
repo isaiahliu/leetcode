29ce0fb4420a1098006aa492451690c03acd27bc
isaiahliu <ziqiang.liu@enoch-car.com>
2023-07-31 12:06:13 +0800

    Problem 1515

--- src/main/kotlin/p15xx/Problem1515.kt
+++ src/main/kotlin/p15xx/Problem1515.kt
@@ -1,7 +1,6 @@
 package p15xx
 
 import kotlin.math.sqrt
-import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -21,34 +20,20 @@ fun main() {
             y /= n.toDouble()
 
             while (true) {
-                for (i in 0 until n) {
-                    val randIndex: Int = Random.nextInt(n)
-                    val rx = positions[i][0]
-                    val ry = positions[i][1]
-                    positions[i][0] = positions[randIndex][0]
-                    positions[i][1] = positions[randIndex][1]
-                    positions[randIndex][0] = rx
-                    positions[randIndex][1] = ry
-                }
-
                 val xPrev = x
                 val yPrev = y
-                var i = 0
-                while (i < n) {
-                    val j = (i + n).toDouble().coerceAtMost(n.toDouble()).toInt()
-                    var dx = 0.0
-                    var dy = 0.0
-                    for (k in i until j) {
-                        val pos = positions[k]
-                        dx += (x - pos[0]) / (sqrt((x - pos[0]) * (x - pos[0]) + (y - pos[1]) * (y - pos[1])) + eps)
-                        dy += (y - pos[1]) / (sqrt((x - pos[0]) * (x - pos[0]) + (y - pos[1]) * (y - pos[1])) + eps)
-                    }
-                    x -= alpha * dx
-                    y -= alpha * dy
-
-                    alpha *= 1.0 - decay
-                    i += n
+                val j = n.toDouble().coerceAtMost(n.toDouble()).toInt()
+                var dx = 0.0
+                var dy = 0.0
+                for (k in 0 until j) {
+                    val pos = positions[k]
+                    dx += (x - pos[0]) / (sqrt((x - pos[0]) * (x - pos[0]) + (y - pos[1]) * (y - pos[1])) + eps)
+                    dy += (y - pos[1]) / (sqrt((x - pos[0]) * (x - pos[0]) + (y - pos[1]) * (y - pos[1])) + eps)
                 }
+                x -= alpha * dx
+                y -= alpha * dy
+
+                alpha *= 1.0 - decay
 
                 if (sqrt((x - xPrev) * (x - xPrev) + (y - yPrev) * (y - yPrev)) < eps) {
                     break

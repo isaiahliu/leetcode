d0461222f8e8a4264a1ad969f46870cbc69d6c96
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-01 15:17:10 +0800

    Problem 1534

---
+++ src/main/kotlin/p15xx/Problem1534.kt
@@ -0,0 +1,39 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countGoodTriplets(arr: IntArray, a: Int, b: Int, c: Int): Int {
+            var result = 0
+
+            for (i in arr.indices) {
+                val range1 = arr[i] - a..arr[i] + a
+                val range3 = arr[i] - c..arr[i] + c
+
+                for (j in i + 1 until arr.size) {
+                    if (arr[j] !in range1) {
+                        continue
+                    }
+
+                    val range2 = arr[j] - b..arr[j] + b
+
+                    for (k in j + 1 until arr.size) {
+                        if (arr[k] in range2 && arr[k] in range3) {
+                            result++
+                        }
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countGoodTriplets(
+            intArrayOf(), 1, 3, 4
+        ).also { println(it) }
+    }
+}
+

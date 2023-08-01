a662b04dfcd02cc70dab2f28860dff816331b783
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-01 16:41:53 +0800

    Problem 1544

---
+++ src/main/kotlin/p15xx/Problem1544.kt
@@ -0,0 +1,33 @@
+package p15xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun makeGood(s: String): String {
+            val chars = LinkedList<Char>()
+
+            val upper = 'A'..'Z'
+            val lower = 'a'..'z'
+
+            s.forEach {
+                when {
+                    chars.isEmpty() -> chars.add(it)
+                    it in upper && chars.peekLast() == 'a' + (it - 'A') -> chars.pollLast()
+                    it in lower && chars.peekLast() == 'A' + (it - 'a') -> chars.pollLast()
+                    else -> chars.add(it)
+                }
+            }
+
+            return String(chars.toCharArray())
+        }
+    }
+
+    measureTimeMillis {
+        Solution().makeGood(
+            "leEeetcode"
+        ).also { println(it) }
+    }
+}
+

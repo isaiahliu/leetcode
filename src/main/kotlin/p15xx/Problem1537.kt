5e24fc8e88692c5d232f5a65685edec2829f7feb
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-01 16:05:37 +0800

    Problem 1537

---
+++ src/main/kotlin/p15xx/Problem1537.kt
@@ -0,0 +1,51 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxSum(nums1: IntArray, nums2: IntArray): Int {
+            val map = hashMapOf<Int, IntArray>()
+
+            nums1.forEachIndexed { index, i ->
+                map.computeIfAbsent(i) { intArrayOf(nums1.size, nums2.size) }[0] = index
+            }
+
+            nums2.forEachIndexed { index, i ->
+                map.computeIfAbsent(i) { intArrayOf(nums1.size, nums2.size) }[1] = index
+            }
+
+            val cache = hashMapOf<Int, Long>()
+            fun dfs(num: Int): Long {
+                if (num in cache) {
+                    return cache[num] ?: 0L
+                }
+
+                var result = 0L
+                map[num]?.also { (index1, index2) ->
+                    nums1.getOrNull(index1 + 1)?.also {
+                        result = result.coerceAtLeast(dfs(it))
+                    }
+
+                    nums2.getOrNull(index2 + 1)?.also {
+                        result = result.coerceAtLeast(dfs(it))
+                    }
+                }
+
+                result += num
+                cache[num] = result
+
+                return result
+            }
+
+            return (dfs(nums1[0]).coerceAtLeast(dfs(nums2[0])) % 1000000007).toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxSum(
+            intArrayOf(), intArrayOf()
+        ).also { println(it) }
+    }
+}
+

0545cd68a98fe72414798bd8ca737539f88d2acd
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-01 16:54:04 +0800

    Problem 1545

---
+++ src/main/kotlin/p15xx/Problem1545.kt
@@ -0,0 +1,45 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findKthBit(n: Int, k: Int): Char {
+            var s = 0
+            val sizes = IntArray(20) {
+                s = s * 2 + 1
+                s
+            }
+
+            fun find(sizeIndex: Int, pos: Int): Int {
+                val size = sizes[sizeIndex]
+                return when {
+                    sizeIndex == 0 -> {
+                        0
+                    }
+
+                    pos < size / 2 -> {
+                        find(sizeIndex - 1, pos)
+                    }
+
+                    pos == size / 2 -> {
+                        1
+                    }
+
+                    else -> {
+                        find(sizeIndex - 1, size - pos - 1) xor 1
+                    }
+                }
+            }
+
+            return '0' + find(n - 1, k - 1)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findKthBit(
+            4, 11
+        ).also { println(it) }
+    }
+}
+

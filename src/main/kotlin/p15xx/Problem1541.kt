085108f0d4ea1992c8529b60de692821677e65b3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-01 16:22:01 +0800

    Problem 1541

---
+++ src/main/kotlin/p15xx/Problem1541.kt
@@ -0,0 +1,49 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minInsertions(s: String): Int {
+            var result = 0
+
+            var leftCount = 0
+
+            var index = 0
+            while (index < s.length) {
+                when (s[index]) {
+                    '(' -> {
+                        leftCount++
+                    }
+
+                    ')' -> {
+                        if (s.getOrNull(index + 1) == ')') {
+                            index++
+                        } else {
+                            result++
+                        }
+
+                        if (leftCount > 0) {
+                            leftCount--
+                        } else {
+                            result++
+                        }
+                    }
+                }
+
+                index++
+            }
+
+            result += leftCount * 2
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minInsertions(
+            "{{}}}"
+        ).also { println(it) }
+    }
+}
+

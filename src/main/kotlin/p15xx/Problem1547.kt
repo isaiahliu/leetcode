a90e8165649faf1459ef36e9072c9947d50abfd2
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-01 17:10:36 +0800

    Problem 1547

---
+++ src/main/kotlin/p15xx/Problem1547.kt
@@ -0,0 +1,45 @@
+package p15xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minCost(n: Int, cuts: IntArray): Int {
+            val cutSet = TreeSet<Int>()
+            cuts.forEach { cutSet.add(it) }
+
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+            fun dfs(from: Int, to: Int): Int {
+                val cacheKey = from to to
+
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                var result = Int.MAX_VALUE
+                cutSet.subSet(from, false, to, false).takeIf { it.isNotEmpty() }?.also {
+                    it.forEach { c ->
+                        result = result.coerceAtMost(dfs(from, c) + dfs(c, to))
+                    }
+
+                    result += to - from
+                } ?: run {
+                    result = 0
+                }
+
+                cache[cacheKey] = result
+                return result
+            }
+
+            return dfs(0, n)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minCost(
+            5, intArrayOf()
+        ).also { println(it) }
+    }
+}
+

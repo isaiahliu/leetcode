bfe5a8f7af03f02c07e8853f6b53bb4fc7a6cdb9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-01 09:42:55 +0800

    Problem 1519

---
+++ src/main/kotlin/p15xx/Problem1519.kt
@@ -0,0 +1,48 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countSubTrees(n: Int, edges: Array<IntArray>, labels: String): IntArray {
+            val connected = Array(n) { hashSetOf<Int>() }
+
+            edges.forEach { (from, to) ->
+                connected[from].add(to)
+                connected[to].add(from)
+            }
+
+            val result = IntArray(n) { 1 }
+
+            val visited = hashSetOf(0)
+            fun dfs(node: Int): Map<Char, Int> {
+                val counts = hashMapOf(labels[node] to 1)
+
+                connected[node].forEach {
+                    if (visited.add(it)) {
+                        dfs(it).forEach { (char, count) ->
+                            if (char == labels[node]) {
+                                result[node] += count
+                            }
+
+                            counts[char] = (counts[char] ?: 0) + count
+                        }
+                    }
+                }
+
+                return counts
+            }
+
+            dfs(0)
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countSubTrees(
+            5, arrayOf(), ""
+        ).also { println(it) }
+    }
+}
+

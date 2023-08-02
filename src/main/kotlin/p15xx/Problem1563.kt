69d386897434460eed266d929897f7146fc07da2
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-02 15:54:02 +0800

    Problem 1563

---
+++ src/main/kotlin/p15xx/Problem1563.kt
@@ -0,0 +1,60 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun stoneGameV(stoneValue: IntArray): Int {
+            var sum = 0
+            val sums = IntArray(stoneValue.size + 1) {
+                stoneValue.getOrNull(it - 1)?.also {
+                    sum += it
+                }
+
+                sum
+            }
+
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+            fun dfs(from: Int, to: Int): Int {
+                val cacheKey = from to to
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                var result = 0
+
+                (from + 1 until to).forEach { mid ->
+                    val leftSum = sums[mid] - sums[from]
+                    val rightSum = sums[to] - sums[mid]
+
+                    result = result.coerceAtLeast(
+                        when {
+                            leftSum > rightSum -> {
+                                rightSum + dfs(mid, to)
+                            }
+
+                            leftSum < rightSum -> {
+                                leftSum + dfs(from, mid)
+                            }
+
+                            else -> {
+                                leftSum + dfs(from, mid).coerceAtLeast(dfs(mid, to))
+                            }
+                        }
+                    )
+                }
+
+                cache[cacheKey] = result
+                return result
+            }
+            return dfs(0, stoneValue.size)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().stoneGameV(
+            intArrayOf(6, 2, 3, 4, 5, 5)
+        ).also { println(it) }
+    }
+}
+

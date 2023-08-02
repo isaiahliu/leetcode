e885c514a6ac0fe62899625cc7e881e114351c88
isaiahliu <isaiahliu@outlook.com>
2023-08-02 21:52:38 +0800

    Problem 1575

---
+++ src/main/kotlin/p15xx/Problem1575.kt
@@ -0,0 +1,54 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countRoutes(locations: IntArray, start: Int, finish: Int, fuel: Int): Int {
+            val m = 1000000007
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+            fun dfs(pos: Int, remain: Int): Int {
+                if (remain < 0) {
+                    return 0
+                }
+
+                val cacheKey = pos to remain
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                var result = 0L
+
+                if (pos == finish) {
+                    result++
+                }
+
+                val location = locations[pos]
+                locations.forEachIndexed { index, i ->
+                    if (index != pos) {
+                        val cost = if (location > i) {
+                            location - i
+                        } else {
+                            i - location
+                        }
+
+                        result += dfs(index, remain - cost)
+                        result %= m
+                    }
+                }
+
+                cache[cacheKey] = result.toInt()
+                return result.toInt()
+            }
+
+            return dfs(start, fuel)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countRoutes(
+            intArrayOf(2, 3, 6, 8, 4), 1, 3, 5
+        ).also { println(it) }
+    }
+}
+

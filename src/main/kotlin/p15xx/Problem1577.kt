02a989020a5af449c1631cc36f5bbfda1fb5def7
isaiahliu <isaiahliu@outlook.com>
2023-08-02 22:08:04 +0800

    Problem 1577

---
+++ src/main/kotlin/p15xx/Problem1577.kt
@@ -0,0 +1,43 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun numTriplets(nums1: IntArray, nums2: IntArray): Int {
+            fun find(left: IntArray, right: IntArray): Int {
+                val leftNums = hashMapOf<Long, Int>()
+
+                left.forEach {
+                    leftNums[it.toLong() * it] = (leftNums[it.toLong() * it] ?: 0) + 1
+                }
+
+                val rightNums = hashMapOf<Long, Int>()
+
+                var result = 0
+                right.forEach {
+                    leftNums.forEach { n, count ->
+                        if (n % it == 0L) {
+                            rightNums[n / it]?.also {
+                                result += count * it
+                            }
+                        }
+                    }
+
+                    rightNums[it.toLong()] = (rightNums[it.toLong()] ?: 0) + 1
+                }
+
+                return result
+            }
+
+            return find(nums1, nums2) + find(nums2, nums1)
+        }
+    }
+
+    measureTimeMillis {
+        Solution().numTriplets(
+            intArrayOf(), intArrayOf()
+        ).also { println(it) }
+    }
+}
+

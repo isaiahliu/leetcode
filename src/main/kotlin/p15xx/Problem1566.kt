d73f58f073ec33589eab0403f26bccdbb4d6b59e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-02 16:01:05 +0800

    Problem 1566

---
+++ src/main/kotlin/p15xx/Problem1566.kt
@@ -0,0 +1,33 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun containsPattern(arr: IntArray, m: Int, k: Int): Boolean {
+            loop@ for (i in 0..arr.size - m * k) {
+                for (pos in 0 until m) {
+                    val set = hashSetOf<Int>()
+                    repeat(k) { r ->
+                        set.add(arr[i + r * m + pos])
+                    }
+
+                    if (set.size > 1) {
+                        continue@loop
+                    }
+                }
+
+                return true
+            }
+
+            return false
+        }
+    }
+
+    measureTimeMillis {
+        Solution().containsPattern(
+            intArrayOf(1, 4, 4, 4), 1, 3
+        ).also { println(it) }
+    }
+}
+

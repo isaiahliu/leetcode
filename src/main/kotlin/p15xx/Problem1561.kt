40676dd270125212f37afa32d4b774975765678e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-02 15:25:31 +0800

    Problem 1561

---
+++ src/main/kotlin/p15xx/Problem1561.kt
@@ -0,0 +1,18 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxCoins(piles: IntArray): Int {
+            return piles.sorted().let { (piles.size / 3 until piles.size step 2).map { index -> it[index] }.sum() }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxCoins(
+            intArrayOf(3, 1)
+        ).also { println(it) }
+    }
+}
+

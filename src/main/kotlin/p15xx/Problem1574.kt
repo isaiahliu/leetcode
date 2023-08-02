e76c0f0089096f84741c7450697f523c06fe68ef
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-02 18:37:05 +0800

    Problem 1574

---
+++ src/main/kotlin/p15xx/Problem1574.kt
@@ -0,0 +1,49 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findLengthOfShortestSubarray(arr: IntArray): Int {
+            var rightIndex = arr.lastIndex
+
+            while (rightIndex > 0) {
+                if (arr[rightIndex - 1] <= arr[rightIndex]) {
+                    rightIndex--
+                } else {
+                    break
+                }
+            }
+
+            var result = rightIndex
+
+            if (result == 0) {
+                return 0
+            }
+
+            var leftIndex = 0
+            while (true) {
+                while (rightIndex < arr.size && arr[rightIndex] < arr[leftIndex]) {
+                    rightIndex++
+                }
+
+                result = result.coerceAtMost(rightIndex - leftIndex - 1)
+
+                leftIndex++
+
+                if (arr[leftIndex] < arr[leftIndex - 1]) {
+                    break
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findLengthOfShortestSubarray(
+            intArrayOf(10, 13, 17, 21, 15, 15, 9, 17, 22, 22, 13)
+        ).also { println(it) }
+    }
+}
+

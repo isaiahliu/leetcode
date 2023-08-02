4bb615bfd16dce856095b4aab3e5bb6ecb178729
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-02 15:40:57 +0800

    Problem 1562

---
+++ src/main/kotlin/p15xx/Problem1562.kt
@@ -0,0 +1,71 @@
+package p15xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findLatestStep(arr: IntArray, m: Int): Int {
+            class Group {
+                var size = 0
+
+                var innerParent: Group? = null
+                    private set
+
+                var parent: Group
+                    set(value) {
+                        innerParent = value
+                        value.size += size
+                    }
+                    get() {
+                        return innerParent?.parent?.also {
+                            innerParent = it
+                        } ?: this
+                    }
+
+                fun join(target: Group) {
+                    val leftParent = parent
+                    val rightParent = target.parent
+
+                    if (leftParent != rightParent) {
+                        leftParent.parent = rightParent
+                    }
+                }
+            }
+
+            val groups = hashMapOf<Int, Group>()
+
+            val matchParents = hashSetOf<Group>()
+
+            var result = -1
+            arr.forEachIndexed { index, pos ->
+                val group = Group().also { it.size++ }
+
+                groups[pos] = group
+
+                groups[pos - 1]?.join(group)
+                groups[pos + 1]?.join(group)
+
+                if (matchParents.none { it.parent.size == m }) {
+                    matchParents.clear()
+                }
+
+                if (group.parent.size == m) {
+                    matchParents.add(group.parent)
+                }
+
+                if (matchParents.isNotEmpty()) {
+                    result = index + 1
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findLatestStep(
+            intArrayOf(3, 5, 1, 2, 4), 1
+        ).also { println(it) }
+    }
+}
+

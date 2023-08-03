306721f52ac90e9ca47ed703efebe04d570111e3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-03 16:25:00 +0800

    Problem 1601

---
+++ src/main/kotlin/p16xx/Problem1601.kt
@@ -0,0 +1,59 @@
+package p16xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maximumRequests(n: Int, requests: Array<IntArray>): Int {
+            fun Int.forEachBit(consumer: (Int) -> Unit) {
+                var t = this
+
+                var index = 0
+                while (t > 0) {
+                    if (t % 2 == 1) {
+                        consumer(index)
+                    }
+
+                    t /= 2
+                    index++
+                }
+            }
+
+            var result = 0
+
+            repeat(1 shl requests.size) {
+                val counts = IntArray(n)
+
+                var balance = 0
+                var people = 0
+                it.forEachBit {
+                    people++
+
+                    requests[it].also { (from, to) ->
+                        counts[from]++
+                        if (counts[from] == 1) {
+                            balance++
+                        }
+                        counts[to]--
+                        if (counts[to] == 0) {
+                            balance--
+                        }
+                    }
+                }
+
+                if (balance == 0) {
+                    result = result.coerceAtLeast(people)
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maximumRequests(
+            1, arrayOf()
+        ).also { println("${it} should be ${it}") }
+    }
+}
+

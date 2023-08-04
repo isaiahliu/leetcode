97bf884d9ecd8cccb15c344ad8c5a0ff2176e56e
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-04 10:17:28 +0800

    Problem 1620

---
+++ src/main/kotlin/p16xx/Problem1620.kt
@@ -0,0 +1,52 @@
+package p16xx
+
+import kotlin.math.sqrt
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun bestCoordinate(towers: Array<IntArray>, radius: Int): IntArray {
+            val result = intArrayOf(0, 0)
+
+            if (towers.all { it[2] == 0 }) {
+                return result
+            }
+
+            var max = -1
+
+            val xs = towers.map { it[0] }
+            val ys = towers.map { it[1] }
+
+            for (x in xs.min()..xs.max()) {
+                for (y in ys.min()..ys.max()) {
+                    var strength = 0
+                    towers.forEach { (tx, ty, str) ->
+                        val d2 = (tx - x) * (tx - x) + (ty - y) * (ty - y)
+
+                        if (d2 <= radius * radius) {
+                            strength += (str / (1 + sqrt(d2.toDouble()))).toInt()
+                        }
+                    }
+
+                    if (strength > max) {
+                        result[0] = x
+                        result[1] = y
+                        max = strength
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().bestCoordinate(
+            arrayOf(
+                intArrayOf(1, 2, 5),
+                intArrayOf(2, 1, 7),
+                intArrayOf(3, 1, 9),
+            ), 2
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

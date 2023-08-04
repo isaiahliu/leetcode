35906eac61b37b30535c7c4643561dca293c1790
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-04 18:40:55 +0800

    Problem 1652

---
+++ src/main/kotlin/p16xx/Problem1652.kt
@@ -0,0 +1,42 @@
+package p16xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun decrypt(code: IntArray, k: Int): IntArray {
+            val result = IntArray(code.size)
+
+            when {
+                k > 0 -> {
+                    var sum = code.take(k).sum()
+
+                    repeat(code.size) {
+                        sum -= code[it]
+                        sum += code[(it + k) % code.size]
+
+                        result[it] = sum
+                    }
+                }
+
+                k < 0 -> {
+                    var sum = code.takeLast(-k).sum()
+
+                    for (index in code.lastIndex downTo 0) {
+                        sum -= code[index]
+                        sum += code[(index + k + code.size) % code.size]
+
+                        result[index] = sum
+                    }
+                }
+            }
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().decrypt(
+            intArrayOf(10, 5, 7, 7, 3, 2, 10, 3, 6, 9, 1, 6), -4
+        ).toList().also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

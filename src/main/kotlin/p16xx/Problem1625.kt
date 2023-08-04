c836a0f86093a91834bb944c765444a5a5a10217
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-04 13:26:28 +0800

    Problem 1625

---
+++ src/main/kotlin/p16xx/Problem1625.kt
@@ -0,0 +1,41 @@
+package p16xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun findLexSmallestString(s: String, a: Int, b: Int): String {
+            val visited = hashSetOf(s)
+            val tasks = hashSetOf(s)
+
+            while (tasks.isNotEmpty()) {
+                tasks.toSet().also { tasks.clear() }.forEach {
+                    val rotateStr = it.drop(b) + it.take(b)
+                    if (visited.add(rotateStr)) {
+                        tasks.add(rotateStr)
+                    }
+
+                    String(it.mapIndexed { index, c ->
+                        if (index % 2 == 1) {
+                            '0' + (c - '0' + a) % 10
+                        } else {
+                            c
+                        }
+                    }.toCharArray()).also {
+                        if (visited.add(it)) {
+                            tasks.add(it)
+                        }
+                    }
+                }
+            }
+
+            return visited.min()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().findLexSmallestString(
+            "5525", 9, 2
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

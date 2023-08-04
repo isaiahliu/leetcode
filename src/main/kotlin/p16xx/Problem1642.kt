fdcbc5f7bf34e0f8f7072f70c7871b562f27df72
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-04 17:08:39 +0800

    Problem 1642

--- src/main/kotlin/p16xx/Problem1642.kt
+++ src/main/kotlin/p16xx/Problem1642.kt
@@ -1,33 +1,33 @@
 package p16xx
 
+import java.util.*
 import kotlin.system.measureTimeMillis
 
 fun main() {
     class Solution {
         fun furthestBuilding(heights: IntArray, bricks: Int, ladders: Int): Int {
-            val diff = IntArray(heights.size) {
-                if (it > 0 && heights[it] > heights[it - 1]) {
-                    heights[it] - heights[it - 1]
-                } else {
-                    0
+            var index = 1
+            val laddersQueue = PriorityQueue<Int>()
+            var brickRequirement = 0
+            while (index < heights.size) {
+                val diff = heights[index] - heights[index - 1]
+
+                if (diff > 0) {
+                    laddersQueue.add(diff)
+
+                    if (laddersQueue.size > ladders) {
+                        brickRequirement += laddersQueue.poll()
+
+                        if (bricks < brickRequirement) {
+                            break
+                        }
+                    }
                 }
+
+                index++
             }
 
-            fun binarySearch(start: Int, end: Int): Int {
-                if (start > end) {
-                    return Int.MIN_VALUE
-                }
-
-                val mid = (start + end) / 2
-
-                return if (bricks >= diff.take(mid + 1).sortedDescending().drop(ladders).sum()) {
-                    mid.coerceAtLeast(binarySearch(mid + 1, end))
-                } else {
-                    binarySearch(start, mid - 1)
-                }
-            }
-
-            return binarySearch(0, heights.lastIndex)
+            return index - 1
         }
     }
 

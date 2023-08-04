96398dd7aad390531496629c0ddb33102dbdbae3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-04 17:32:01 +0800

    Problem 1643

---
+++ src/main/kotlin/p16xx/Problem1643.kt
@@ -0,0 +1,60 @@
+package p16xx
+
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        val cache = hashMapOf<Pair<Int, Int>, Int>()
+        fun p(row: Int, column: Int): Int {
+            if (row == 0 || column == 0) {
+                return 1
+            }
+
+            val cacheKey = row.coerceAtMost(column) to row.coerceAtLeast(column)
+            if (cacheKey in cache) {
+                return cache[cacheKey] ?: 0
+            }
+
+            var result = BigInteger.ONE
+
+            repeat(row + column) {
+                result *= (it + 1).toBigInteger()
+            }
+
+            repeat(row) {
+                result /= (it + 1).toBigInteger()
+            }
+
+            repeat(column) {
+                result /= (it + 1).toBigInteger()
+            }
+
+            cache[cacheKey] = result.toInt()
+            return result.toInt()
+        }
+
+        fun kthSmallestPath(destination: IntArray, k: Int): String {
+            val (r, c) = destination
+
+            return if (r == 0) {
+                "H".repeat(c)
+            } else if (c == 0) {
+                "V".repeat(r)
+            } else {
+                val horizontalFirst = p(r, c - 1)
+                if (horizontalFirst >= k) {
+                    "H" + kthSmallestPath(intArrayOf(r, c - 1), k)
+                } else {
+                    "V" + kthSmallestPath(intArrayOf(r - 1, c), k - horizontalFirst)
+                }
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().kthSmallestPath(
+            intArrayOf(15, 15), 1
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}

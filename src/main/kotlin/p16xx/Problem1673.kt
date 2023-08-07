df88a2fa41388886590ffa9820357ba8dd2c55a8
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-07 15:27:43 +0800

    Problem 1673

---
+++ src/main/kotlin/p16xx/Problem1673.kt
@@ -0,0 +1,62 @@
+package p16xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun mostCompetitive(nums: IntArray, k: Int): IntArray {
+            class Node(val num: Int) {
+                lateinit var prev: Node
+                lateinit var next: Node
+            }
+
+            val head = Node(Int.MIN_VALUE)
+            val tail = Node(Int.MAX_VALUE)
+            head.next = tail
+            tail.prev = head
+
+            val decNodes = LinkedList<Node>()
+
+            nums.forEachIndexed { index, num ->
+                val newNode = Node(nums[index]).also {
+                    it.prev = tail.prev
+                    it.next = tail
+                    tail.prev.next = it
+                    tail.prev = it
+                }
+
+                if (newNode.num < newNode.prev.num) {
+                    decNodes.add(newNode)
+                }
+
+                if (index >= k) {
+                    decNodes.poll()?.also {
+                        it.prev.prev.next = it
+                        it.prev = it.prev.prev
+
+                        if (it.num < it.prev.num) {
+                            decNodes.push(it)
+                        }
+                    } ?: run {
+                        tail.prev.prev.next = tail
+                        tail.prev = tail.prev.prev
+                    }
+                }
+            }
+
+            var t = head
+            return IntArray(k) {
+                t = t.next
+                t.num
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().mostCompetitive(
+            intArrayOf(), 3
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}
+

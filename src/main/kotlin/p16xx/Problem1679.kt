500b0b10fd8c66bb09f50ab34650b42836f0b931
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-07 17:05:05 +0800

    Problem 167

---
+++ src/main/kotlin/p16xx/Problem1679.kt
@@ -0,0 +1,35 @@
+package p16xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxOperations(nums: IntArray, k: Int): Int {
+            val map = hashMapOf<Int, Int>()
+
+            var result = 0
+            nums.forEach { num ->
+                map[k - num]?.also {
+                    if (it == 1) {
+                        map.remove(k - num)
+                    } else {
+                        map[k - num] = it - 1
+                    }
+
+                    result++
+                } ?: run {
+                    map[num] = (map[num] ?: 0) + 1
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxOperations(
+            intArrayOf(3, 1, 3, 4, 3), 6
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}
+

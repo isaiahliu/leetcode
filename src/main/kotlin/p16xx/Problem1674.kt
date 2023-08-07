0228646cc5e4b21ad917b46eab38485b97e054d3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-07 16:41:47 +0800

    Problem 1674

---
+++ src/main/kotlin/p16xx/Problem1674.kt
@@ -0,0 +1,37 @@
+package p16xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minMoves(nums: IntArray, limit: Int): Int {
+            val diff = IntArray(limit * 2 + 2)
+
+            repeat(nums.size / 2) {
+                val left = nums[it].coerceAtMost(nums[nums.lastIndex - it])
+                val right = nums[it].coerceAtLeast(nums[nums.lastIndex - it])
+
+                diff[left + 1]--
+                diff[left + right]--
+                diff[left + right + 1]++
+                diff[right + limit + 1]++
+            }
+
+            var result = Int.MAX_VALUE
+            var sum = 0
+
+            diff.forEach {
+                sum += it
+                result = result.coerceAtMost(nums.size + sum)
+            }
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minMoves(
+            intArrayOf(1, 2, 4, 3), 4
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}
+

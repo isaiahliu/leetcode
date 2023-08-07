c97428d5861042d7ae5c9703b441bddffbe3a0b3
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-07 11:17:13 +0800

    Problem 1664

---
+++ src/main/kotlin/p16xx/Problem1664.kt
@@ -0,0 +1,46 @@
+package p16xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun waysToMakeFair(nums: IntArray): Int {
+            val forward = Array(nums.size + 1) { IntArray(2) }
+
+            val sums = intArrayOf(0, 0)
+
+            nums.forEachIndexed { index, num ->
+                sums[index % 2] += num
+
+                forward[index + 1][0] = sums[0]
+                forward[index + 1][1] = sums[1]
+            }
+
+            val backward = Array(nums.size + 1) { IntArray(2) }
+            sums[0] = 0
+            sums[1] = 0
+
+            var result = 0
+            for (index in nums.lastIndex downTo 0) {
+                val num = nums[index]
+
+                if (forward[index][0] + backward[index + 1][1] == forward[index][1] + backward[index + 1][0]) {
+                    result++
+                }
+
+                sums[index % 2] += num
+                backward[index][0] = sums[0]
+                backward[index][1] = sums[1]
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().waysToMakeFair(
+            intArrayOf(1, 2, 3)
+        ).also { println(it) }
+    }.also { println("Time cost: ${it}ms") }
+}
+

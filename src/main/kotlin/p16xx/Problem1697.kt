8f235738d2514040fd561505b6ea3691b4ed1f69
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-08 10:45:59 +0800

    Problem 1697

---
+++ src/main/kotlin/p16xx/Problem1697.kt
@@ -0,0 +1,60 @@
+package p16xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun distanceLimitedPathsExist(n: Int, edgeList: Array<IntArray>, queries: Array<IntArray>): BooleanArray {
+            class Group {
+                var innerParent: Group? = null
+                    private set
+
+                var parent: Group
+                    set(value) {
+                        innerParent = value
+                    }
+                    get() {
+                        return innerParent?.parent?.also {
+                            innerParent = it
+                        } ?: this
+                    }
+
+                fun join(target: Group) {
+                    val leftParent = parent
+                    val rightParent = target.parent
+
+                    if (leftParent != rightParent) {
+                        leftParent.parent = rightParent
+                    }
+                }
+            }
+
+            val result = BooleanArray(queries.size)
+            val groups = Array(n) { Group() }
+
+            val queue = PriorityQueue<IntArray>(compareBy { it[2] })
+            edgeList.forEach { queue.add(it) }
+
+            queries.indices.sortedBy { queries[it][2] }.forEach { queryIndex ->
+                val (from, to, limit) = queries[queryIndex]
+
+                while (queue.isNotEmpty() && queue.peek()[2] < limit) {
+                    queue.poll().also { (f, t, _) ->
+                        groups[f].join(groups[t])
+                    }
+                }
+
+                result[queryIndex] = groups[from].parent == groups[to].parent
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().distanceLimitedPathsExist(
+            1, arrayOf(), arrayOf()
+        ).also { println("${it} should be ${it}") }
+    }.also { println("Time cost: ${it}ms") }
+}

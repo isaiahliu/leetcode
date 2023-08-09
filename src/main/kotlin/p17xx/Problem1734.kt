b3dfc55bf4496b0e27a3c9e1cb73a65eaff49add
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-09 18:26:42 +0800

    Problem 1734

--- src/main/kotlin/p17xx/Problem1734.kt
+++ src/main/kotlin/p17xx/Problem1734.kt
@@ -5,12 +5,11 @@ import kotlin.system.measureTimeMillis
 fun main() {
     class Solution {
         fun decode(encoded: IntArray): IntArray {
-            var num = 0
-            repeat(encoded.size + 1) {
-                num = num xor (it + 1)
-            }
+            var num = 1
 
             for (i in 1 until encoded.size step 2) {
+                num = num xor i * 2
+                num = num xor (i * 2 + 1)
                 num = num xor encoded[i]
             }
 

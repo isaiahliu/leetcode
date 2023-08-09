fca8fbbc559976bb1870b31bb18460974b3d2795
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-09 18:25:47 +0800

    Problem 1734

---
+++ src/main/kotlin/p17xx/Problem1734.kt
@@ -0,0 +1,33 @@
+package p17xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun decode(encoded: IntArray): IntArray {
+            var num = 0
+            repeat(encoded.size + 1) {
+                num = num xor (it + 1)
+            }
+
+            for (i in 1 until encoded.size step 2) {
+                num = num xor encoded[i]
+            }
+
+            val result = IntArray(encoded.size + 1) {
+                if (it > 0) {
+                    num = num xor encoded[it - 1]
+                }
+                num
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().decode(
+            intArrayOf(6, 5, 4, 6)
+        ).toList().also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

deb6d1c92d0b633f76060c332fc9747e46acf1de
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-09 10:04:11 +0800

    Problem 1722

---
+++ src/main/kotlin/p17xx/Problem1722.kt
@@ -0,0 +1,58 @@
+package p17xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minimumHammingDistance(source: IntArray, target: IntArray, allowedSwaps: Array<IntArray>): Int {
+            class Group {
+                var innerParent: Group? = null
+                    private set
+
+                var parent: Group
+                    set(value) {
+                        innerParent = value
+                    }
+                    get() {
+                        return innerParent?.parent?.also {
+                            innerParent = it
+                        } ?: this
+                    }
+
+                fun join(target: Group) {
+                    val leftParent = parent
+                    val rightParent = target.parent
+
+                    if (leftParent != rightParent) {
+                        leftParent.parent = rightParent
+                    }
+                }
+            }
+
+            val groups = Array(source.size) { Group() }
+            allowedSwaps.forEach { (from, to) ->
+                groups[from].join(groups[to])
+            }
+
+            var result = groups.size
+            groups.indices.groupBy { groups[it].parent }.values.forEach {
+                val map1 = it.groupingBy { source[it] }.eachCount()
+                val map2 = it.groupingBy { target[it] }.eachCount()
+
+                map1.forEach { (key, count) ->
+                    map2[key]?.also {
+                        result -= count.coerceAtMost(it)
+                    }
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minimumHammingDistance(
+            intArrayOf(), intArrayOf(), arrayOf()
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

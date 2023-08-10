6e7b7c778395bd4db1cfbdf83fc16164324f9231
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-10 10:11:17 +0800

    Problem 1736

---
+++ src/main/kotlin/p17xx/Problem1736.kt
@@ -0,0 +1,41 @@
+package p17xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maximumTime(time: String): String {
+            var (h1, h2, _, m1, m2) = time.toCharArray()
+
+            if (h1 == '?') {
+                h1 = when (h2) {
+                    in '4'..'9' -> '1'
+                    else -> '2'
+                }
+            }
+
+            if (h2 == '?') {
+                h2 = when (h1) {
+                    '2' -> '3'
+                    else -> '9'
+                }
+            }
+
+            if (m1 == '?') {
+                m1 = '5'
+            }
+
+            if (m2 == '?') {
+                m2 = '9'
+            }
+
+            return "${h1}${h2}:${m1}${m2}"
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maximumTime(
+            ""
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

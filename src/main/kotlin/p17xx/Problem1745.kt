4b2a9de0b1217e00aa6f3628a416c832a7e8eed1
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-10 17:39:51 +0800

    Problem 1745

--- src/main/kotlin/p17xx/Problem1745.kt
+++ src/main/kotlin/p17xx/Problem1745.kt
@@ -5,6 +5,69 @@ import kotlin.system.measureTimeMillis
 fun main() {
     class Solution {
         fun checkPartitioning(s: String): Boolean {
+            val str = s.toCharArray().joinToString("#")
+
+            val r = IntArray(str.length) { 1 }
+
+            var rightBorder = 0
+            var center = 0
+
+            for (i in 1 until r.size) {
+                var size = 1
+                if (i < rightBorder) {
+                    size = r[center * 2 - i].coerceAtMost(rightBorder - i + 1)
+                }
+
+                if (i + size - 1 >= rightBorder) {
+                    center = i
+                    while (true) {
+                        if ((str.getOrNull(i - size) ?: break) == (str.getOrNull(i + size) ?: break)) {
+                            size++
+                            rightBorder = i + size - 1
+                        } else {
+                            break
+                        }
+                    }
+                }
+
+                r[i] = size
+            }
+
+            val maxSuffixIndex = IntArray(r.size) { Int.MAX_VALUE }
+            maxSuffixIndex[maxSuffixIndex.lastIndex] = maxSuffixIndex.lastIndex
+
+            for (i in r.lastIndex downTo 0) {
+                if (r[i] == r.size - i) {
+                    (i * 2 - r.size).takeIf { it >= 0 }?.also {
+                        maxSuffixIndex[it] = i
+                    }
+                }
+
+                maxSuffixIndex.getOrNull(i + 1)?.also {
+                    maxSuffixIndex[i] = maxSuffixIndex[i].coerceAtMost(it)
+                }
+            }
+
+            r.forEachIndexed { index, i ->
+                if (i == index + 1) {
+                    val left = index + i - 1
+
+                    maxSuffixIndex.getOrNull(left + 3 - (index % 2))?.also { suffix ->
+                        val right = suffix - r[suffix] + 1
+
+                        val mid = (left + right) / 2
+
+                        if (r[mid] >= mid - left) {
+                            return true
+                        }
+                    }
+                }
+            }
+
+            return false
+        }
+
+        fun checkPartitioning2(s: String): Boolean {
             val leftMatches = hashSetOf(0)
             val rightMatches = hashSetOf(s.lastIndex)
             val midMatches = hashSetOf<Pair<Int, Int>>()
@@ -46,7 +109,19 @@ fun main() {
 
     measureTimeMillis {
         Solution().checkPartitioning(
-            ""
-        ).also { println("${it} should be $it") }
+            "aabc"
+        ).also { println("${it} should be true") }
+
+        Solution().checkPartitioning(
+            "acab"
+        ).also { println("${it} should be false") }
+
+        Solution().checkPartitioning(
+            "abcbdd"
+        ).also { println("${it} should be true") }
+
+//        Solution().checkPartitioning(
+//            "abacdcee"
+//        ).also { println("${it} should be $it") }
     }.also { println("Time cost: ${it}ms") }
 }

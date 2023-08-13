9a79345c14001c1fd40ee3380a6a2613882fc7fa
isaiahliu <isaiahliu@outlook.com>
2023-08-13 23:42:27 +0800

    Problem 1799

---
+++ src/main/kotlin/p17xx/Problem1799.kt
@@ -0,0 +1,58 @@
+package p17xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxScore(nums: IntArray): Int {
+            val dp = IntArray(1 shl nums.size)
+
+            fun Int.bitPos(): List<Int> {
+                var t = this
+
+                val result = arrayListOf<Int>()
+                var index = 0
+                while (t > 0) {
+                    if (t % 2 == 1) {
+                        result.add(index)
+                    }
+
+                    t /= 2
+                    index++
+                }
+                return result
+            }
+
+
+            repeat(dp.size) { status ->
+                val bitCount = Integer.bitCount(status)
+                if (bitCount.let { it > 0 && it % 2 == 0 }) {
+                    val picked = status.bitPos()
+
+                    val times = bitCount / 2
+                    var result = 0
+                    picked.forEachIndexed { index, pos1 ->
+                        for (index2 in index + 1 until picked.size) {
+                            val pos2 = picked[index2]
+
+                            result = result.coerceAtLeast(
+                                nums[pos1].toBigInteger().gcd(nums[pos2].toBigInteger())
+                                    .toInt() * times + dp[status - (1 shl pos1) - (1 shl pos2)]
+                            )
+                        }
+                    }
+
+                    dp[status] = result
+                }
+            }
+
+            return dp.last()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxScore(
+            intArrayOf(1, 4, 10, 3, 1)
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

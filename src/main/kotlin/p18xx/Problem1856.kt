c3dd52ce8c63d253f36da295f2f85b72801cda8f
isaiahliu <isaiahliu@outlook.com>
2023-08-15 22:59:56 +0800

    Problem 1856

---
+++ src/main/kotlin/p18xx/Problem1856.kt
@@ -0,0 +1,53 @@
+package p18xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun maxSumMinProduct(nums: IntArray): Int {
+            val lengths = Array(nums.size) {
+                IntArray(2)
+            }
+            arrayOf(
+                { index: Int -> index },
+                { index: Int -> nums.lastIndex - index }).forEachIndexed { lengthIndex, indexGetter ->
+                val map = LinkedList<Pair<Int, Int>>()
+
+                for (index in nums.indices) {
+                    val numIndex = indexGetter(index)
+                    val num = nums[numIndex]
+
+                    var size = 1
+                    while (map.isNotEmpty() && map.peekLast().first >= num) {
+                        size += map.pollLast().second
+                    }
+
+                    map.add(num to size)
+                    lengths[numIndex][lengthIndex] = size
+                }
+            }
+            var result = 0L
+
+            var sum = 0L
+            val sums = LongArray(nums.size) {
+                sum += nums[it]
+                sum
+            }
+            lengths.forEachIndexed { index, l ->
+                result = result.coerceAtLeast(
+                    nums[index].toLong() * (sums[index + l[1] - 1] - (sums.getOrNull(index - l[0]) ?: 0))
+                )
+            }
+
+            return (result % 1000000007).toInt()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().maxSumMinProduct(
+            intArrayOf(3, 1, 5, 6, 4, 2)
+        ).also { println("${it} should be $it") }
+
+    }.also { println("Time cost: ${it}ms") }
+}

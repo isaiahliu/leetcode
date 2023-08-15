2514d5c2c6a9a932e8e01c0b2caecac2f9bf74b0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-15 17:55:54 +0800

    Problem 1849

---
+++ src/main/kotlin/p18xx/Problem1849.kt
@@ -0,0 +1,45 @@
+package p18xx
+
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun splitString(s: String): Boolean {
+            fun dfs(index: Int, prevNum: BigInteger): Boolean {
+                if (index == s.length) {
+                    return true
+                }
+
+                if (prevNum == BigInteger.ZERO) {
+                    return false
+                }
+
+                val target = prevNum - BigInteger.ONE
+
+                for (i in index until s.length) {
+                    if (s.substring(index..i).toBigInteger() == target && dfs(i + 1, target)) {
+                        return true
+                    }
+                }
+
+                return false
+            }
+
+            for (i in 0 until s.lastIndex) {
+                if (dfs(i + 1, s.substring(0..i).toBigInteger())) {
+                    return true
+                }
+            }
+
+            return false
+        }
+    }
+
+    measureTimeMillis {
+        Solution().splitString(
+            ""
+        ).also { println("${it} should be $it") }
+
+    }.also { println("Time cost: ${it}ms") }
+}

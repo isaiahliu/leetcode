b78dad20ef5d7a93478a0d232b83cf053b49252d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-15 13:41:53 +0800

    Problem 1828

---
+++ src/main/kotlin/p18xx/Problem1828.kt
@@ -0,0 +1,37 @@
+package p18xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countPoints(points: Array<IntArray>, queries: Array<IntArray>): IntArray {
+            val map = TreeMap<Int, MutableMap<Int, Int>>()
+            points.forEach { (x, y) ->
+                map.computeIfAbsent(x) { hashMapOf() }.also {
+                    it[y] = (it[y] ?: 0) + 1
+                }
+            }
+
+            return queries.map { (x, y, r) ->
+                var result = 0
+                map.subMap(x - r, true, x + r, true).forEach { x1, ys ->
+                    ys.forEach { (y1, count) ->
+                        if ((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y) <= r * r) {
+                            result += count
+                        }
+                    }
+                }
+
+                result
+            }.toIntArray()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countPoints(
+            arrayOf(), arrayOf()
+        ).also { println("${it} should be $it") }
+
+    }.also { println("Time cost: ${it}ms") }
+}

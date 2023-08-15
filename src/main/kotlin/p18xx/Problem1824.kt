b17a20d330fb130286878f98a6b0dc138ea1d89f
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-15 11:15:42 +0800

    Problem 1824

---
+++ src/main/kotlin/p18xx/Problem1824.kt
@@ -0,0 +1,50 @@
+package p18xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minSideJumps(obstacles: IntArray): Int {
+            val stones = Array(3) {
+                TreeSet<Int>()
+            }
+
+            obstacles.forEachIndexed { index, i ->
+                if (i > 0) {
+                    stones[i - 1].add(index)
+                }
+            }
+            var result = 0
+            var runway = 1
+            var pos = 0
+
+            while (true) {
+                stones[runway].higher(pos)?.also { stone ->
+                    result++
+
+                    var bestRunway = 0
+                    var distance = 1
+                    stones.forEachIndexed { index, s ->
+                        s.ceiling(stone - 1)?.also {
+                            if (it > distance) {
+                                distance = it
+                                bestRunway = index
+                            }
+                        } ?: return result
+
+                        runway = bestRunway
+                        pos = stone - 1
+                    }
+
+                } ?: return result
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minSideJumps(
+            intArrayOf()
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

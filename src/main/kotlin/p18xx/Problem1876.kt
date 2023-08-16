4bbb3e6dc114ed17cf4f018a7d609cf0335870e6
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-16 14:37:00 +0800

    Problem 1876

---
+++ src/main/kotlin/p18xx/Problem1876.kt
@@ -0,0 +1,59 @@
+package p18xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countGoodSubstrings(s: String): Int {
+            if (s.length < 3) {
+                return 0
+            }
+
+            val counts = IntArray(26)
+            var dupCount = 0
+
+            fun addChar(char: Char) {
+                if (counts[char - 'a'] == 1) {
+                    dupCount++
+                }
+
+                counts[char - 'a']++
+            }
+
+            fun removeChar(char: Char) {
+                if (counts[char - 'a'] == 2) {
+                    dupCount--
+                }
+
+                counts[char - 'a']--
+            }
+
+            repeat(3) {
+                addChar(s[it])
+            }
+
+            var result = 0
+
+            if (dupCount == 0) {
+                result++
+            }
+
+            for (i in 3 until s.length) {
+                removeChar(s[i - 3])
+                addChar(s[i])
+
+                if (dupCount == 0) {
+                    result++
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countGoodSubstrings(
+            "aababcabc"
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

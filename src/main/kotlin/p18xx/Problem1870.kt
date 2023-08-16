2b73de56127e6ec622c7f45b384b50e615d00210
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-16 11:30:53 +0800

    Problem 1870

---
+++ src/main/kotlin/p18xx/Problem1870.kt
@@ -0,0 +1,36 @@
+package p18xx
+
+import kotlin.math.ceil
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minSpeedOnTime(dist: IntArray, hour: Double): Int {
+            fun binarySearch(min: Int, max: Int): Int {
+                if (min > max) {
+                    return Int.MAX_VALUE
+                }
+
+                val mid = min + (max - min) / 2
+
+                val cost = dist.fold(0.0) { used, distance ->
+                    ceil(used) + distance.toDouble() / mid
+                }
+
+                return if (cost <= hour) {
+                    mid.coerceAtMost(binarySearch(min, mid - 1))
+                } else {
+                    binarySearch(mid + 1, max)
+                }
+            }
+
+            return binarySearch(1, 10000000).takeIf { it < Int.MAX_VALUE } ?: -1
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minSpeedOnTime(
+            intArrayOf(1, 3, 2), 6.0
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

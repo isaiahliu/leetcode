7f703f3fbe8782744f2418d73629597414c04e88
isaiahliu <isaiahliu@outlook.com>
2023-08-16 20:53:18 +0800

    Problem 1905

---
+++ src/main/kotlin/p19xx/Problem1905.kt
@@ -0,0 +1,51 @@
+package p19xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun countSubIslands(grid1: Array<IntArray>, grid2: Array<IntArray>): Int {
+            val lands = hashSetOf<Pair<Int, Int>>()
+
+            grid2.forEachIndexed { r, row ->
+                row.forEachIndexed { c, num ->
+                    if (num > 0) {
+                        lands.add(r to c)
+                    }
+                }
+            }
+
+            var result = 0
+            while (lands.isNotEmpty()) {
+                val first = lands.first()
+                lands.remove(first)
+                val tasks = hashSetOf(first)
+
+                var success = true
+                while (tasks.isNotEmpty()) {
+                    tasks.toSet().also { tasks.clear() }.forEach { (r, c) ->
+                        success = success and (grid1[r][c] == 1)
+
+                        arrayOf(r - 1 to c, r + 1 to c, r to c - 1, r to c + 1).forEach {
+                            if (lands.remove(it)) {
+                                tasks.add(it)
+                            }
+                        }
+                    }
+                }
+
+                if (success) {
+                    result++
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().countSubIslands(
+            arrayOf(), arrayOf()
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

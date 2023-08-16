73bc677acb3916c561c8b4135410940ce4305188
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-16 15:34:47 +0800

    Problem 1882

---
+++ src/main/kotlin/p18xx/Problem1882.kt
@@ -0,0 +1,48 @@
+package p18xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun assignTasks(servers: IntArray, tasks: IntArray): IntArray {
+            val result = IntArray(tasks.size)
+
+            val idle = PriorityQueue(compareBy<Int> { servers[it] }.thenBy { it })
+            idle.addAll(servers.indices)
+
+            val busy = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+
+            var tasksIndex = 0
+
+            var time = 0
+
+            while (tasksIndex < tasks.size) {
+                while (busy.isNotEmpty() && busy.peek().second <= time) {
+                    idle.add(busy.poll().first)
+                }
+
+                while (tasksIndex <= time && tasksIndex < tasks.size && idle.isNotEmpty()) {
+                    val server = idle.poll()
+                    result[tasksIndex] = server
+                    busy.add(server to time + tasks[tasksIndex])
+                    tasksIndex++
+                }
+
+                if (idle.isNotEmpty()) {
+                    time++
+                } else {
+                    time = busy.peek().second
+                }
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().assignTasks(
+            intArrayOf(3, 3, 2), intArrayOf(1, 2, 3, 2, 1, 2)
+        ).toList().also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

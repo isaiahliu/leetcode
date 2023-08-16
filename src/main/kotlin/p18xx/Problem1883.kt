9b1af6c018e3f20edf21b686e14b064ee1118f93
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-16 16:15:41 +0800

    Problem 1883

---
+++ src/main/kotlin/p18xx/Problem1883.kt
@@ -0,0 +1,55 @@
+package p18xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minSkips(dist: IntArray, speed: Int, hoursBefore: Int): Int {
+            fun Pair<Int, Int>.min(right: Pair<Int, Int>?): Pair<Int, Int> {
+                return right?.takeIf { right.first < first || right.first == first && right.second < second } ?: this
+            }
+
+            fun Pair<Int, Int>.valid(): Pair<Int, Int>? {
+                return this.takeIf { (hour, rem) -> hour < hoursBefore || hour == hoursBefore && rem == 0 }
+            }
+
+            val dp = hashMapOf(0 to (0 to 0))
+
+            for (road in dist) {
+                dp.toMap().also { dp.clear() }.forEach { (skipCount, cost) ->
+                    var (hour, rem) = cost
+
+                    rem += road % speed
+                    hour += road / speed + rem / speed
+                    rem %= speed
+
+                    if (rem > 0) {
+                        (hour to rem).min(dp[skipCount + 1]).valid()?.also {
+                            dp[skipCount + 1] = it
+                        }
+
+                        (hour + 1 to 0).min(dp[skipCount]).valid()?.also {
+                            dp[skipCount] = it
+                        }
+                    } else {
+                        (hour to 0).min(dp[skipCount]).valid()?.also {
+                            dp[skipCount] = it
+                        }
+                    }
+                }
+
+                if (dp.isEmpty()) {
+                    return -1
+                }
+            }
+
+            return dp.keys.min()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minSkips(
+            intArrayOf(1, 3, 2), 4, 2
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

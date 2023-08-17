94a45a321be6a44d7dce7e1c178679020cfa8556
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-17 17:05:24 +0800

    Problem 1932

---
+++ src/main/kotlin/p19xx/Problem1932.kt
@@ -0,0 +1,113 @@
+package p19xx
+
+import util.TreeNode
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun canMerge(trees: List<TreeNode>): TreeNode? {
+            val singles = hashSetOf<Int>()
+
+            val roots = hashMapOf<Int, TreeNode>()
+            val leaves = hashSetOf<Int>()
+
+            fun TreeNode.dfs(): Boolean {
+                if (left == null && right == null) {
+                    if (`val` in leaves) {
+                        return false
+                    }
+                    leaves.add(`val`)
+                } else {
+                    left?.also {
+                        if (!it.dfs()) {
+                            return false
+                        }
+                    }
+
+                    right?.also {
+                        if (!it.dfs()) {
+                            return false
+                        }
+                    }
+                }
+
+                return true
+            }
+
+            trees.forEach {
+                when {
+                    it.left == null && it.right == null -> {
+                        singles.add(it.`val`)
+                    }
+
+                    it.`val` in roots -> {
+                        return null
+                    }
+
+                    !it.dfs() -> {
+                        return null
+                    }
+
+                    else -> {
+                        roots[it.`val`] = it
+                    }
+                }
+            }
+
+            if (roots.isEmpty()) {
+                return trees.firstOrNull()?.takeIf { singles.size == 1 }
+            }
+
+            singles.removeAll(roots.keys)
+            singles.removeAll(leaves)
+
+            if (singles.isNotEmpty()) {
+                return null
+            }
+
+            val singleRoot = roots.keys - leaves
+            if (singleRoot.size != 1) {
+                return null
+            }
+
+            val root = roots.remove(singleRoot.first()) ?: return null
+
+            var failed = false
+            fun TreeNode.mergeAndValidate(min: Int, max: Int) {
+                if (failed) {
+                    return
+                }
+
+                if (`val` < min || `val` > max) {
+                    failed = true
+                    return
+                }
+
+                if (left == null && right == null) {
+                    roots.remove(`val`)?.also {
+                        left = it.left
+                        right = it.right
+                    }
+                }
+
+                left?.mergeAndValidate(min, `val` - 1)
+                right?.mergeAndValidate(`val` + 1, max)
+            }
+
+            return root.takeIf {
+                root.mergeAndValidate(Int.MIN_VALUE, Int.MAX_VALUE)
+                !failed && roots.isEmpty()
+            }
+        }
+    }
+
+    measureTimeMillis {
+        Solution().canMerge(
+            listOf(
+                TreeNode(2, TreeNode(1), TreeNode(3)),
+                TreeNode(1),
+                TreeNode(3),
+            )
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

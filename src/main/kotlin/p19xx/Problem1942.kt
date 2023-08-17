94b45805045b2e0588eeee93f6ad16afeb01c53e
isaiahliu <isaiahliu@outlook.com>
2023-08-17 20:54:17 +0800

    Problem 1942

---
+++ src/main/kotlin/p19xx/Problem1942.kt
@@ -0,0 +1,54 @@
+package p19xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun smallestChair(times: Array<IntArray>, targetFriend: Int): Int {
+            val chairs = TreeSet<Int>()
+            repeat(times.size) {
+                chairs.add(it)
+            }
+
+            val use = IntArray(times.size)
+
+            val queue = PriorityQueue(compareBy<Pair<Int, Pair<Int, Int>>> { it.first }.thenBy { it.second.second })
+
+            times.forEachIndexed { index, (join, leave) ->
+                queue.add(join to (index to 1))
+                queue.add(leave to (index to 0))
+            }
+
+            while (queue.isNotEmpty()) {
+                val (index, event) = queue.poll().second
+
+                if (index == targetFriend) {
+                    return chairs.first()
+                }
+
+                when (event) {
+                    0 -> {
+                        chairs.add(use[index])
+                    }
+
+                    1 -> {
+                        use[index] = chairs.pollFirst() ?: 0
+                    }
+                }
+            }
+
+            return 0
+        }
+    }
+
+    measureTimeMillis {
+        Solution().smallestChair(
+            arrayOf(
+                intArrayOf(3, 10),
+                intArrayOf(1, 5),
+                intArrayOf(2, 6),
+            ), 0
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

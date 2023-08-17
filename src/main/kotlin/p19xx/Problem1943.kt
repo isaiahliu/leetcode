c2c2fc222debed15fdb973b9450fc0d13e927c1f
isaiahliu <isaiahliu@outlook.com>
2023-08-17 21:34:09 +0800

    Problem 1943

---
+++ src/main/kotlin/p19xx/Problem1943.kt
@@ -0,0 +1,51 @@
+package p19xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun splitPainting(segments: Array<IntArray>): List<List<Long>> {
+            val map = TreeMap<Int, MutableSet<Pair<Int, Long>>>()
+
+            segments.forEach { (start, end, color) ->
+                map.computeIfAbsent(start) { hashSetOf() }.add(0 to color.toLong())
+                map.computeIfAbsent(end) { hashSetOf() }.add(1 to color.toLong())
+            }
+
+            val result = arrayListOf<List<Long>>()
+
+            var (pre, s) = map.pollFirstEntry()
+            var colors = s.map { it.second }.sum()
+
+            while (map.isNotEmpty()) {
+                val (time, event) = map.pollFirstEntry()
+
+                if (colors > 0) {
+                    result.add(listOf(pre.toLong(), time.toLong(), colors))
+                }
+
+                event.forEach { (e, c) ->
+                    when (e) {
+                        0 -> colors += c
+                        1 -> colors -= c
+                    }
+                }
+
+                pre = time
+            }
+
+            return result
+        }
+    }
+
+    measureTimeMillis {
+        Solution().splitPainting(
+            arrayOf(
+                intArrayOf(3, 10),
+                intArrayOf(1, 5),
+                intArrayOf(2, 6),
+            )
+        ).also { println("${it} should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

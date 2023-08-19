0ee99ae5eeef53d5a24ac7f73af8228ecf6a2759
isaiahliu <isaiahliu@outlook.com>
2023-08-19 13:02:33 +0800

    Problem 1971

---
+++ src/main/kotlin/p19xx/Problem1971.kt
@@ -0,0 +1,49 @@
+package p19xx
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun validPath(n: Int, edges: Array<IntArray>, source: Int, destination: Int): Boolean {
+            class Group {
+                var innerParent: Group? = null
+                    private set
+
+                var parent: Group
+                    set(value) {
+                        innerParent = value
+                    }
+                    get() {
+                        return innerParent?.parent?.also {
+                            innerParent = it
+                        } ?: this
+                    }
+
+                fun join(target: Group) {
+                    val leftParent = parent
+                    val rightParent = target.parent
+
+                    if (leftParent != rightParent) {
+                        leftParent.parent = rightParent
+                    }
+                }
+            }
+
+            val groups = Array(n) {
+                Group()
+            }
+
+            edges.forEach { (from, to) ->
+                groups[from].join(groups[to])
+            }
+
+            return groups[source].parent == groups[destination].parent
+        }
+    }
+
+    measureTimeMillis {
+        Solution().validPath(
+            2, arrayOf(), 1, 1
+        ).also { println("$it should be $it") }
+    }.also { println("Time cost: ${it}ms") }
+}

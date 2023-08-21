59f4960530a74651099539d9bbfcd52005f37b43
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-21 10:20:37 +0800

    Refactor

--- src/main/kotlin/util/Utils.kt
+++ src/main/kotlin/util/Utils.kt
@@ -10,7 +10,7 @@ val input by lazy {
 
 fun expect(result: Any? = null, dsl: () -> Any?) {
     measureTimeMillis {
-        dsl.also { println("$it should be ${result ?: it}") }
+        dsl().also { println("$it should be ${result ?: it}") }
     }.also { println("Time cost: ${it}ms") }
 }
 

5d6cc5bbd2dd4a3d64a8c0e17b867b9095503227
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-21 09:38:52 +0800

    Refactor

--- src/main/kotlin/util/Utils.kt
+++ src/main/kotlin/util/Utils.kt
@@ -1,11 +1,16 @@
 package util
 
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 val input by lazy {
-    ClassLoader.getSystemResource("util/sample.txt")?.file?.let { File(it) }
-        ?.readLines()
-        .orEmpty()
+    ClassLoader.getSystemResource("util/sample.txt")?.file?.let { File(it) }?.readLines().orEmpty()
 
+}
+
+fun expect(result: Any? = null, dsl: () -> Any?) {
+    measureTimeMillis {
+        dsl.also { println("$it should be ${result ?: it}") }
+    }.also { println("Time cost: ${it}ms") }
 }
 

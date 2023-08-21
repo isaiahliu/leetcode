1622e6b0c840b6033439dd1a294ff7efe69a59e0
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-21 09:34:45 +0800

    Problem 2337

---
+++ src/main/kotlin/p23xx/Problem2337.kt
@@ -0,0 +1,92 @@
+package p23xx
+
+import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    class Solution {
+        fun minSpaceWastedKResizing(nums: IntArray, k: Int): Int {
+            val greaterNums = Array(nums.size) {
+                hashSetOf<Int>()
+            }
+
+            val greater = LinkedList<Int>()
+
+            for (index in nums.lastIndex downTo 0) {
+                val num = nums[index]
+
+                while (greater.isNotEmpty() && greater.peek() < num) {
+                    greater.poll()
+                }
+
+                greater.push(num)
+
+                greaterNums[index].addAll(greater)
+            }
+
+            val dp = Array(nums.size) {
+                //remainK -- currentSize -- waste
+                Array(k + 1) { hashMapOf<Int, Int>() }
+            }
+
+            greater.forEach {
+                dp[0][k][it] = it - nums[0]
+            }
+
+            fun MutableMap<Int, Int>.renew(size: Int, waste: Int) {
+                this[size] = (this[size] ?: Int.MAX_VALUE).coerceAtMost(waste)
+            }
+
+            for (index in 1 until dp.size) {
+                val last = dp[index - 1]
+                val current = dp[index]
+
+                val num = nums[index]
+
+                last.forEachIndexed { remainK, wastes ->
+                    var best = Int.MAX_VALUE
+                    wastes.forEach { (size, waste) ->
+                        if (size >= num) {
+                            current[remainK].renew(size, waste + size - num)
+                        }
+
+                        best = best.coerceAtMost(waste)
+                    }
+
+                    if (best < Int.MAX_VALUE) {
+                        greaterNums[index].forEach {
+                            current.getOrNull(remainK - 1)?.renew(it, best + it - num)
+                        }
+                    }
+                }
+            }
+
+            return dp[dp.lastIndex].map {
+                it.values.minOrNull() ?: Int.MAX_VALUE
+            }.min()
+        }
+    }
+
+    measureTimeMillis {
+        Solution().minSpaceWastedKResizing(
+            intArrayOf(39, 41, 11, 35, 47, 16, 11, 30, 25, 18), 1
+        ).also { println("$it should be 112") }
+
+        Solution().minSpaceWastedKResizing(
+            intArrayOf(10, 20, 15, 30, 20), 2
+        ).also { println("$it should be 15") }
+
+        Solution().minSpaceWastedKResizing(
+            intArrayOf(10, 20), 0
+        ).also { println("$it should be 10") }
+
+        Solution().minSpaceWastedKResizing(
+            intArrayOf(10, 20, 30), 1
+        ).also { println("$it should be 10") }
+
+
+        Solution().minSpaceWastedKResizing(
+            intArrayOf(13, 46, 42, 47, 35), 4
+        ).also { println("$it should be 0") }
+    }.also { println("Time cost: ${it}ms") }
+}

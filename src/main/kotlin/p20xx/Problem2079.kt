0c694baa4ec3d2d7000f71f5058bb78613eddddc
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-25 15:15:43 +0800

    Problem 2079

---
+++ src/main/kotlin/p20xx/Problem2079.kt
@@ -0,0 +1,34 @@
+package p20xx
+
+import util.expect
+
+fun main() {
+    class Solution {
+        fun wateringPlants(plants: IntArray, capacity: Int): Int {
+            var current = capacity
+
+            var index = 0
+
+            var result = 0
+            while (index < plants.size) {
+                if (current < plants[index]) {
+                    current = capacity - plants[index]
+                    result += index * 2
+                } else {
+                    current -= plants[index]
+                }
+
+                result++
+                index++
+            }
+
+            return result
+        }
+    }
+
+    expect {
+        Solution().wateringPlants(
+            intArrayOf(), 1
+        )
+    }
+}

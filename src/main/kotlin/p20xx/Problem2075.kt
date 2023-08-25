361b75c26209db6097143718c26a3a3729607238
isaiahliu <ziqiang.liu@enoch-car.com>
2023-08-25 14:26:30 +0800

    Problem 2075

--- src/main/kotlin/p20xx/Problem2075.kt
+++ src/main/kotlin/p20xx/Problem2075.kt
@@ -5,10 +5,8 @@ import util.expect
 fun main() {
     class Solution {
         fun decodeCiphertext(encodedText: String, rows: Int): String {
-            val size = encodedText.length / rows
-
             return String(CharArray((encodedText.length - (rows - 1) * (rows - 1)).coerceAtLeast(0)) {
-                encodedText[it % rows * (size + 1) + it / rows]
+                encodedText[it % rows * (encodedText.length / rows + 1) + it / rows]
             }).trimEnd()
         }
     }

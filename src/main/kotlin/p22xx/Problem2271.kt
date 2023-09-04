543a21f9eadf206a6c8c965fb3afb633625fcaf9
isaiahliu <ziqiang.liu@enoch-car.com>
2023-09-04 14:23:48 +0800

    Problem 2271

--- src/main/kotlin/p22xx/Problem2271.kt
+++ src/main/kotlin/p22xx/Problem2271.kt
@@ -5,67 +5,32 @@ import util.expect
 fun main() {
     class Solution {
         fun maximumWhiteTiles(tiles: Array<IntArray>, carpetLen: Int): Int {
-            class SegNode(val start: Int, val end: Int) {
-                var size = 0
+            tiles.sortBy { it[0] }
 
-                val children by lazy {
-                    val mid = (start + end) / 2
-                    arrayOf(
-                        SegNode(start, mid),
-                        SegNode(mid + 1, end),
-                    )
-                }
-
-                fun cover(from: Int, to: Int) {
-                    when {
-                        from <= start && to >= end -> {
-                            size = end - start + 1
-                        }
-
-                        from > end || to < start -> {
-                            return
-                        }
-
-                        else -> {
-                            size += to.coerceAtMost(end) - from.coerceAtLeast(start) + 1
-                            children.forEach {
-                                it.cover(from, to)
-                            }
-                        }
-                    }
-                }
-
-                fun query(from: Int, to: Int): Int {
-                    val left = from.coerceAtLeast(start)
-                    val right = to.coerceAtMost(end)
-
-                    return when {
-                        size == 0 || from > end || to < start -> {
-                            0
-                        }
-
-                        size == end - start + 1 -> {
-                            right - left + 1
-                        }
-
-                        else -> {
-                            children.sumOf {
-                                it.query(from, to)
-                            }
-                        }
-                    }
-                }
-            }
-
-            val root = SegNode(tiles.minOf { it[0] }, tiles.maxOf { it[1] })
-
-            tiles.forEach { (from, to) ->
-                root.cover(from, to)
-            }
+            var l = 0
+            var r = 1
+            var gap = 0
 
             var result = 0
-            tiles.forEach { (from, _) ->
-                result = result.coerceAtLeast(root.query(from, from + carpetLen - 1))
+            while (l < tiles.size) {
+                while (r < tiles.size && tiles[r][0] - tiles[l][0] < carpetLen) {
+                    gap += tiles[r][0] - tiles[r - 1][1] - 1
+                    r++
+                }
+
+                result =
+                    result.coerceAtLeast(
+                        carpetLen - (gap + (tiles[l][0] + carpetLen - tiles[r - 1][1] - 1).coerceAtLeast(
+                            0
+                        ))
+                    )
+
+                l++
+                if (l == tiles.size) {
+                    break
+                }
+
+                gap -= tiles[l][0] - tiles[l - 1][1] - 1
             }
 
             return result
@@ -75,12 +40,9 @@ fun main() {
     expect {
         Solution().maximumWhiteTiles(
             arrayOf(
-                intArrayOf(1, 5),
+                intArrayOf(1, 1),
                 intArrayOf(10, 11),
-                intArrayOf(12, 18),
-                intArrayOf(20, 25),
-                intArrayOf(30, 32),
-            ), 10
+            ), 2
         )
     }
 }

f41bedb4be4f1c78d4973c39df0385a76507b196
isaiahliu <ziqiang.liu@enoch-car.com>
2023-09-07 17:15:31 +0800

    Problem 2334

--- src/main/kotlin/p23xx/Problem2334.kt
+++ src/main/kotlin/p23xx/Problem2334.kt
@@ -1,78 +1,66 @@
 package p23xx
 
 import util.expect
-import java.util.*
 
 fun main() {
     class Solution {
         fun validSubarraySize(nums: IntArray, threshold: Int): Int {
-            if (threshold < nums.size) {
-                return nums.size
-            } else if (threshold < nums.max()) {
-                return 1
-            }
+            return when {
+                threshold < nums.size -> {
+                    nums.size
+                }
 
-            class Group {
-                var size = 0
+                threshold < nums.max() -> {
+                    1
+                }
 
-                var innerParent: Group? = null
-                    private set
+                else -> {
 
-                var parent: Group
-                    set(value) {
-                        innerParent = value
-                        value.size += size
+                    class Group {
+                        var size = 0
+
+                        var innerParent: Group? = null
+                            private set
+
+                        var parent: Group
+                            set(value) {
+                                innerParent = value
+                                value.size += size
+                            }
+                            get() {
+                                return innerParent?.parent?.also {
+                                    innerParent = it
+                                } ?: this
+                            }
+
+                        fun join(target: Group) {
+                            val leftParent = parent
+                            val rightParent = target.parent
+
+                            if (leftParent != rightParent) {
+                                leftParent.parent = rightParent
+                            }
+                        }
                     }
-                    get() {
-                        return innerParent?.parent?.also {
-                            innerParent = it
-                        } ?: this
+
+                    val groups = arrayOfNulls<Group>(nums.size)
+
+                    for (maxIndex in groups.indices.sortedByDescending { nums[it] }) {
+                        val num = nums[maxIndex]
+
+                        val group = Group().also { it.size++ }
+                        groups.getOrNull(maxIndex - 1)?.join(group)
+                        groups.getOrNull(maxIndex + 1)?.join(group)
+
+                        if (num > threshold / group.parent.size) {
+                            return group.parent.size
+                        }
+                        groups[maxIndex] = group
                     }
 
-                fun join(target: Group) {
-                    val leftParent = parent
-                    val rightParent = target.parent
-
-                    if (leftParent != rightParent) {
-                        leftParent.parent = rightParent
-                    }
+                    -1
                 }
             }
-
-            val groups = Array(nums.size) {
-                Group().also { it.size++ }
-            }
-
-            val queue = PriorityQueue<Int>(compareByDescending { nums[it] })
-            queue.addAll(groups.indices)
-
-            while (queue.isNotEmpty()) {
-                val maxIndex = queue.poll()
-                val num = nums[maxIndex]
-
-                if (num == 1) {
-                    break
-                }
-
-                nums.getOrNull(maxIndex + 1)?.takeIf { it >= num }?.also {
-                    groups[maxIndex].join(groups[maxIndex + 1])
-                }
-
-                nums.getOrNull(maxIndex - 1)?.takeIf { it >= num }?.also {
-                    groups[maxIndex].join(groups[maxIndex - 1])
-                }
-
-                var requireSize = threshold / num
-                while (num <= threshold / requireSize) {
-                    requireSize++
-                }
-
-                if (requireSize <= groups[maxIndex].parent.size) {
-                    return requireSize
-                }
-            }
-
-            return -1
         }
     }
 

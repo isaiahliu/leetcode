4cd4779270ff97793b9cba376e5f0ad8b41f5147
isaiahliu <ziqiang.liu@enoch-car.com>
2023-09-08 14:17:50 +0800

    Problem 2370

--- src/main/kotlin/p23xx/Problem2370.kt
+++ src/main/kotlin/p23xx/Problem2370.kt
@@ -4,50 +4,20 @@ import util.expect
 
 fun main() {
     class Solution {
-        fun validPartition(nums: IntArray): Boolean {
-            val visited = hashSetOf<Int>()
-            fun dfs(index: Int): Boolean {
-                return when {
-                    index == nums.size -> {
-                        return true
-                    }
+        fun longestIdealString(s: String, k: Int): Int {
+            val dp = IntArray(26)
 
-                    !visited.add(index) -> {
-                        return false
-                    }
-
-                    else -> {
-                        var result = false
-                        if (nums.size - index >= 2) {
-                            if (nums[index] == nums[index + 1]) {
-                                result = dfs(index + 2)
-                            }
-                        }
-
-                        if (!result && nums.size - index >= 3) {
-                            if (nums[index] == nums[index + 1] && nums[index] == nums[index + 2] || nums[index] + 1 == nums[index + 1] && nums[index] + 2 == nums[index + 2]) {
-                                result = dfs(index + 3)
-                            }
-                        }
-
-                        result
-                    }
-                }
+            s.forEach {
+                dp[it - 'a'] = (it - 'a' - k..it - 'a' + k).maxOf { dp.getOrNull(it) ?: 0 } + 1
             }
 
-            return dfs(0)
+            return dp.max()
         }
     }
 
     expect {
-        Solution().validPartition(
-            intArrayOf(
-                1699,
-                1700,
-                1701,
-                1701,
-                1701,
-            )
+        Solution().longestIdealString(
+            "", 1
         )
     }
 }

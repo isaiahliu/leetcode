0de221c8937abfae195dfa0a0a1700129a9c4955
isaiahliu <ziqiang.liu@enoch-car.com>
2023-09-08 14:15:22 +0800

    Problem 2369

--- src/main/kotlin/p23xx/Problem2369.kt
+++ src/main/kotlin/p23xx/Problem2369.kt
@@ -5,58 +5,37 @@ import util.expect
 fun main() {
     class Solution {
         fun validPartition(nums: IntArray): Boolean {
-            val SAME_START = 0
-            val SAME_END = 1
-            val LEFT = 2
-            val MID = 3
-            val RIGHT = 4
-
-            val visited = hashSetOf<Pair<Int, Int>>()
-            fun dfs(index: Int, status: Int): Boolean {
+            val visited = hashSetOf<Int>()
+            fun dfs(index: Int): Boolean {
                 return when {
                     index == nums.size -> {
-                        return status == SAME_START || status == LEFT
+                        return true
                     }
 
-                    !visited.add(index to status) -> {
+                    !visited.add(index) -> {
                         return false
                     }
 
-                    status == SAME_START -> {
-                        dfs(index + 1, SAME_END)
-                    }
-
-                    status == SAME_END -> {
-                        if (nums[index - 1] == nums[index]) {
-                            dfs(index + 1, SAME_START) || dfs(index + 1, SAME_END) || dfs(index + 1, LEFT)
-                        } else {
-                            false
-                        }
-                    }
-
-                    status == LEFT -> {
-                        dfs(index + 1, MID)
-                    }
-
-                    status == MID -> {
-                        if (nums[index - 1] + 1 == nums[index]) {
-                            dfs(index + 1, RIGHT)
-                        } else {
-                            false
-                        }
-                    }
-
                     else -> {
-                        if (nums[index - 1] + 1 == nums[index]) {
-                            dfs(index + 1, SAME_START) || dfs(index + 1, LEFT)
-                        } else {
-                            false
+                        var result = false
+                        if (nums.size - index >= 2) {
+                            if (nums[index] == nums[index + 1]) {
+                                result = dfs(index + 2)
+                            }
                         }
+
+                        if (!result && nums.size - index >= 3) {
+                            if (nums[index] == nums[index + 1] && nums[index] == nums[index + 2] || nums[index] + 1 == nums[index + 1] && nums[index] + 2 == nums[index + 2]) {
+                                result = dfs(index + 3)
+                            }
+                        }
+
+                        result
                     }
                 }
             }
 
-            return dfs(1, SAME_END) || dfs(1, MID)
+            return dfs(0)
         }
     }
 

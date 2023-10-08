eb1b811ec8a567262367625606c5a9ccf29bbb3e
isaiahliu <isaiahliu@outlook.com>
2023-10-09 00:35:10 +0800

    Problem 2578

--- src/main/kotlin/p25xx/Problem2578.kt
+++ src/main/kotlin/p25xx/Problem2578.kt
@@ -5,16 +5,13 @@ import util.expect
 fun main() {
     class Solution {
         fun splitNum(num: Int): Int {
-            var result = 0
             var base = 1
 
-            num.toString().toCharArray().sortedDescending().forEachIndexed { index, c ->
-                result += (c - '0') * base
-
-                base *= (index % 2) * 9 + 1
+            return num.toString().toCharArray().sortedDescending().foldIndexed(0) { index, sum, c ->
+                (sum + (c - '0') * base).also {
+                    base *= (index % 2) * 9 + 1
+                }
             }
-
-            return result
         }
     }
 

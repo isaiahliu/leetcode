13c337580d9ea1b8894721af0fb11f6eb12c0dda
isaiahliu <isaiahliu@outlook.com>
2024-03-27 01:15:01 +0800

    Problem 2642

---
+++ src/main/kotlin/p26xx/Problem2642.kt
@@ -0,0 +1,51 @@
+package p26xx
+
+import util.expect
+import java.util.*
+
+fun main() {
+    class Graph(val n: Int, edges: Array<IntArray>) {
+        val adjacent = Array(n) { hashMapOf<Int, Int>() }
+
+        init {
+            edges.forEach { (from, to, cost) ->
+                adjacent[from][to] = cost
+            }
+        }
+
+        fun addEdge(edge: IntArray) {
+            val (from, to, cost) = edge
+
+            adjacent[from][to] = cost
+        }
+
+        fun shortestPath(node1: Int, node2: Int): Int {
+            val visited = hashSetOf<Int>()
+
+            val nodes = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+            nodes += node1 to 0
+
+            while (nodes.isNotEmpty()) {
+                val (next, sum) = nodes.poll()
+
+                when {
+                    next == node2 -> {
+                        return sum
+                    }
+
+                    visited.add(next) -> {
+                        adjacent[next].forEach { (to, cost) ->
+                            nodes.add(to to sum + cost)
+                        }
+                    }
+                }
+            }
+
+            return -1
+        }
+    }
+
+    expect {
+        Graph(1, arrayOf())
+    }
+}

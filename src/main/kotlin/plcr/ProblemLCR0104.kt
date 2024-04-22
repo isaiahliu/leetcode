8ce232015f3540d77f8aa39229e7dcf305397a60
isaiahliu <isaiahliu@outlook.com>
2024-04-22 15:30:48 +0800

    Problem LCR0104

---
+++ src/main/kotlin/plcr/ProblemLCR0104.kt
@@ -0,0 +1,44 @@
+package plcr
+
+import util.expect
+
+fun main() {
+    class Solution {
+        fun combinationSum4(nums: IntArray, target: Int): Int {
+            val cache = hashMapOf<Pair<Int, Int>, Int>()
+            fun combination(startIndex: Int, target: Int): Int {
+                val cacheKey = startIndex to target
+                if (cacheKey in cache) {
+                    return cache[cacheKey] ?: 0
+                }
+
+                var result = 0
+
+                for (i in startIndex until nums.size) {
+                    when {
+                        nums[i] < target -> {
+                            result += combination(startIndex, target - nums[i])
+                        }
+
+                        nums[i] == target -> {
+                            result++
+                        }
+                    }
+                }
+
+                cache[cacheKey] = result
+
+                return result
+            }
+
+            return combination(0, target)
+        }
+    }
+
+    expect {
+        Solution().combinationSum4(
+            intArrayOf(1, 2, 3), 4
+        )
+    }
+}
+

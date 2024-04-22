5e82155deec141343719d4ae771adbcdeb70fdeb
isaiahliu <isaiahliu@outlook.com>
2024-04-22 15:26:45 +0800

    Problem LCR0082

---
+++ src/main/kotlin/plcr/ProblemLCR0082.kt
@@ -0,0 +1,75 @@
+package plcr
+
+import util.expect
+ 
+fun main() {
+    class Solution {
+        fun combinationSum2(candidates: IntArray, target: Int): List<List<Int>> {
+            fun binarySearch(leftIndex: Int, rightIndex: Int, target: Int): Int {
+                if (leftIndex > rightIndex) {
+                    return -1
+                }
+
+                if (target > candidates[rightIndex] || target < candidates[leftIndex]) {
+                    return -1
+                }
+
+                if (leftIndex == rightIndex) {
+                    return if (candidates[leftIndex] == target) leftIndex else -1
+                }
+
+                val midIndex = leftIndex + (rightIndex - leftIndex) / 2
+
+                return if (candidates[midIndex] == target) {
+                    midIndex
+                } else if (candidates[midIndex] > target) {
+                    binarySearch(leftIndex, midIndex, target)
+                } else {
+                    binarySearch(midIndex + 1, rightIndex, target)
+                }
+            }
+
+            candidates.sort()
+
+            val result = arrayListOf<List<Int>>()
+
+            if (binarySearch(0, candidates.lastIndex, target) >= 0) {
+                result.add(listOf(target))
+            }
+
+            fun walk(minIndex: Int, route: List<Int>, target: Int) {
+                var last = (candidates.getOrNull(minIndex) ?: 0) - 1
+                for (i in minIndex until candidates.size) {
+                    val num = candidates[i]
+                    if (num == last) {
+                        continue
+                    } else {
+                        last = num
+                    }
+
+                    val remaining = target - num
+
+                    if (remaining < num) {
+                        break
+                    } else {
+                        if (binarySearch(i + 1, candidates.lastIndex, remaining) >= 0) {
+                            result.add(route + num + remaining)
+                        }
+
+                        walk(i + 1, route + num, remaining)
+                    }
+                }
+            }
+
+            walk(0, emptyList(), target)
+
+            return result
+        }
+    }
+
+    expect {
+        Solution().combinationSum2(intArrayOf(1), 2)
+    }
+}
+
+

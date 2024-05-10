8f819842515774af728a6157b07f5b2e8ec03d80
isaiahliu <isaiahliu@outlook.com>
2024-05-11 00:26:11 +0800

    Problem 2960

---
+++ src/main/kotlin/p29xx/Problem2960.kt
@@ -0,0 +1,20 @@
+package p29xx
+
+import util.expect
+
+fun main() {
+    class Solution {
+        fun countTestedDevices(batteryPercentages: IntArray): Int {
+            return batteryPercentages.fold(0) { count, p ->
+                count + (p > count).compareTo(false)
+            }
+        }
+    }
+
+    println(true.compareTo(false))
+    expect {
+        Solution().countTestedDevices(
+            intArrayOf(7, 12, 9, 8, 9, 15)
+        )
+    }
+}

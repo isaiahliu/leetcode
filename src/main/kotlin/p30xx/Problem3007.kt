1bcded982ab1cfed385250225a79214c1b7a8abe
isaiahliu <isaiahliu@outlook.com>
2024-08-21 14:56:56 +0800

    1

--- src/main/kotlin/p30xx/Problem3007.kt
+++ src/main/kotlin/p30xx/Problem3007.kt
@@ -7,38 +7,34 @@ fun main() {
     class Solution {
         fun findMaximumNumber(k: Long, x: Int): Long {
             var remain = k
-            fun find(fixedValues: Int, maxDigit: Int): Long {
-                if (maxDigit == 0) {
-                    return 0L
-                }
-
-                var sum = ((1 % x).sign xor 1).toLong()
-                var value = fixedValues.toLong() + sum
-                var digit = 1
+            fun find(fixedPrefixValues: Int, maxDigit: Int): Long {
+                var sum = 0L
+                var value = 0L
+                var digit = 0
 
                 while (digit < maxDigit) {
-                    digit++
-                    var newSum = sum * 2
-                    var newValue = sum
-                    newValue += fixedValues * (1L shl (digit - 1))
-                    newValue += (digit % x).sign xor 1
+                    val preCount = 1L shl (digit++)
+                    val digitValue = (digit % x).sign xor 1
 
-                    newSum += ((digit % x).sign xor 1) * (1L shl (digit - 1))
+                    var newValue = sum
+                    sum *= 2
+                    sum += digitValue * preCount
+                    newValue += fixedPrefixValues * preCount
+                    newValue += digitValue
 
                     if (newValue <= remain) {
                         value = newValue
-                        sum = newSum
                     } else {
                         digit--
                         break
                     }
                 }
 
-                if (remain >= value) {
+                return if (digit > 0 && remain >= value) {
                     remain -= value
-                    return (1L shl (digit - 1)) + find(fixedValues + ((digit % x).sign xor 1), digit - 1)
+                    (1L shl (digit - 1)) + find(fixedPrefixValues + ((digit % x).sign xor 1), digit - 1)
                 } else {
-                    return 0L
+                    0L
                 }
             }
 
@@ -46,14 +42,14 @@ fun main() {
         }
     }
 
-    expect(50) {
-        Solution().findMaximumNumber(
-            19, 6
-        )
-    }
     expect(6) {
         Solution().findMaximumNumber(
             9, 1
+        )
+    }
+    expect(50) {
+        Solution().findMaximumNumber(
+            19, 6
         )
     }
 

5aecd3c1b54c90d88b12d6cbe3072cb86ac4e9b7
isaiahliu <isaiahliu@outlook.com>
2024-08-22 12:46:22 +0800

    1

--- src/main/kotlin/p31xx/Problem3133.kt
+++ src/main/kotlin/p31xx/Problem3133.kt
@@ -5,25 +5,28 @@ import util.expect
 fun main() {
     class Solution {
         fun minEnd(n: Int, x: Int): Long {
-            val bNum1 = x.toString(2).reversed()
+            var bNum1 = x
+            var bNum2 = n - 1
 
-            val bNum2 = (n - 1).toString(2).reversed()
+            var result = 0L
+            var base = 1L
 
-            return buildString {
-                var index2 = 0
+            while (bNum1 > 0) {
+                result += base * ((bNum1 % 2).takeIf { it == 1 } ?: run {
+                    (bNum2 % 2).also { bNum2 /= 2 }
+                })
 
-                bNum1.forEach {
-                    if (it == '1') {
-                        insert(0, it)
-                    } else {
-                        insert(0, bNum2.getOrElse(index2++) { '0' })
-                    }
-                }
+                bNum1 /= 2
+                base *= 2
+            }
 
-                while (index2 < bNum2.length) {
-                    insert(0, bNum2[index2++])
-                }
-            }.toLong(2)
+            while (bNum2 > 0) {
+                result += base * (bNum2 % 2)
+                bNum2 /= 2
+                base *= 2
+            }
+
+            return result
         }
     }
 

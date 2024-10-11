4de437f3d1f91896bc73854fdb10f67cd6a1c11d
isaiahliu <isaiahliu@outlook.com>
2024-10-11 10:16:23 +0800

    1

---
+++ src/main/kotlin/p31xx/Problem3164.kt
@@ -0,0 +1,42 @@
+package p31xx
+
+import util.expect
+import java.util.*
+import kotlin.math.sign
+
+fun main() {
+    class Solution {
+        fun numberOfPairs(nums1: IntArray, nums2: IntArray, k: Int): Long {
+            val map1 = TreeMap<Int, Int>()
+
+            nums1.forEach {
+                if (it % k == 0) {
+                    map1[it / k] = (map1[it / k] ?: 0) + 1
+                }
+            }
+
+            return nums2.sumOf { num2 ->
+                var result = 0L
+                var t = num2
+
+                while (true) {
+                    map1[t]?.also {
+                        result += it
+                    }
+
+                    val next = map1.higherKey(t) ?: break
+
+                    t = (next / num2 + (next % num2).sign) * num2
+                }
+
+                result
+            }
+        }
+    }
+
+    expect {
+        Solution().numberOfPairs(
+            intArrayOf(0, 1, 1, 1), intArrayOf(), 1
+        )
+    }
+}

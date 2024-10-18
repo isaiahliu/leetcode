dc3751fe49348299e735137bd269496dad07e641
isaiahliu <isaiahliu@outlook.com>
2024-10-19 03:04:56 +0800

    1

--- src/main/kotlin/p31xx/Problem3193.kt
+++ src/main/kotlin/p31xx/Problem3193.kt
@@ -1,63 +1,84 @@
 package p31xx
 
 import util.expect
+import java.util.*
 
 fun main() {
     class Solution {
         fun numberOfPermutations(n: Int, requirements: Array<IntArray>): Int {
-            val m = 1000000007
+            val m = 1000000007L
 
-            val req = IntArray(n) { -1 }
-            var maxCount = 0
-            req[0] = 0
-            for ((num, count) in requirements) {
-                req[num] = count
-                maxCount = maxOf(maxCount, count)
+            fun c(n: Int, k: Int): Int {
+                val count = minOf(k, n - k)
+
+                val f = LinkedList<Int>()
+                repeat(count) {
+                    f.add(it + 1)
+                }
+
+                var result = 1L
+                repeat(count) {
+                    result *= n - it
+
+                    while (f.isNotEmpty() && result % f.peek() == 0L) {
+                        result /= f.poll()
+                    }
+                }
+
+                result %= m
+
+                return result.toInt()
             }
 
-            if (req[0] != 0) {
+            val forcedSize = hashSetOf<Int>()
+            val maxCounts = IntArray(n) { Int.MAX_VALUE }
+            requirements.forEach { (end, count) ->
+                maxCounts[end] = count
+                forcedSize += end
+            }
+
+            if (maxCounts[0] in 1 until Int.MAX_VALUE) {
                 return 0
             }
 
-            val dp = Array(n) { IntArray(maxCount + 1) { -1 } }
-            fun dfs(end: Int, count: Int): Int {
-                return when {
-                    end == 0 -> {
-                        1
-                    }
-
-                    dp[end][count] != -1 -> {
-                        dp[end][count]
-                    }
-
-                    req[end - 1] >= 0 -> {
-                        val r = req[end - 1]
-
-                        if (r <= count && count <= end + r) {
-                            dfs(end - 1, r)
-                        } else {
-                            0
-                        }.also { dp[end][count] = it }
-                    }
-
-                    else -> {
-                        var sm = 0
-                        for (i in 0..minOf(end, count)) {
-                            sm = (sm + dfs(end - 1, count - i)) % m
-                        }
-                        sm.also { dp[end][count] = it }
-                    }
-                }
+            for (i in n - 2 downTo 0) {
+                maxCounts[i] = minOf(maxCounts[i], maxCounts[i + 1])
             }
 
-            return dfs(n - 1, req[n - 1])
+            val dp = Array(n) { LongArray(maxCounts.last() + 1) }
+
+            dp[0][0] = 1
+
+            for (i in 1 until dp.size) {
+                val current = dp[i]
+                val forced = i in forcedSize
+                val maxCount = maxCounts[i]
+
+                for (num in i downTo 0) {
+                    val greater = i - num
+                    val less = num
+                    for (newCount in maxOf(i - less, 0)..minOf(maxCount, greater)) {
+                        val prevCountEnd = maxCount - newCount
+                        val prevCountStart = if (forced) prevCountEnd else 0
+
+                        for (prevCount in prevCountStart..prevCountEnd) {
+                            current[newCount + prevCount] += 1L * dp[i - 1][prevCount] * c(greater, newCount) * c(less, i - newCount)
+                            current[newCount + prevCount] %= m
+                        }
+                    }
+                }
+
+            }
+
+            return dp.last().last().toInt()
         }
     }
 
     expect {
         Solution().numberOfPermutations(
-            3, arrayOf(
-                intArrayOf(2, 2), intArrayOf(0, 0)
+            2,
+            arrayOf(
+                intArrayOf(1, 1),
             )
         )
     }

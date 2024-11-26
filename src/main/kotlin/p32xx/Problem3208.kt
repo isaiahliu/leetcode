232e435f2146dc4a710774f537ce1e0c3beae177
isaiahliu <isaiahliu@outlook.com>
2024-11-27 00:31:27 +0800

    1

--- src/main/kotlin/p32xx/Problem3208.kt
+++ src/main/kotlin/p32xx/Problem3208.kt
@@ -5,15 +5,14 @@ import util.expect
 fun main() {
     class Solution {
         fun numberOfAlternatingGroups(colors: IntArray, k: Int): Int {
-            var diffCount = (colors.size - k + 1 until colors.lastIndex).count {
+            var diffCount = (colors.size - k until colors.lastIndex).count {
                 colors[it] != colors[it + 1]
             }
             return colors.indices.count { index ->
                 diffCount += colors[index] xor colors[(index - 1).mod(colors.size)]
+                diffCount -= colors[(index - k).mod(colors.size)] xor colors[(index - k + 1).mod(colors.size)]
 
-                k - 1 == diffCount.also {
-                    diffCount -= colors[(index - k + 1).mod(colors.size)] xor colors[(index - k + 2).mod(colors.size)]
-                }
+                k - 1 == diffCount
             }
         }
     }

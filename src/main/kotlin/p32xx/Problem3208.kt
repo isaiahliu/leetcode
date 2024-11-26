714765d1873018df8de4c3162a58e19492570f70
isaiahliu <isaiahliu@outlook.com>
2024-11-27 00:54:29 +0800

    1

--- src/main/kotlin/p32xx/Problem3208.kt
+++ src/main/kotlin/p32xx/Problem3208.kt
@@ -1,24 +1,21 @@
 package p32xx
 
 import util.expect
+import kotlin.math.sign
 
 fun main() {
     class Solution {
         fun numberOfAlternatingGroups(colors: IntArray, k: Int): Int {
-            var diffCount = (colors.size - k until colors.lastIndex).count {
-                colors[it] != colors[it + 1]
-            }
-            return colors.indices.count { index ->
-                diffCount += colors[index] xor colors[(index - 1).mod(colors.size)]
-                diffCount -= colors[(index - k).mod(colors.size)] xor colors[(index - k + 1).mod(colors.size)]
-
-                k - 1 == diffCount
-            }
+            return (0 until colors.size + k - 1).fold(intArrayOf(0, 0)) { acc, index ->
+                acc[0] = acc[0] * (colors[index % colors.size] xor colors[(index + 1) % colors.size]) + 1
+                acc[1] += (acc[0] / k).sign
+                acc
+            }[1]
         }
     }
     expect {
         Solution().numberOfAlternatingGroups(
-            intArrayOf(), 1
+            intArrayOf(0, 1, 0, 1, 0), 3
         )
     }
 }

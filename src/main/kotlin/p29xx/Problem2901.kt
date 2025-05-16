90a9aabb5c0d83521670ef9c2dfe4332b1a876ca
isaiahliu <isaiahliu@outlook.com>
2025-05-16 14:05:15 +0800

    1

--- src/main/kotlin/p29xx/Problem2901.kt
+++ src/main/kotlin/p29xx/Problem2901.kt
@@ -1,47 +1,55 @@
 package p29xx
 
 import util.expect
+import java.util.*
 import kotlin.math.absoluteValue
 
 fun main() {
     class Solution {
         fun getWordsInLongestSubsequence(words: Array<String>, groups: IntArray): List<String> {
-            val lengthGroups = hashMapOf<Int, MutableList<Int>>()
-
             val prevIndices = arrayOfNulls<Int>(words.size)
-            val prevLengths = IntArray(words.size)
+
+            val lengthGroups = hashMapOf<Int, TreeMap<Int, MutableSet<Int>>>()
+
+            var result: Int? = 0
+            var maxLength = 0
 
             words.forEachIndexed { index, word ->
-                lengthGroups[word.length]?.forEach { prevIndex ->
-                    if (groups[index] != groups[prevIndex] && word.indices.sumOf {
-                            word[it].compareTo(words[prevIndex][it]).absoluteValue
-                        } == 1) {
-                        val length = prevLengths[prevIndex] + 1
-                        if (prevLengths[index] < length) {
-                            prevLengths[index] = length
+                val prevLengths = lengthGroups.computeIfAbsent(word.length) { TreeMap(compareByDescending { it }) }
+
+                for ((length, indices) in prevLengths) {
+                    indices.forEach { prevIndex ->
+                        if (groups[index] != groups[prevIndex] && word.indices.sumOf {
+                                word[it].compareTo(words[prevIndex][it]).absoluteValue
+                            } == 1) {
+                            if (length + 1 > maxLength) {
+                                maxLength = length + 1
+                                result = index
+                            }
                             prevIndices[index] = prevIndex
+                            prevLengths.computeIfAbsent(length + 1) { hashSetOf() } += index
+
+                            return@forEachIndexed
                         }
                     }
                 }
 
-                lengthGroups.computeIfAbsent(word.length) { arrayListOf() } += index
+                prevLengths.computeIfAbsent(0) { hashSetOf() } += index
             }
 
-            return words.indices.maxBy { prevLengths[it] }.let {
-                buildList {
-                    var t: Int? = it
-                    while (t != null) {
-                        add(0, words[t])
-                        t = prevIndices[t]
-                    }
+            return buildList {
+                while (result != null) {
+                    add(0, words[result])
+                    result = prevIndices[result]
                 }
+
             }
         }
     }
 
     expect {
         Solution().getWordsInLongestSubsequence(
-            arrayOf("bdb", "aaa", "ada"), intArrayOf(2, 1, 3)
+            arrayOf("bad", "dc", "bc", "ccd", "dd", "da", "cad", "dba", "aba"), intArrayOf(9, 7, 1, 2, 6, 8, 3, 7, 2)
         )
     }
 }

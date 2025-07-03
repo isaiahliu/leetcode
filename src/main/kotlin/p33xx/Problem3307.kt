9aa9285131bd91d650c602ae7ad6b8d05e747738
isaiahliu <isaiahliu@outlook.com>
2025-07-03 13:33:13 +0800

    1

--- src/main/kotlin/p33xx/Problem3307.kt
+++ src/main/kotlin/p33xx/Problem3307.kt
@@ -1,20 +1,15 @@
 package p33xx
 
 import util.expect
+import kotlin.math.sign
 
 fun main() {
     class Solution {
         fun kthCharacter(k: Long, operations: IntArray): Char {
-            var operationIndex = 0
-            var t = k - 1
+            var index = -1
             var offset = 0
-            while (t > 0) {
-                if (t % 2 == 1L && operations[operationIndex] > 0) {
-                    offset++
-                }
-
-                operationIndex++
-                t /= 2
+            while ((1L shl ++index) < k) {
+                offset += ((k - 1) and (1L shl index)).sign * operations[index]
             }
 
             return 'a' + offset % 26

3d5e04d3355a2185fb1d52fa388deacd350d2ef1
isaiahliu <isaiahliu@outlook.com>
2025-07-14 09:56:17 +0800

    1

--- src/main/kotlin/p12xx/Problem1290.kt
+++ src/main/kotlin/p12xx/Problem1290.kt
@@ -6,16 +6,7 @@ import util.expect
 fun main() {
     class Solution {
         fun getDecimalValue(head: ListNode?): Int {
-            var result = 0
-
-            var t = head
-            while (t != null) {
-                result *= 2
-                result += t.`val`
-                t = t.next
-            }
-
-            return result
+            return generateSequence(head) { it.next }.fold(0) { acc, n -> acc * 2 + n.`val` }
         }
     }
 

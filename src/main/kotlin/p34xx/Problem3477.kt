b4033dc941be54fdd45ebeb6399faae09cff793c
isaiahliu <isaiahliu@outlook.com>
2025-08-05 22:49:12 +0800

    1

--- src/main/kotlin/p34xx/Problem3477.kt
+++ src/main/kotlin/p34xx/Problem3477.kt
@@ -5,17 +5,55 @@ import util.expect
 fun main() {
     class Solution {
         fun numOfUnplacedFruits(fruits: IntArray, baskets: IntArray): Int {
-            var result = 0
+            class SegNode(val left: Int, val right: Int) {
+                var max: Int = 0
 
-            fruits.forEach { f ->
-                for (i in baskets.indices) {
-                    if (f <= baskets[i]) {
-                        baskets[i] = -1
-                        return@forEach
+                val children by lazy {
+                    arrayOf(SegNode(left, (left + right) / 2), SegNode((left + right) / 2 + 1, right))
+                }
+
+                fun mark(index: Int, value: Int) {
+                    if (index < left || index > right) {
+                        return
+                    }
+
+                    max = maxOf(max, value)
+
+                    if (left < right) {
+                        children.forEach {
+                            it.mark(index, value)
+                        }
                     }
                 }
 
-                result++
+                fun remove(value: Int): Int? {
+                    if (value > max) {
+                        return null
+                    }
+
+                    if (left == right) {
+                        max = -1
+                        return left
+                    } else {
+                        val result = children[0].remove(value) ?: children[1].remove(value)
+
+                        max = children.maxOf { it.max }
+
+                        return result
+                    }
+                }
+            }
+
+            val root = SegNode(0, baskets.lastIndex)
+
+            baskets.forEachIndexed { index, b ->
+                root.mark(index, b)
+            }
+
+            var result = 0
+
+            fruits.forEach { f ->
+                root.remove(f) ?: result++
             }
 
             return result

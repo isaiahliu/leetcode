3dcf160289a6d9c56652d9454343c09c92ab9b08
isaiahliu <isaiahliu@outlook.com>
2025-09-06 09:36:59 +0800

    1

--- src/main/kotlin/p34xx/Problem3495.kt
+++ src/main/kotlin/p34xx/Problem3495.kt
@@ -5,28 +5,10 @@ import util.expect
 fun main() {
     class Solution {
         fun minOperations(queries: Array<IntArray>): Long {
-            return queries.sumOf {
-                var result = 0L
-                var left = it[0]
-                val right = it[1] + 1
-
-                for (op in 1..15) {
-                    val l = 1 shl ((op - 1) * 2)
-                    val r = 1 shl (op * 2)
-
-                    when {
-                        l > right -> {
-                            break
-                        }
-
-                        left < r -> {
-                            result += (minOf(r, right) - left) * op.toLong()
-                            left = r
-                        }
-                    }
-                }
-
-                (result + 1) / 2
+            return queries.sumOf { (left, right) ->
+                (1..15).sumOf { op ->
+                    maxOf(((minOf(1 shl (op * 2), right + 1) - maxOf(1 shl ((op - 1) * 2), left)) * op.toLong()), 0L)
+                }.let { (it + 1) / 2 }
             }
         }
     }

95dc1c898cdedf13b6b98c86c6897911a2c7eac3
isaiahliu <isaiahliu@outlook.com>
2025-09-14 01:28:58 +0800

    1

--- src/main/kotlin/p09xx/Problem0966.kt
+++ src/main/kotlin/p09xx/Problem0966.kt
@@ -7,77 +7,44 @@ fun main() {
         fun spellchecker(wordlist: Array<String>, queries: Array<String>): Array<String> {
             val vowels = setOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
 
-            val STRICT = 0
-            val IGNORE_CASE = 1
-            val IGNORE_VOWELS = 2
-
-            class Trie {
-                val children by lazy { hashMapOf<Char, Trie>() }
-
-                var wordIndex: Int? = null
-
-                fun mark(wordIndex: Int, index: Int) {
-                    wordlist[wordIndex].getOrNull(index)?.also {
-                        children.computeIfAbsent(it) { Trie() }.mark(wordIndex, index + 1)
-                    } ?: run {
-                        this.wordIndex = this.wordIndex ?: wordIndex
-                    }
-                }
-
-                fun query(word: String, index: Int, type: Int): Pair<Int, Int>? {
-                    if (word.length == index) {
-                        return wordIndex?.let { type to it }
-                    }
-
-                    var result: Pair<Int, Int>? = null
-                    (0..IGNORE_VOWELS).forEach {
-                        when (it) {
-                            STRICT -> {
-                                result = children[word[index]]?.query(word, index + 1, maxOf(type, STRICT))
-                            }
-
-                            IGNORE_CASE -> {
-                                if ((result?.first ?: it) < it) {
-                                    return@forEach
-                                }
-                                arrayOf(word[index].uppercaseChar(), word[index].lowercaseChar()).mapNotNull {
-                                    children[it]?.query(word, index + 1, maxOf(type, IGNORE_CASE))
-                                }.minWithOrNull(compareBy<Pair<Int, Int>> { it.first }.thenBy { it.second })?.also {
-                                    if (it.first < (result?.first ?: Int.MAX_VALUE)) {
-                                        result = it
-                                    } else if (it.first == result?.first && it.second < (result?.second ?: Int.MAX_VALUE)) {
-                                        result = it
-                                    }
-                                }
-                            }
-
-                            IGNORE_VOWELS -> {
-                                if (word[index] in vowels) {
-                                    vowels.mapNotNull {
-                                        children[it]?.query(word, index + 1, maxOf(type, IGNORE_VOWELS))
-                                    }.minWithOrNull(compareBy<Pair<Int, Int>> { it.first }.thenBy { it.second })?.also {
-                                        if (it.first < (result?.first ?: Int.MAX_VALUE)) {
-                                            result = it
-                                        } else if (it.first == result?.first && it.second < (result?.second ?: Int.MAX_VALUE)) {
-                                            result = it
-                                        }
-                                    }
-                                }
-                            }
+            fun String.toWildCardVowel(): String {
+                return buildString {
+                    this@toWildCardVowel.forEach {
+                        if (it in vowels) {
+                            append('*')
+                        } else {
+                            append(it.lowercaseChar())
                         }
                     }
-
-                    return result
                 }
             }
 
-            val root = Trie()
-            wordlist.indices.forEach {
-                root.mark(it, 0)
+            val stricts = hashSetOf<String>()
+            val ignoreCases = hashMapOf<String, Int>()
+            val ignoreVowels = hashMapOf<String, Int>()
+            wordlist.forEachIndexed { index, str ->
+                stricts += str
+                ignoreCases.putIfAbsent(str.lowercase(), index)
+
+                ignoreVowels.putIfAbsent(str.toWildCardVowel(), index)
             }
 
             return Array(queries.size) {
-                root.query(queries[it], 0, STRICT)?.second?.let { wordlist[it] }.orEmpty()
+                val query = queries[it]
+
+                if (query in stricts) {
+                    return@Array query
+                }
+
+                ignoreCases[query.lowercase()]?.also {
+                    return@Array wordlist[it]
+                }
+
+                ignoreVowels[query.toWildCardVowel()]?.also {
+                    return@Array wordlist[it]
+                }
+
+                return@Array ""
             }
         }
     }

277d05e8c981c7712ed6351c2f9b3ed793a17880
isaiahliu <isaiahliu@outlook.com>
2025-09-17 17:27:02 +0800

    1

--- src/main/kotlin/p19xx/Problem1935.kt
+++ src/main/kotlin/p19xx/Problem1935.kt
@@ -5,7 +5,27 @@ import util.expect
 fun main() {
     class Solution {
         fun canBeTypedWords(text: String, brokenLetters: String): Int {
-            return text.split(" ").count { it.none { it in brokenLetters } }
+            var result = 0
+            val brokenSet = brokenLetters.toSet()
+            var length = 0
+            text.forEach {
+                when {
+                    it in brokenSet -> length = Int.MIN_VALUE
+                    it != ' ' -> length++
+                    length > 0 -> {
+                        result++
+                        length = 0
+                    }
+
+                    else -> length = 0
+                }
+            }
+
+            if (length > 0) {
+                result++
+            }
+
+            return result
         }
     }
 

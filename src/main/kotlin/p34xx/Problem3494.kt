2ca0e26ef40243e51f2378651686b2ce0caccffe
isaiahliu <isaiahliu@outlook.com>
2025-10-09 10:45:32 +0800

    1

--- src/main/kotlin/p34xx/Problem3494.kt
+++ src/main/kotlin/p34xx/Problem3494.kt
@@ -5,28 +5,15 @@ import util.expect
 fun main() {
     class Solution {
         fun minTime(skill: IntArray, mana: IntArray): Long {
-            var offset = 0L
-            var sum = 0L
+            val sums = LongArray(skill.size + 1)
 
-            val sums = LongArray(skill.size) {
-                sum += skill[it] * mana[0]
-                sum
-            }
-
-            for (manaIndex in 1 until mana.size) {
-                sum = 0L
-                var newOffset = 0L
-
-                skill.forEachIndexed { skillIndex, s ->
-                    newOffset = maxOf(newOffset, sums[skillIndex] + offset - sum)
-                    sum += s * mana[manaIndex]
-                    sums[skillIndex] = sum
+            return mana.indices.fold(0L) { lastOffset, manaIndex ->
+                skill.indices.maxOf { skillIndex ->
+                    (sums[skillIndex + 1] + lastOffset - sums[skillIndex]).also {
+                        sums[skillIndex + 1] = sums[skillIndex] + skill[skillIndex] * mana[manaIndex]
+                    }
                 }
-
-                offset = newOffset
-            }
-
-            return sum + offset
+            } + sums.last()
         }
     }
 
